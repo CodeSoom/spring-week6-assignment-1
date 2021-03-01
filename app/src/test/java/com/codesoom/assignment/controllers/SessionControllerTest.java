6083700df73b6a70e902a4a44f86508450b795d7
newoo <newoo4297@naver.com>
2021-03-01 17:59:52 +0900

    SessionController의 login메소드 테스트 통과

--- app/src/test/java/com/codesoom/assignment/controllers/SessionControllerTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/SessionControllerTest.java
@@ -1,11 +1,15 @@
 package com.codesoom.assignment.controllers;
 
+import com.codesoom.assignment.application.AuthenticationService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.containsString;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -14,6 +18,14 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class SessionControllerTest {
     @Autowired
     private MockMvc mockMvc;
+
+    @MockBean
+    private AuthenticationService authenticationService;
+
+    @BeforeEach
+    void setUp() {
+        given(authenticationService.login()).willReturn("a.b.c");
+    }
 
     @Test
     void login() throws Exception {

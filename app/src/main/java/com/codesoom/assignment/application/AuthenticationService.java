aeb43ab035a57f3bf767f4fb5228140adacc0434
newoo <newoo4297@naver.com>
2021-03-01 18:28:48 +0900

    AuthenticationService에 암호화 기능 추가

--- app/src/main/java/com/codesoom/assignment/application/AuthenticationService.java
+++ app/src/main/java/com/codesoom/assignment/application/AuthenticationService.java
@@ -1,10 +1,20 @@
 package com.codesoom.assignment.application;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
 
 @Service
 public class AuthenticationService {
     public String login() {
-        return null;
+        String secret = "12345678901234567890123456789012";
+        Key key = Keys.hmacShaKeyFor(secret.getBytes());
+
+        return Jwts.builder()
+                .claim("userId", 1L)
+                .signWith(key)
+                .compact();
     }
 }

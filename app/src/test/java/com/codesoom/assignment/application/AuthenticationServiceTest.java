d1be7e21b64190b8058f56a97d2b1fe392cc976d
newoo <newoo4297@naver.com>
2021-03-01 18:07:07 +0900

    AuthenticationService의 login메소드에 대한 테스트코드 추가

--- app/src/test/java/com/codesoom/assignment/application/AuthenticationServiceTest.java
+++ app/src/test/java/com/codesoom/assignment/application/AuthenticationServiceTest.java
@@ -1,7 +1,22 @@
 package com.codesoom.assignment.application;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class AuthenticationServiceTest {
+    private AuthenticationService authenticationService;
 
+    @BeforeEach
+    void setUp() {
+        authenticationService = new AuthenticationService();
+    }
+
+    @Test
+    void login() {
+        String accessToken = authenticationService.login();
+
+        assertThat(accessToken).contains(".");
+    }
 }

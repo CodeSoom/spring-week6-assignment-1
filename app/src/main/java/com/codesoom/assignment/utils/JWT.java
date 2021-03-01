54a6c48d6b515369ab8638cdc549880ec1e7ffb3
juunini <juuni.ni.i@gmail.com>
2021-03-01 18:02:18 +0900

    Add jwt decode method

--- app/src/main/java/com/codesoom/assignment/utils/JWT.java
+++ app/src/main/java/com/codesoom/assignment/utils/JWT.java
@@ -1,7 +1,10 @@
 package com.codesoom.assignment.utils;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 
 import javax.crypto.SecretKey;
 import java.util.Map;
@@ -9,8 +12,8 @@ import java.util.Map;
 public class JWT {
     private final SecretKey secretKey;
 
-    public JWT(String key) {
-        secretKey = Keys.hmacShaKeyFor(key.getBytes());
+    public JWT(@Value("jwt.secret") String secret) {
+        secretKey = Keys.hmacShaKeyFor(secret.getBytes());
     }
 
     public String encode(Map<String, Object> headers, Map<String, Object> claims) {
@@ -19,5 +22,12 @@ public class JWT {
                 .addClaims(claims)
                 .signWith(secretKey)
                 .compact();
+    }
+
+    public Jws<Claims> decode(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(secretKey)
+                .build()
+                .parseClaimsJws(token);
     }
 }
--- app/src/test/java/com/codesoom/assignment/utils/JWTTest.java
+++ app/src/test/java/com/codesoom/assignment/utils/JWTTest.java
@@ -1,5 +1,8 @@
 package com.codesoom.assignment.utils;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.JwsHeader;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -14,24 +17,44 @@ class JWTTest {
     private final String key = "11111111111111111111111111111111";
     private final JWT jwt = new JWT(key);
 
+    private final Map<String, Object> givenBody = new HashMap<>() {{
+        put("key", "value");
+    }};
+    private final Map<String, Object> givenHeaders = new HashMap<>() {{
+        put("alg", "HS256");
+    }};
+
+    private final String token = "eyJhbGciOiJIUzI1NiJ9."
+            + "eyJrZXkiOiJ2YWx1ZSJ9."
+            + "oVwICBWJOb_ggP34mBdxQJumkUGaBYdargu_3um3JsQ";
+
     @Nested
     @DisplayName("encode 메서드는")
     class Describe_encode {
-        private final Map<String, Object> givenClaims = new HashMap<>() {{
-            put("key", "value");
-        }};
-        private final Map<String, Object> givenHeaders = new HashMap<>() {{
-            put("alg", "HS256");
-        }};
 
         @Test
         @DisplayName("jwt 토큰을 리턴한다.")
         void It_returns_jwt_token() {
-            assertThat(jwt.encode(givenHeaders, givenClaims)).isEqualTo(
-                    "eyJhbGciOiJIUzI1NiJ9."
-                            + "eyJrZXkiOiJ2YWx1ZSJ9."
-                            + "oVwICBWJOb_ggP34mBdxQJumkUGaBYdargu_3um3JsQ"
-            );
+            assertThat(jwt.encode(givenHeaders, givenBody)).isEqualTo(token);
+        }
+    }
+
+    @Nested
+    @DisplayName("decode 메서드는")
+    class Describe_decode {
+        @Nested
+        @DisplayName("주어진 토큰이 올바를 때")
+        class Context_given_token_is_valid {
+            @Test
+            @DisplayName("jwt 토큰의 body 를 리턴한다.")
+            void It_returns_jwt_body() {
+                Jws<Claims> claims = jwt.decode(token);
+                JwsHeader headers = claims.getHeader();
+                Claims body = claims.getBody();
+
+                assertThat(headers.get("alg")).isEqualTo(givenHeaders.get("alg"));
+                assertThat(body.get("key")).isEqualTo(givenBody.get("key"));
+            }
         }
     }
 }

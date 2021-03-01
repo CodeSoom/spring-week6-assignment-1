dd8507e4ad05a5d3c263ae373f7a0bc36355da9d
newoo <newoo4297@naver.com>
2021-03-01 16:34:02 +0900

    로그인 테스트 메서드에 토큰값에 대한 테스트 추가

--- app/src/test/java/com/codesoom/assignment/controllers/SessionControllerTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/SessionControllerTest.java
@@ -5,7 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(SessionController.class)
@@ -16,6 +18,7 @@ class SessionControllerTest {
     @Test
     void login() throws Exception {
         mockMvc.perform(post("/session"))
-                .andExpect(status().isCreated());
+                .andExpect(status().isCreated())
+                .andExpect(content().string(containsString(".")));
     }
 }

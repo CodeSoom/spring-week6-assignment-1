ce4cea886867a9904bfbbcf9bd4da6a4d9bdca20
etff <wish2rich@gmail.com>
2021-03-01 20:46:34 +0900

    fix: add no ArgsConstructor to deserialize object

--- app/src/main/java/com/codesoom/assignment/user/dto/UserRegistrationData.java
+++ app/src/main/java/com/codesoom/assignment/user/dto/UserRegistrationData.java
@@ -4,12 +4,14 @@ import com.github.dozermapper.core.Mapping;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
 @Getter
 @Builder
+@NoArgsConstructor
 @AllArgsConstructor
 public class UserRegistrationData {
     @NotBlank

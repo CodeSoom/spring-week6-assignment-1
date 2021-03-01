4c610c99c65ed6e2abf734e1bd23db65460855a1
Gyu <melon9751@naver.com>
2021-03-01 19:17:35 +0900

    Add test for session login
    Add login for jwt login. This just checkes create status.

---
+++ app/src/main/java/com/codesoom/assignment/controllers/SessionController.java
@@ -0,0 +1,6 @@
+package com.codesoom.assignment.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+public class SessionController {
+}
---
+++ app/src/test/java/com/codesoom/assignment/controllers/SessionControllerTest.java
@@ -0,0 +1,24 @@
+package com.codesoom.assignment.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@AutoConfigureMockMvc
+@WebMvcTest(SessionController.class)
+class SessionControllerTest {
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    void login() throws Exception {
+        mockMvc.perform(post("/session"))
+                .andExpect(status().isCreated());
+    }
+}

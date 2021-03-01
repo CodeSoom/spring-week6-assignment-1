e198a48ea76888ef183967f13fccf9103304b8cc
Gyu <melon9751@naver.com>
2021-03-01 19:51:11 +0900

    Add login in service for jwt login
    Add login in service to handle controller request. Return null.
    Controller makes accessToken with login in service.

--- app/src/main/java/com/codesoom/assignment/controllers/SessionController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/SessionController.java
@@ -1,16 +1,29 @@
 package com.codesoom.assignment.controllers;
 
+import com.codesoom.assignment.application.AuthenticationService;
+import com.codesoom.assignment.dto.SessionResponseData;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.AuthProvider;
+
 @RestController
 
 public class SessionController {
+    private final AuthenticationService authenticationService;
+    public SessionController(AuthenticationService authenticationService) {
+        this.authenticationService = authenticationService;
+    }
+
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public void login() {
-        //Todo
+    public SessionResponseData login() {
+        String accessToken = authenticationService.login();
+
+        return SessionResponseData.builder()
+                .accessToken(accessToken)
+                .build();
     }
 }

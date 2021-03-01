f461094ad5ff88f2ccd7eabb9e5b21ee20fbda11
juunini <juuni.ni.i@gmail.com>
2021-03-01 20:12:27 +0900

    Finish sign in correct test [Refactoring]

--- app/src/main/java/com/codesoom/assignment/application/AuthService.java
+++ app/src/main/java/com/codesoom/assignment/application/AuthService.java
@@ -1,6 +1,9 @@
 package com.codesoom.assignment.application;
 
+import com.codesoom.assignment.domain.User;
 import com.codesoom.assignment.domain.UserRepository;
+import com.codesoom.assignment.errors.UserNotFoundByEmailException;
+import com.codesoom.assignment.errors.WrongUserPasswordException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,5 +12,15 @@ public class AuthService {
 
     public AuthService(UserRepository userRepository) {
         this.userRepository = userRepository;
+    }
+
+    public User signIn(String email, String password) {
+        final User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UserNotFoundByEmailException(email));
+
+        if (!user.getPassword().equals(password)) {
+            throw new WrongUserPasswordException();
+        }
+        return user;
     }
 }

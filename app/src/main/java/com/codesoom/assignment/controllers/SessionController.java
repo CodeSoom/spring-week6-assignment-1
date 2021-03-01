e827d5655edb7351d0a02be0cfc012435f847297
newoo <newoo4297@naver.com>
2021-03-01 15:51:57 +0900

    SessionController 생성

---
+++ app/src/main/java/com/codesoom/assignment/controllers/SessionController.java
@@ -0,0 +1,9 @@
+package com.codesoom.assignment.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/session")
+public class SessionController {
+}

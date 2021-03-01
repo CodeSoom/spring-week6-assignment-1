999a468806dd83d819eef71f9e9d8b4613bb7c54
Gyu <melon9751@naver.com>
2021-03-01 19:36:51 +0900

    Add login for session
    Add login for session controller. Controller is empty.

--- app/src/main/java/com/codesoom/assignment/controllers/SessionController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/SessionController.java
@@ -1,6 +1,16 @@
 package com.codesoom.assignment.controllers;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+
 public class SessionController {
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void login() {
+        //Todo
+    }
 }

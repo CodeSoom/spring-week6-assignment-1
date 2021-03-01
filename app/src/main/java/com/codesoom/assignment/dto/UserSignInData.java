31786d7e006c78d8e53f935c20085272c75212d1
juunini <juuni.ni.i@gmail.com>
2021-03-01 15:48:21 +0900

    Add UserSignInData

--- app/src/main/java/com/codesoom/assignment/dto/UserSignInData.java
+++ app/src/main/java/com/codesoom/assignment/dto/UserSignInData.java
@@ -1,5 +1,36 @@
 package com.codesoom.assignment.dto;
 
-public class UserSignInData {
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.github.dozermapper.core.Mapping;
+import lombok.Getter;
+import lombok.experimental.Accessors;
+import lombok.experimental.FieldDefaults;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+@Accessors(fluent = true)
+@FieldDefaults(makeFinal = true)
+@Getter
+public class UserSignInData {
+    @NotBlank
+    @Email
+    @Mapping("email")
+    @JsonProperty("email")
+    String email;
+
+    @NotBlank
+    @Mapping("password")
+    @JsonProperty("password")
+    String password;
+
+    @JsonCreator
+    public UserSignInData(
+            @JsonProperty("email") String email,
+            @JsonProperty("password") String password
+    ) {
+        this.email = email;
+        this.password = password;
+    }
 }

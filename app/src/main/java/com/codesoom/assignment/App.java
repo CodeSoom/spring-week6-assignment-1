e0c73bb67d132605b3aa1e7b7d3fdb2ea8db0b7e
seungin yang <rhfpdk92@naver.com>
2021-03-02 14:58:10 +0900

    refact : dozermapper를 분리

--- app/src/main/java/com/codesoom/assignment/App.java
+++ app/src/main/java/com/codesoom/assignment/App.java
@@ -11,9 +11,4 @@ public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
     }
-
-    @Bean
-    public Mapper dozerMapper() {
-        return DozerBeanMapperBuilder.buildDefault();
-    }
 }

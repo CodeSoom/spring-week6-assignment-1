fc9d077cf28d6c39c2b35d194d8abb249f912fe0
developerOlive <chloehiho@gmail.com>
2021-03-03 08:37:10 +0900

    Refactor : AuthenticationService의 parseToken() 메소드 내용 구현

--- app/src/main/java/com/codesoom/assignment/application/AuthenticationService.java
+++ app/src/main/java/com/codesoom/assignment/application/AuthenticationService.java
@@ -1,6 +1,9 @@
 package com.codesoom.assignment.application;
 
+import com.codesoom.assignment.errors.InvalidAccessTokenException;
 import com.codesoom.assignment.utils.JwtUtil;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.security.SignatureException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -16,6 +19,11 @@ public class AuthenticationService {
     }
 
     public Long parseToken(String accessToken) {
-        return null;
+        try {
+            Claims claims = jwtUtil.decode(accessToken);
+            return claims.get("userId", Long.class);
+        } catch (SignatureException e) {
+            throw new InvalidAccessTokenException(accessToken);
+        }
     }
 }

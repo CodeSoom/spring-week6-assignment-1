a9d95c62775e40d13d138e66f6c76175237bdeba
Gyu <melon9751@naver.com>
2021-03-02 13:57:21 +0900

    토큰 디코딩을 구현하고 테스트를 작성하라
    생성한 토큰으로 디코딩을 하는 함수를 구현합니다.
    존재하는 토큰으로 디코딩을 하면 성공하는 테스트와 존재하지 않는 토큰으로
    디코딩할 때 예외가 발생하는 테스트를 작성합니다.

--- app/src/main/java/com/codesoom/assignment/util/JwtUtil.java
+++ app/src/main/java/com/codesoom/assignment/util/JwtUtil.java
@@ -1,5 +1,7 @@
 package com.codesoom.assignment.util;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
@@ -20,5 +22,13 @@ public class JwtUtil {
                 .claim("userId", 1)
                 .signWith(key)
                 .compact();
+    }
+
+    public Claims decode(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
     }
 }
--- app/src/test/java/com/codesoom/assignment/util/JwtUtilTest.java
+++ app/src/test/java/com/codesoom/assignment/util/JwtUtilTest.java
@@ -1,15 +1,20 @@
 package com.codesoom.assignment.util;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.security.SignatureException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 class JwtUtilTest {
     private static final String SECRET = "12345678901234567890123456789010";
-    private static final String TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjF9." +
+    private static final String EXISTED_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjF9." +
             "neCsyNLzy3lQ4o2yliotWT06FwSGZagaHpKdAkjnGGw";
+    private static final String NOT_EXISTED_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjF9." +
+            "neCsyNLzy3lQ4o2yliotWT06FwSGZagaHpKdAkjnGGW";
 
     private JwtUtil jwtUtil;
 
@@ -22,6 +27,19 @@ class JwtUtilTest {
     void encode() {
         String token = jwtUtil.encode(1L);
 
-        assertThat(token).isEqualTo(TOKEN);
+        assertThat(token).isEqualTo(EXISTED_TOKEN);
+    }
+
+    @Test
+    void decodeWithValidToken() {
+        Claims claims = jwtUtil.decode(EXISTED_TOKEN);
+
+        assertThat(claims.get("userId", Long.class)).isEqualTo(1L);
+    }
+
+    @Test
+    void decodeWithInvalidToken() {
+        assertThatThrownBy(() -> jwtUtil.decode(NOT_EXISTED_TOKEN))
+                .isInstanceOf(SignatureException.class);
     }
 }

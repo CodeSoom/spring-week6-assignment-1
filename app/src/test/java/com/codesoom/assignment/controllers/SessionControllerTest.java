7bedae786a67a6f29941a866f14c5de4336150a9
seungin yang <rhfpdk92@naver.com>
2021-03-02 15:35:04 +0900

    add : SessionControllerTest

---
+++ app/src/test/java/com/codesoom/assignment/controllers/SessionControllerTest.java
@@ -0,0 +1,30 @@
+package com.codesoom.assignment.controllers;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(SessionController.class)
+class SessionControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Nested
+    @DisplayName("POST - /session")
+    class Describe_post_session {
+
+        @Test
+        void it_test() throws Exception {
+            mockMvc.perform(post("/session"))
+            .andExpect(status().isCreated());
+        }
+    }
+}

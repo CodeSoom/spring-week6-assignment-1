6a517a3d7164f15035f14a626a37c4b628de2789
etff <wish2rich@gmail.com>
2021-03-02 16:08:26 +0900

    doc: add java doc
    add javadoc to JwtTokenProvider

--- app/src/main/java/com/codesoom/assignment/auth/infra/JwtTokenProvider.java
+++ app/src/main/java/com/codesoom/assignment/auth/infra/JwtTokenProvider.java
@@ -8,6 +8,9 @@ import org.springframework.stereotype.Component;
 import java.security.Key;
 import java.util.Date;
 
+/**
+ * JWT 토큰을 관리합니다.
+ */
 @Component
 public class JwtTokenProvider {
     private final Key key;
@@ -19,6 +22,11 @@ public class JwtTokenProvider {
         this.key = Keys.hmacShaKeyFor(secret.getBytes());
     }
 
+    /**
+     * 토큰을 생성합니다.
+     * @param userId 사용자 id
+     * @return 사용자 JWT 토큰
+     */
     public String encode(Long userId) {
         Date now = new Date();
         Date validity = new Date(now.getTime() + validityInMilliseconds);

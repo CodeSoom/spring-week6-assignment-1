b9845713de9d47565f3d8d3c86a3b5aa609e1409
developerOlive <chloehiho@gmail.com>
2021-03-03 08:36:32 +0900

    Feat : AuthenticationServiceTest 클래스에 parseToken 메소드 테스트 코드 생성

--- app/src/test/java/com/codesoom/assignment/application/AuthenticationServiceTest.java
+++ app/src/test/java/com/codesoom/assignment/application/AuthenticationServiceTest.java
@@ -1,14 +1,26 @@
 package com.codesoom.assignment.application;
 
+import com.codesoom.assignment.errors.InvalidAccessTokenException;
 import com.codesoom.assignment.utils.JwtUtil;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class AuthenticationServiceTest {
 
     private static final String SECRET = "12345678901234567890123456789010";
+
+    private static final String VALID_TOKEN
+            = "eyJhbGciOiJIUzI1NiJ9."
+            + "eyJ1c2VySWQiOjF9."
+            + "neCsyNLzy3lQ4o2yliotWT06FwSGZagaHpKdAkjnGGw";
+
+    private static final String INVALID_TOKEN
+            = "eyJhbGciOiJIUzI1NiJ9."
+            + "eyJ1c2VySWQiOjF9."
+            + "neCsyNLzy3lQ4o2yliotWT06FwSGZagaHpKdAkjnGG0";
 
     private AuthenticationService authenticationService;
 
@@ -23,6 +35,20 @@ class AuthenticationServiceTest {
     void login() {
         String accessToken = authenticationService.login();
 
-        assertThat(accessToken).contains(".");
+        assertThat(accessToken).isEqualTo(VALID_TOKEN);
+    }
+
+    @Test
+    void parseTokenWithValidToken() {
+        Long userId = authenticationService.parseToken(VALID_TOKEN);
+
+        assertThat(userId).isEqualTo(1L);
+    }
+
+    @Test
+    void parseTokenWithInvalidToken() {
+        assertThatThrownBy(
+                () -> authenticationService.parseToken(INVALID_TOKEN)
+        ).isInstanceOf(InvalidAccessTokenException.class);
     }
 }

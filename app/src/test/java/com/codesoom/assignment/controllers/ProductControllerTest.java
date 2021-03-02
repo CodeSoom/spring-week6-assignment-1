6e2156d7d935980a0d7075ef42ade98171f4ad78
developerOlive <chloehiho@gmail.com>
2021-03-03 08:15:04 +0900

    Feat : ProductControllerTest 클래스에 createWithInvalidAccessToken() 메소드 생성

--- app/src/test/java/com/codesoom/assignment/controllers/ProductControllerTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/ProductControllerTest.java
@@ -4,6 +4,7 @@ import com.codesoom.assignment.application.AuthenticationService;
 import com.codesoom.assignment.application.ProductService;
 import com.codesoom.assignment.domain.Product;
 import com.codesoom.assignment.dto.ProductData;
+import com.codesoom.assignment.errors.InvalidAccessTokenException;
 import com.codesoom.assignment.errors.ProductNotFoundException;
 import com.codesoom.assignment.utils.JwtUtil;
 import org.junit.jupiter.api.BeforeEach;
@@ -85,7 +86,9 @@ class ProductControllerTest {
                 .willThrow(new ProductNotFoundException(1000L));
 
         given(authenticationService.parseToken(VALID_TOKEN)).willReturn(1L);
-        
+
+        given(authenticationService.parseToken(INVALID_TOKEN))
+                .willThrow(new InvalidAccessTokenException(INVALID_TOKEN));
     }
 
     @Test
@@ -143,6 +146,20 @@ class ProductControllerTest {
     }
 
     @Test
+    void createWithInvalidAccessToken() throws Exception {
+        mockMvc.perform(
+                post("/products")
+                        .accept(MediaType.APPLICATION_JSON_UTF8_VALUE)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"name\":\"쥐돌이\",\"maker\":\"냥이월드\"," +
+                                "\"price\":5000}")
+                        .header("Authorization", "Bearer " + INVALID_TOKEN)
+
+        )
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
     void createWithValidAttributes() throws Exception {
         mockMvc.perform(
                 post("/products")
@@ -150,6 +167,7 @@ class ProductControllerTest {
                         .contentType(MediaType.APPLICATION_JSON)
                         .content("{\"name\":\"쥐돌이\",\"maker\":\"냥이월드\"," +
                                 "\"price\":5000}")
+                        .header("Authorization", "Bearer " + VALID_TOKEN)
         )
                 .andExpect(status().isCreated())
                 .andExpect(content().string(containsString("쥐돌이")));
@@ -165,6 +183,7 @@ class ProductControllerTest {
                         .contentType(MediaType.APPLICATION_JSON)
                         .content("{\"name\":\"\",\"maker\":\"\"," +
                                 "\"price\":0}")
+                        .header("Authorization", "Bearer " + VALID_TOKEN)
         )
                 .andExpect(status().isBadRequest());
     }

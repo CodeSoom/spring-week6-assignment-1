15ac2b983384c828d6c4636f6bf326939f893f16
JamieShin0201 <jamieshin1450@gmail.com>
2021-03-02 11:39:01 +0900

    Added Private NoArgsConstructor in UserDtos

--- app/src/main/java/com/codesoom/assignment/dto/UserLoginData.java
+++ app/src/main/java/com/codesoom/assignment/dto/UserLoginData.java
@@ -1,13 +1,16 @@
 package com.codesoom.assignment.dto;
 
 import com.github.dozermapper.core.Mapping;
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 
 @Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class UserLoginData {
 
     @NotBlank

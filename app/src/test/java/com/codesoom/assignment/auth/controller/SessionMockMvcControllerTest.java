f38f7f3deebe451eee762a461973c05324d769d0
etff <wish2rich@gmail.com>
2021-03-02 20:11:59 +0900

    test: remove exception test

--- app/src/test/java/com/codesoom/assignment/auth/controller/SessionMockMvcControllerTest.java
+++ app/src/test/java/com/codesoom/assignment/auth/controller/SessionMockMvcControllerTest.java
@@ -1,6 +1,8 @@
 package com.codesoom.assignment.auth.controller;
 
 import com.codesoom.assignment.auth.application.AuthenticationService;
+import com.codesoom.assignment.auth.dto.AuthenticationRequestDto;
+import com.codesoom.assignment.auth.dto.SessionResponseData;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -8,27 +10,24 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.web.context.WebApplicationContext;
 
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
+@SpringBootTest
 @AutoConfigureMockMvc
 @DisplayName("SessionController 클래스")
 class SessionMockMvcControllerTest {
     private static final String USER_EMAIL = "test@test.com";
     private static final String USER_PASSWORD = "password";
 
-    private static final String NOT_EXIST_EMAIL = "not_exist@test.com";
-    private static final String WRONG_PASSWORD = "not_password";
-
-
-    @Autowired
-    private WebApplicationContext wac;
+    private static final String VALID_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjEsImlhdCI6MTYxNDY2ODE2NiwiZXhwIjox" +
+            "NjE0NjY4MTY2fQ.0DARRfpDRIQLjkJyX5K4UvJNO-0x9fAw3-6YPCA3tdA";
 
     @Autowired
     private MockMvc mockMvc;
@@ -36,80 +35,32 @@ class SessionMockMvcControllerTest {
     @MockBean
     private AuthenticationService authenticationService;
 
-    private AuthenticationRequestDto authenticationRequestDto;
-
     @Autowired
     private ObjectMapper objectMapper;
 
-    @BeforeEach
-    void setUp() {
-        mockMvc = webAppContextSetup(wac).addFilter(((request, response, chain) -> {
-            response.setCharacterEncoding("UTF-8");
-            chain.doFilter(request, response);
-        })).build();
-    }
-
     @Nested
-    @DisplayName("등록할 사용자가 주어지면")
-    class Context_with_user {
-        @BeforeEach
-        void setUp() {
-            authenticationRequestDto = AuthenticationRequestDto.builder()
-                    .email(USER_EMAIL)
-                    .password(USER_PASSWORD)
-                    .build();
-        }
+    @DisplayName("POST /session은")
+    class Describe_session {
+        AuthenticationRequestDto authenticationRequestDto;
 
-        @DisplayName("201 Created 상태를 응답한다.")
-        @Test
-        void It_responds_token() throws Exception {
-            mockMvc.perform(post("/session")
-                    .contentType(MediaType.APPLICATION_JSON_VALUE)
-                    .content(objectMapper.writeValueAsString(authenticationRequestDto)))
-                    .andExpect(status().isCreated());
+        @Nested
+        @DisplayName("등록할 사용자가 주어지면")
+        class Context_with_user {
+            @BeforeEach
+            void setUp() {
+                authenticationRequestDto = new AuthenticationRequestDto(USER_EMAIL, USER_PASSWORD);
+                given(authenticationService.authenticate(authenticationRequestDto))
+                        .willReturn(new SessionResponseData(VALID_TOKEN));
+            }
+
+            @DisplayName("201 Created 상태를 응답한다.")
+            @Test
+            void It_responds_token() throws Exception {
+                mockMvc.perform(post("/session")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .content(objectMapper.writeValueAsString(authenticationRequestDto)))
+                        .andExpect(status().isCreated());
+            }
         }
     }
-
-    @Nested
-    @DisplayName("생성 정보가 없으면")
-    class Context_not_existed_email {
-        @BeforeEach
-        void setUp() {
-            authenticationRequestDto = AuthenticationRequestDto.builder()
-                    .email(NOT_EXIST_EMAIL)
-                    .password(USER_PASSWORD)
-                    .build();
-        }
-
-        @DisplayName("400 Bad Request 상태를 응답한다.")
-        @Test
-        void It_responds_bad_request() throws Exception {
-            mockMvc.perform(post("/session")
-                    .contentType(MediaType.APPLICATION_JSON_VALUE)
-                    .content(objectMapper.writeValueAsString(authenticationRequestDto)))
-                    .andExpect(status().isBadRequest());
-        }
-    }
-
-    @Nested
-    @DisplayName("잘못된 비밀번호가 입력되면")
-    class Context_with_wrong_password {
-        @BeforeEach
-        void setUp() {
-            authenticationRequestDto = AuthenticationRequestDto.builder()
-                    .email(USER_EMAIL)
-                    .password(WRONG_PASSWORD)
-                    .build();
-        }
-
-        @DisplayName("400 Bad Request 상태를 응답한다.")
-        @Test
-        void It_responds_bad_request() throws Exception {
-            mockMvc.perform(post("/sessions")
-                    .contentType(MediaType.APPLICATION_JSON_VALUE)
-                    .content(objectMapper.writeValueAsString(authenticationRequestDto)))
-                    .andExpect(status().isBadRequest());
-        }
-    }
-}
 }

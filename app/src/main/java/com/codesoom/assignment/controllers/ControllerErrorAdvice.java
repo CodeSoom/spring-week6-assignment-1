948ab5d3a35736e3792bd22525ec3d072494ab0a
JSS <rlawlstjd077@gmail.com>
2021-03-02 17:59:45 +0900

    test: Add login with invalid param test

--- app/src/main/java/com/codesoom/assignment/controllers/ControllerErrorAdvice.java
+++ app/src/main/java/com/codesoom/assignment/controllers/ControllerErrorAdvice.java
@@ -2,7 +2,9 @@ package com.codesoom.assignment.controllers;
 
 import com.codesoom.assignment.dto.ErrorResponse;
 import com.codesoom.assignment.errors.ProductNotFoundException;
+import com.codesoom.assignment.errors.UserAuthenticationFailedException;
 import com.codesoom.assignment.errors.UserEmailDuplicationException;
+import com.codesoom.assignment.errors.UserEmailNotExistException;
 import com.codesoom.assignment.errors.UserNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -29,5 +31,17 @@ public class ControllerErrorAdvice {
     @ExceptionHandler(UserEmailDuplicationException.class)
     public ErrorResponse handleUserEmailIsAlreadyExisted() {
         return new ErrorResponse("User's email address is already existed");
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(UserEmailNotExistException.class)
+    public ErrorResponse handleUserEmailIsNotExist() {
+        return new ErrorResponse("User's email is not exist");
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(UserAuthenticationFailedException.class)
+    public ErrorResponse handleUserAuthenticationIsFailed() {
+        return new ErrorResponse("User's authentication failed");
     }
 }
--- app/src/test/java/com/codesoom/assignment/controllers/AuthenticationControllerTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/AuthenticationControllerTest.java
@@ -9,6 +9,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import com.codesoom.assignment.application.AuthenticationService;
 import com.codesoom.assignment.domain.User;
 import com.codesoom.assignment.dto.UserLoginData;
+import com.codesoom.assignment.errors.UserAuthenticationFailedException;
+import com.codesoom.assignment.errors.UserEmailNotExistException;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
@@ -21,6 +24,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 
 @DisplayName("AuthenticationController 클래스")
 @WebMvcTest(AuthenticationController.class)
@@ -28,6 +32,9 @@ class AuthenticationControllerTest {
   final String VALID_EMAIL = "rlawlstjd@gmail.com";
   final String VALID_PASSWORD = "12345";
   final String VALID_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjF9.5VoLz2Ug0E6jQK_anP6RND1YHpzlBdxsR2ORgYef_aQ";
+
+  final String INVALID_EMAIL = "test@gmail.com";
+  final String INVALID_PASSWORD = "123456";
 
   @Autowired
   MockMvc mockMvc;
@@ -43,34 +50,79 @@ class AuthenticationControllerTest {
     Mockito.reset(authenticationService);
 
     given(authenticationService.createSession(VALID_EMAIL, VALID_PASSWORD)).willReturn(VALID_TOKEN);
+    given(authenticationService.createSession(INVALID_EMAIL, VALID_PASSWORD))
+        .willThrow(new UserEmailNotExistException(INVALID_EMAIL));
+    given(authenticationService.createSession(VALID_EMAIL, INVALID_PASSWORD))
+        .willThrow(new UserAuthenticationFailedException(INVALID_EMAIL));
   }
 
   @Nested
   @DisplayName("POST /session 요청은")
   class Describe_POST_session {
 
+    ResultActions subject(UserLoginData loginData) throws Exception {
+      return mockMvc.perform(
+          post("/session")
+              .contentType(MediaType.APPLICATION_JSON)
+              .content(objectMapper.writeValueAsString(loginData))
+      );
+    }
+
     @Nested
     @DisplayName("유효한 email과 password가 주어진다면")
     class Context_with_email_password {
-
       String givenEmail = VALID_EMAIL;
       String givenPassword = VALID_PASSWORD;
 
-      @DisplayName("token을 응답한다")
+      @DisplayName("201코드와 token을 응답한다")
       @Test
-      void it_returns_token() throws Exception {
+      void it_returns_201_with_token() throws Exception {
         UserLoginData loginData = UserLoginData.builder()
             .email(givenEmail)
             .password(givenPassword)
             .build();
 
-        mockMvc.perform(
-            post("/session")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(loginData))
-        )
+        subject(loginData)
             .andExpect(status().isCreated())
             .andExpect(content().string(VALID_TOKEN));
+      }
+    }
+
+    @Nested
+    @DisplayName("존재하지 않는 email이 주어진다면")
+    class Context_with_not_exist_email {
+      String givenEmail = INVALID_EMAIL;
+      String givenPassword = VALID_PASSWORD;
+
+      @DisplayName("400 코드를 응답한다")
+      @Test
+      void it_returns_400_code() throws Exception {
+        UserLoginData loginData = UserLoginData.builder()
+            .email(givenEmail)
+            .password(givenPassword)
+            .build();
+
+        subject(loginData)
+            .andExpect(status().isBadRequest());
+      }
+    }
+
+    @Nested
+    @DisplayName("유효하지 않은 password가 주어진다면")
+    class Context_with_invalid_password {
+      String givenEmail = VALID_EMAIL;
+      String givenPassword = INVALID_PASSWORD;
+
+      @DisplayName("400 코드를 응답한다")
+      @Test
+      void it_returns_400_code() throws Exception {
+        UserLoginData loginData = UserLoginData.builder()
+            .email(givenEmail)
+            .password(givenPassword)
+            .build();
+
+        subject(loginData)
+            .andExpect(status().isBadRequest());
       }
     }
   }

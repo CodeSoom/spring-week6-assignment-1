676a2fefe6ef3bd46e7628e29a773f3baa84fc0d
developerOlive <chloehiho@gmail.com>
2021-03-03 03:06:46 +0900

    Feat : JwtUtilTest 클래스 생성

---
+++ app/src/test/java/com/codesoom/assignment/utils/JwtUtilTest.java
@@ -0,0 +1,38 @@
+package com.codesoom.assignment.utils;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class JwtUtilTest {
+
+    private static final String SECRET = "12345678901234567890123456789010";
+
+    private static final String ACCESS_TOKEN
+            = "eyJhbGciOiJIUzI1NiJ9."
+            + "eyJ1c2VySWQiOjF9."
+            + "neCsyNLzy3lQ4o2yliotWT06FwSGZagaHpKdAkjnGGw";
+
+    private JwtUtil jwtUtil;
+
+    @BeforeEach
+    void setUp() {
+        jwtUtil = new JwtUtil(SECRET);
+    }
+
+    @Test
+    void encode() {
+        String accessToken = jwtUtil.encode(1L);
+
+        assertThat(accessToken).isEqualTo(".");
+    }
+
+    @Test
+    void decode() {
+        jwtUtil.decode(ACCESS_TOKEN);
+
+
+    }
+
+}

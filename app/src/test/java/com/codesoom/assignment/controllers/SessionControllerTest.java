426e6787dd36f313fdd6899ee7d707952e77ac9a
developerOlive <chloehiho@gmail.com>
2021-03-02 23:43:24 +0900

    Feat : sessionControllerTest - 테스트 진행을 위해 @MockBean 이용하여 service 주입

--- app/src/test/java/com/codesoom/assignment/controllers/SessionControllerTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/SessionControllerTest.java
@@ -1,11 +1,15 @@
 package com.codesoom.assignment.controllers;
 
+import com.codesoom.assignment.application.AuthenticationService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.containsString;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -16,12 +20,19 @@ class SessionControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @MockBean
+    private AuthenticationService authenticationService;
+
+    @BeforeEach
+    void setup() {
+        given(authenticationService.login()).willReturn("a.b.c");
+    }
+
     @Test
     void login() throws Exception {
         mockMvc.perform(post("/session"))
-            .andExpect(status().isCreated())
-
-        .andExpect(content().string(containsString(".")));
+                .andExpect(status().isCreated())
+                .andExpect(content().string(containsString(".")));
     }
 
 }

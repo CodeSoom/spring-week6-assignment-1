adb122e8933782102d7b2aa3ab1ac3e7666ae537
wenodev <logicalstark1105@gmail.com>
2021-03-03 08:27:57 +0900

    test: login을 실패하라

--- app/src/test/java/com/codesoom/assignment/controllers/SessionControllerTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/SessionControllerTest.java
@@ -1,7 +1,26 @@
 package com.codesoom.assignment.controllers;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+/*
+1. createToken
+ */
+@WebMvcTest(SessionController.class)
 class SessionControllerTest {
 
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    void login() throws Exception {
+        mockMvc.perform(post("/session"))
+                .andExpect(status().isOk());
+    }
 }

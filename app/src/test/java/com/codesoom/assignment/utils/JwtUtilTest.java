5020b51a63f5462ddf79b0d30430e9c2f4c76fb4
newoo <newoo4297@naver.com>
2021-03-02 18:46:21 +0900

    encode메소드에 대한 테스트 추가 및 통과

--- app/src/test/java/com/codesoom/assignment/utils/JwtUtilTest.java
+++ app/src/test/java/com/codesoom/assignment/utils/JwtUtilTest.java
@@ -1,7 +1,26 @@
 package com.codesoom.assignment.utils;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class JwtUtilTest {
+    final String secret = "12345678901234567890123456789012";
+    final String accessTokenValue
+            = "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjF9.ZZ3CUl0jxeLGvQ1Js5nG2Ty5qGTlqai5ubDMXZOdaDk";
 
+    private JwtUtil jwtUtil;
+
+    @BeforeEach
+    void setUp() {
+        jwtUtil = new JwtUtil(secret);
+    }
+
+    @Test
+    void encode() {
+        String accessToken = jwtUtil.encode(1L);
+
+        assertThat(accessToken).isEqualTo(accessTokenValue);
+    }
 }

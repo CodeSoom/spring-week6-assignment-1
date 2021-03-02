bc5527931c33ca44954b2ac77b10638aca125f3b
etff <wish2rich@gmail.com>
2021-03-02 16:26:31 +0900

    test: add jwt decode method

--- app/src/test/java/com/codesoom/assignment/auth/infra/JwtTokenProviderTest.java
+++ app/src/test/java/com/codesoom/assignment/auth/infra/JwtTokenProviderTest.java
@@ -1,5 +1,6 @@
 package com.codesoom.assignment.auth.infra;
 
+import io.jsonwebtoken.Claims;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -8,6 +9,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class JwtTokenProviderTest {
     private static final String SECRET = "12345678901234567890123456789010";
+    private static final String VALID_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjEsImlhdCI6MTYxNDY2ODE2NiwiZXhwIjox" +
+            "NjE0NjY4MTY2fQ.0DARRfpDRIQLjkJyX5K4UvJNO-0x9fAw3-6YPCA3tdA";
     private static final Long GIVEN_ID = 1L;
 
     private JwtTokenProvider jwtTokenProvider;
@@ -23,6 +26,14 @@ class JwtTokenProviderTest {
         String token = jwtTokenProvider.encode(GIVEN_ID);
 
         assertThat(token).isNotEmpty();
+    }
+
+    @DisplayName("decode 메서드는 유효한 토큰에 담긴 id를 리턴한다")
+    @Test
+    void decodeWithValidToken() {
+        Claims claims = jwtTokenProvider.decode(VALID_TOKEN);
+
+        assertThat(claims.get("userId", Long.class)).isEqualTo(GIVEN_ID);
     }
 
 }

84a3216a2a562ca5cf09c4316fb059e1ce6fd6e2
rlawlstjd0077 <rlawlstjd077@gmail.com>
2021-03-02 21:11:17 +0900

    feat: Add 'TokenResponse' DTO

--- app/src/main/java/com/codesoom/assignment/controllers/AuthenticationController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/AuthenticationController.java
@@ -1,12 +1,14 @@
 package com.codesoom.assignment.controllers;
 
 import com.codesoom.assignment.application.AuthenticationService;
+import com.codesoom.assignment.dto.TokenResponse;
 import com.codesoom.assignment.dto.UserLoginData;
 import javax.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,7 +25,10 @@ public class AuthenticationController {
 
   @PostMapping
   @ResponseStatus(HttpStatus.CREATED)
-  String login(@RequestBody @Valid UserLoginData loginData) {
-    return authService.createSession(loginData.getEmail(), loginData.getPassword());
+  TokenResponse login(@RequestBody @Valid UserLoginData loginData) {
+    String token = authService.createSession(loginData.getEmail(), loginData.getPassword());
+    return TokenResponse.builder()
+            .token(token)
+            .build();
   }
 }
---
+++ app/src/main/java/com/codesoom/assignment/dto/TokenResponse.java
@@ -0,0 +1,14 @@
+package com.codesoom.assignment.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+public class TokenResponse {
+    private String token;
+}

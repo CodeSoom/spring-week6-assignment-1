9305b3a8bf5b768f9afcb6549c8fe7ae082d7739
thkim <thkim2017@gmail.com>
2021-03-03 17:11:39 +0900

    Apply AuthenticationService in SessionController An accessToken is the result of authenticationService.login()

--- app/src/main/java/com/codesoom/assignment/controllers/SessionController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/SessionController.java
@@ -1,6 +1,9 @@
 package com.codesoom.assignment.controllers;
 
 import com.codesoom.assignment.dto.SessionResponseData;
+import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
+import org.checkerframework.checker.units.qual.A;
+import org.hibernate.Session;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,11 +13,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/session")
 public class SessionController {
+    private AuthenticationService authenticationService;
+
+    public SessionController(AuthenticationService authenticationService) {
+        this.authenticationService = authenticationService;
+    }
+
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public SessionResponseData login() {
+        String accessToken = authenticationService.login();
+        
         return SessionResponseData.builder()
-                .accessToken("a.b.c")
+                .accessToken(accessToken)
                 .build();
     }
 }

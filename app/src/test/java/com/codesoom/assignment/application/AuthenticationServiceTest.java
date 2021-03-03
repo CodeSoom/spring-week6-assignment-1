32da6ffa49106f1bbb133053992d3c3c5a9727e1
JamieShin0201 <jamieshin1450@gmail.com>
2021-03-03 15:56:33 +0900

    Change test code to BDD style in AuthenticationServiceTest

--- app/src/test/java/com/codesoom/assignment/application/AuthenticationServiceTest.java
+++ app/src/test/java/com/codesoom/assignment/application/AuthenticationServiceTest.java
@@ -8,25 +8,24 @@ import com.codesoom.assignment.errors.UserAuthenticationFailException;
 import com.codesoom.assignment.utils.JwtUtil;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.Optional;
-import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
 
+@DisplayName("AuthenticationService 클래스")
 class AuthenticationServiceTest {
 
     private AuthenticationService authenticationService;
 
-    private UserRepository userRepository = mock(UserRepository.class);
+    private final UserRepository userRepository = mock(UserRepository.class);
+
+    private final Long parsedUserId = 1L;
 
     private final String secret = "12345678901234567890123456789012";
     private final String validToken = "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjF9" +
@@ -34,108 +33,163 @@ class AuthenticationServiceTest {
     private final String inValidToken = "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjF9" +
             ".ZZ3CUl0jxeLGvQ1Js5nG2Ty5qGTlqai5ubDMXZOdaDA";
 
-    private static final String GIVEN_VALID_EMAIL = "jamie@example.com";
-    private static final String GIVEN_VALID_PASSWORD = "12345678";
-    private static final String GIVEN_INVALID_EMAIL = "invalidEmail@example.com";
-    private static final String GIVEN_INVALID_PASSWORD = "invalidPassword";
+    private final String email = "jamie@example.com";
+    private final String password = "12345678";
 
-    private static final User user = User.builder()
-            .email(GIVEN_VALID_EMAIL)
-            .password(GIVEN_VALID_PASSWORD)
-            .build();
+    private User user;
+    private User deletedUser;
 
-    private static final UserLoginData userLoginData = UserLoginData.builder()
-            .email(GIVEN_VALID_EMAIL)
-            .password(GIVEN_VALID_PASSWORD)
-            .build();
+    private UserLoginData userLoginData;
+    private UserLoginData userLoginDataWithNotExistingEmail;
+    private UserLoginData userLoginDataWithWrongPassword;
+
 
     @BeforeEach
     void setUp() {
         JwtUtil jwtUtil = new JwtUtil(secret);
 
         authenticationService = new AuthenticationService(jwtUtil, userRepository);
-    }
 
-    @DisplayName("login 메소드에 유효한 회원 정보가 주어진다면 생성된 액세스 토큰을 리턴한다.")
-    @Test
-    void loginWithValidUserLoginData() {
-        given(userRepository.findByEmail(any()))
-                .willReturn(Optional.of(user));
-
-        String accessToken = authenticationService.login(userLoginData);
-
-        assertThat(accessToken).contains(".");
-    }
-
-    @DisplayName("login 메소드에 유효하지 않은 회원 정보가 주어진다면 '회원 인증에 실패했습니다' 라는 예외를 던진다.")
-    @ParameterizedTest(name = "{index} {3}")
-    @MethodSource("provideInvalidUser")
-    void loginWithInvalidUserLoginData(
-            UserLoginData invalidUserLoginData,
-            Optional<User> invalidUser,
-            String errorKeyword,
-            String representation) {
-        given(userRepository.findByEmail(invalidUserLoginData.getEmail()))
-                .willReturn(invalidUser);
-
-        UserAuthenticationFailException exception = assertThrows(UserAuthenticationFailException.class,
-                () -> authenticationService.login(invalidUserLoginData));
-
-        assertThat(exception.getMessage()).contains(errorKeyword);
-    }
-
-    @DisplayName("parseToken 메소드는 유효한 토큰이 주어진다면 파싱된 값을 리턴한다.")
-    @Test
-    void parseTokenWithValidToken() {
-        Long userId = authenticationService.parseToken(validToken);
-
-        assertThat(userId).isEqualTo(1L);
-    }
-
-    @DisplayName("parseToken 메소드는 유효하지 않은 토큰이 주어진다면 '토큰이 유효하지 않습니다.' 라는 예외를 던진다.")
-    @Test
-    void parseTokenWithInvalidToken() {
-        assertThrows(InvalidTokenException.class,
-                () -> authenticationService.parseToken(inValidToken));
-    }
-
-    private static Stream<Arguments> provideInvalidUser() {
-        UserLoginData userLoginDataWithNotExistingEmail = UserLoginData.builder()
-                .email(GIVEN_INVALID_EMAIL)
-                .password(GIVEN_VALID_PASSWORD)
+        user = User.builder()
+                .email(email)
+                .password(password)
                 .build();
 
-        UserLoginData userLoginDataWithWrongPassword = UserLoginData.builder()
-                .email(GIVEN_VALID_EMAIL)
-                .password(GIVEN_INVALID_PASSWORD)
-                .build();
-
-        User deletedUser = User.builder()
-                .email(GIVEN_VALID_EMAIL)
-                .password(GIVEN_VALID_PASSWORD)
+        deletedUser = User.builder()
+                .email(email)
+                .password(password)
                 .deleted(true)
                 .build();
 
-        return Stream.of(
-                Arguments.of(
-                        userLoginDataWithNotExistingEmail,
-                        Optional.empty(),
-                        "이메일",
-                        "이메일이 존재하지 않을 경우"
-                ),
-                Arguments.of(
-                        userLoginDataWithWrongPassword,
-                        Optional.of(user),
-                        "비밀번호",
-                        "비밀번호가 일치하지 않을 경우"
-                ),
-                Arguments.of(
-                        userLoginData,
-                        Optional.of(deletedUser),
-                        "삭제된",
-                        "삭제된 회원일 경우"
-                )
-        );
+        userLoginData = UserLoginData.builder()
+                .email(email)
+                .password(password)
+                .build();
+
+        userLoginDataWithNotExistingEmail = UserLoginData.builder()
+                .email(email + "_invalid_email")
+                .password(password)
+                .build();
+
+        userLoginDataWithWrongPassword = UserLoginData.builder()
+                .email(email)
+                .password(password + "_wrong_password")
+                .build();
+    }
+
+    @Nested
+    @DisplayName("login")
+    class Describe_login {
+        @Nested
+        @DisplayName("주어진 로그인 정보에 해당하는 회원이 있다면")
+        class Context_when_the_user_exists {
+            @BeforeEach
+            void setUp() {
+                given(userRepository.findByEmail(userLoginData.getEmail()))
+                        .willReturn(Optional.of(user));
+            }
+
+            @DisplayName("액세스 토큰을 리턴한다.")
+            @Test
+            void it_returns_the_access_token() {
+                String accessToken = authenticationService.login(userLoginData);
+
+                assertThat(accessToken).contains(".");
+            }
+        }
+
+        @Nested
+        @DisplayName("주어진 로그인 정보에 해당하는 회원이 없다면")
+        class Context_when_the_user_does_not_exist {
+            private final String errorKeyword = "이메일";
+
+            @BeforeEach
+            void setUp() {
+                given(userRepository.findByEmail(userLoginDataWithNotExistingEmail.getEmail()))
+                        .willReturn(Optional.empty());
+            }
+
+            @DisplayName("'회원 인증에 실패했습니다' 라는 예외를 던진다.")
+            @Test
+            void it_throws_user_authentication_fail_exception() {
+                UserAuthenticationFailException e = assertThrows(UserAuthenticationFailException.class,
+                        () -> authenticationService.login(userLoginDataWithNotExistingEmail));
+
+                assertThat(e.getMessage()).contains(errorKeyword);
+            }
+        }
+
+        @Nested
+        @DisplayName("주어진 로그인 정보의 비밀번호가 다르다면")
+        class Context_when_the_password_is_wrong {
+            private final String errorKeyword = "비밀번호";
+
+            @BeforeEach
+            void setUp() {
+                given(userRepository.findByEmail(userLoginDataWithWrongPassword.getEmail()))
+                        .willReturn(Optional.of(user));
+            }
+
+            @DisplayName("'회원 인증에 실패했습니다' 라는 예외를 던진다.")
+            @Test
+            void it_throws_user_authentication_fail_exception() {
+                UserAuthenticationFailException e = assertThrows(UserAuthenticationFailException.class,
+                        () -> authenticationService.login(userLoginDataWithWrongPassword));
+
+                assertThat(e.getMessage()).contains(errorKeyword);
+            }
+        }
+
+        @Nested
+        @DisplayName("주어진 로그인 정보에 해당하는 회원이 이미 삭제된 회원 이라면")
+        class Context_when_the_user_is_already_deleted {
+            private final String errorKeyword = "삭제된";
+
+            @BeforeEach
+            void setUp() {
+                given(userRepository.findByEmail(userLoginData.getEmail()))
+                        .willReturn(Optional.of(deletedUser));
+            }
+
+            @DisplayName("'회원 인증에 실패했습니다' 라는 예외를 던진다.")
+            @Test
+            void it_throws_user_authentication_fail_exception() {
+                UserAuthenticationFailException e = assertThrows(UserAuthenticationFailException.class,
+                        () -> authenticationService.login(userLoginData));
+
+                assertThat(e.getMessage()).contains(errorKeyword);
+            }
+        }
+    }
+
+    @Nested
+    @DisplayName("parseToken")
+    class Describe_parseToken {
+        @Nested
+        @DisplayName("유효한 토큰이 주어진다면")
+        class Context_with_a_valid_token {
+            @DisplayName("파싱된 값을 리턴한다.")
+            @Test
+            void it_returns_the_parsed_value() {
+                Long userId = authenticationService.parseToken(validToken);
+
+                assertThat(userId).isEqualTo(parsedUserId);
+            }
+        }
+
+        @Nested
+        @DisplayName("유효하지 않은 토큰이 주어진다면")
+        class Context_with_a_invalid_token {
+            @DisplayName("'토큰이 유효하지 않습니다.' 라는 예외를 던진다.")
+            @Test
+            void it_throws_invalid_token_exception() {
+                assertThrows(InvalidTokenException.class,
+                        () -> authenticationService.parseToken(inValidToken));
+            }
+        }
     }
 
 }
+
+
+

785ccf38cfc1730404d090279b2334cf16f36beb
etff <wish2rich@gmail.com>
2021-03-03 12:03:58 +0900

    feature: add time util
    add localdatetime converter

---
+++ app/src/main/java/com/codesoom/assignment/global/utils/TimeUtil.java
@@ -0,0 +1,27 @@
+package com.codesoom.assignment.global.utils;
+
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+/**
+ * 시간 처리를 담당합니다.
+ */
+@Component
+public class TimeUtil {
+    /**
+     * LocalDatetime 포맷을 Date로 변경합니다.
+     *
+     * @param localDateTime 입력받은 시간
+     * @return 변경된 시간 포맷
+     */
+    public Date convertLocalDateTime(LocalDateTime localDateTime) {
+        ZonedDateTime zonedDateTime = ZonedDateTime.of(localDateTime, ZoneId.systemDefault());
+        GregorianCalendar cal = GregorianCalendar.from(zonedDateTime);
+        return cal.getTime();
+    }
+}

a0b45ca32787cb1ba99c61ab617b745e9d1291bb
seungin yang <rhfpdk92@naver.com>
2021-03-03 15:56:27 +0900

    Clean unnecessary import

--- app/src/main/java/com/codesoom/assignment/App.java
+++ app/src/main/java/com/codesoom/assignment/App.java
@@ -1,10 +1,7 @@
 package com.codesoom.assignment;
 
-import com.github.dozermapper.core.DozerBeanMapperBuilder;
-import com.github.dozermapper.core.Mapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class App {

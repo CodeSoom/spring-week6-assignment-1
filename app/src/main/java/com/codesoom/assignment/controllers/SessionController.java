b32da29237e82abaa5395fe8c3935ef588084df7
developerOlive <chloehiho@gmail.com>
2021-03-04 04:07:38 +0900

    Refactor : SessionController JavaDoc 작성 및 필요한 어노테이션 추가

--- app/src/main/java/com/codesoom/assignment/controllers/SessionController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/SessionController.java
@@ -3,14 +3,22 @@ package com.codesoom.assignment.controllers;
 import com.codesoom.assignment.application.AuthenticationService;
 import com.codesoom.assignment.dto.SessionResponseData;
 import com.codesoom.assignment.dto.UserLoginData;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * 세션과 관련된 HTTP 요청을 처리합니다.
+ */
 @RestController
 @RequestMapping("/session")
+@CrossOrigin
+@RequiredArgsConstructor
 public class SessionController {
 
     private AuthenticationService authenticationService;
@@ -19,11 +27,15 @@ public class SessionController {
         this.authenticationService = authenticationService;
     }
 
-    private UserLoginData userLoginData;
-
+    /**
+     * 주어진 회원 로그인 정보에 해당하는 액세스 토큰을 응답합니다.
+     *
+     * @param userLoginData 회원 로그인 정보
+     * @return 생성된 액세스 토큰
+     */
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public SessionResponseData login() {
+    public SessionResponseData login(@RequestBody UserLoginData userLoginData) {
 
         String accessToken = authenticationService.login(userLoginData);
 

0b8d50d966c15dd191d4f4b83b6a48eea6dfb2ff
wenodev <logicalstark1105@gmail.com>
2021-03-04 08:06:42 +0900

    feat: AuthenticationService 생성하라

--- app/src/main/java/com/codesoom/assignment/controllers/SessionController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/SessionController.java
@@ -1,5 +1,7 @@
 package com.codesoom.assignment.controllers;
 
+import com.codesoom.assignment.application.AuthenticationService;
+import com.codesoom.assignment.dto.SessionResponseData;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -8,9 +10,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SessionController {
 
+    private AuthenticationService authenticationService;
+
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/session")
-    public void login(){
+    public SessionResponseData login(){
+
+        authenticationService.login();
+
+        return SessionResponseData.builder()
+                .accessToken(accessToken)
+                .build();
     }
 
 }

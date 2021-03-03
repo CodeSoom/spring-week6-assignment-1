d1b01332d5e40c6a0734893008070005abde1bb9
mikekang47 <gyeong0308@gmail.com>
2021-03-03 16:35:55 +0900

    Added login method in SessionController

--- app/src/main/java/com/codesoom/assignment/controllers/SessionController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/SessionController.java
@@ -1,5 +1,19 @@
 package com.codesoom.assignment.controllers;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/session")
 public class SessionController {
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void login() {
+
+    }
+
 
 }

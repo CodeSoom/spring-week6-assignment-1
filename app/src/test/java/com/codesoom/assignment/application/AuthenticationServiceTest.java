fcf8939136a9b5e2c4a5fe3b3aa8db76ef8efd68
thkim <thkim2017@gmail.com>
2021-03-03 17:34:39 +0900

    Add a test for login() in AuthenticationServiceTest It asserts that accessToken contains "."

---
+++ app/src/test/java/com/codesoom/assignment/application/AuthenticationServiceTest.java
@@ -0,0 +1,24 @@
+package com.codesoom.assignment.application;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class AuthenticationServiceTest {
+    private AuthenticationService authenticationService;
+
+    @BeforeEach
+    void setUp() {
+        authenticationService = new AuthenticationService();
+    }
+
+    @Test
+    void login() {
+        String accessToken = authenticationService.login();
+
+        assertThat(accessToken).contains(".");
+    }
+
+}

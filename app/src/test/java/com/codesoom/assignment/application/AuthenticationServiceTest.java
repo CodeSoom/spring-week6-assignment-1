65d176f6267ce1855d9459c277e2c461cef395c2
thkim <thkim2017@gmail.com>
2021-03-03 17:54:26 +0900

    Create JwtUtil and apply it It encodes userId to an accessToken Apply JwtUtil at AuthenticationService, and the test

--- app/src/test/java/com/codesoom/assignment/application/AuthenticationServiceTest.java
+++ app/src/test/java/com/codesoom/assignment/application/AuthenticationServiceTest.java
@@ -1,17 +1,18 @@
 package com.codesoom.assignment.application;
 
+import com.codesoom.assignment.utils.JwtUtil;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class AuthenticationServiceTest {
     private AuthenticationService authenticationService;
 
     @BeforeEach
     void setUp() {
-        authenticationService = new AuthenticationService();
+        JwtUtil jwtUtil = new JwtUtil();
+        authenticationService = new AuthenticationService(jwtUtil);
     }
 
     @Test

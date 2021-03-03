44e3e2ce5e4c6f5d9ae65973eeccce31ce7317c3
etff <wish2rich@gmail.com>
2021-03-03 17:24:08 +0900

    resove confilict

--- app/src/test/java/com/codesoom/assignment/product/controller/ProductControllerTest.java
+++ app/src/test/java/com/codesoom/assignment/product/controller/ProductControllerTest.java
@@ -118,7 +118,6 @@ class ProductControllerTest {
                         .contentType(MediaType.APPLICATION_JSON)
                         .content("{\"name\":\"쥐돌이\",\"maker\":\"냥이월드\"," +
                                 "\"price\":5000}")
-                        .header("Authorization", "Bearer " + VALID_TOKEN)
         )
                 .andExpect(status().isCreated())
                 .andExpect(content().string(containsString("쥐돌이")));

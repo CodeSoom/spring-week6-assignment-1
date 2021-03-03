68f0a76189b4dfbb3502e5e59949c89dfaf638f0
thkim <thkim2017@gmail.com>
2021-03-03 17:29:43 +0900

    Mock authenticationService in SessionControllerTest Plus implement login() temporily in AuthenticationService

--- app/src/main/java/com/codesoom/assignment/controllers/SessionController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/SessionController.java
@@ -1,9 +1,7 @@
 package com.codesoom.assignment.controllers;
 
+import com.codesoom.assignment.application.AuthenticationService;
 import com.codesoom.assignment.dto.SessionResponseData;
-import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
-import org.checkerframework.checker.units.qual.A;
-import org.hibernate.Session;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,7 +21,7 @@ public class SessionController {
     @ResponseStatus(HttpStatus.CREATED)
     public SessionResponseData login() {
         String accessToken = authenticationService.login();
-        
+
         return SessionResponseData.builder()
                 .accessToken(accessToken)
                 .build();

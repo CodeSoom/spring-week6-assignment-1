840044cca7d156bca9767d5a53db0a8aeb6ae5bb
thkim <thkim2017@gmail.com>
2021-03-03 18:07:07 +0900

    Apply secret of application.yml in JwtUtil Secret of application.yml as a parameter of JwtUtil constructor

--- app/src/main/java/com/codesoom/assignment/utils/JwtUtil.java
+++ app/src/main/java/com/codesoom/assignment/utils/JwtUtil.java
@@ -2,16 +2,16 @@ package com.codesoom.assignment.utils;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import javax.crypto.SecretKey;
 import java.security.Key;
 
 @Component
 public class JwtUtil {
     private final Key key;
 
-    public JwtUtil(String secret) {
+    public JwtUtil(@Value("${jwt.secret}") String secret) {
         key = Keys.hmacShaKeyFor(secret.getBytes());
     }
 

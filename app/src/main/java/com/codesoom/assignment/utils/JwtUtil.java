1f3880e1ea1165af60534317f84b27655dc6ae3f
thkim <thkim2017@gmail.com>
2021-03-03 18:01:03 +0900

    Add a constructor of JwtUtil with secret Plus Apply it in the test

--- app/src/main/java/com/codesoom/assignment/utils/JwtUtil.java
+++ app/src/main/java/com/codesoom/assignment/utils/JwtUtil.java
@@ -4,14 +4,18 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Component;
 
+import javax.crypto.SecretKey;
 import java.security.Key;
 
 @Component
 public class JwtUtil {
-    public String encode(Long userId) {
-        String secret = "12345678901234567890123456789012";
-        Key key = Keys.hmacShaKeyFor(secret.getBytes());
+    private final Key key;
 
+    public JwtUtil(String secret) {
+        key = Keys.hmacShaKeyFor(secret.getBytes());
+    }
+
+    public String encode(Long userId) {
         return Jwts.builder()
                 .claim("userId", 1L)
                 .signWith(key)
--- app/src/test/java/com/codesoom/assignment/application/AuthenticationServiceTest.java
+++ app/src/test/java/com/codesoom/assignment/application/AuthenticationServiceTest.java
@@ -7,11 +7,13 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class AuthenticationServiceTest {
+    private static final String secret = "12345678901234567890123456789012";
+
     private AuthenticationService authenticationService;
 
     @BeforeEach
     void setUp() {
-        JwtUtil jwtUtil = new JwtUtil();
+        JwtUtil jwtUtil = new JwtUtil(secret);
         authenticationService = new AuthenticationService(jwtUtil);
     }
 

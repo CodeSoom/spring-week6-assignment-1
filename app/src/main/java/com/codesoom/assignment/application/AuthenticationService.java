3bfc29b9ff44f3878b89081747f5a2de2d899f03
Lee Dong Wook <dwlee@crscube.io>
2021-03-04 08:31:53 +0900

    FEATURE: add AuthenticationService

--- app/src/main/java/com/codesoom/assignment/application/AuthenticationService.java
+++ app/src/main/java/com/codesoom/assignment/application/AuthenticationService.java
@@ -1,7 +1,11 @@
 package com.codesoom.assignment.application;
 
+import com.codesoom.assignment.errors.InvalidAccessTokenException;
 import com.codesoom.assignment.utils.JwtUtil;
 import org.springframework.stereotype.Service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.security.SignatureException;
 
 @Service
 public class AuthenticationService {
@@ -14,5 +18,18 @@ public class AuthenticationService {
 
     public String login() {
         return jwtUtil.encode(1L);
+    }
+
+    public Long parseToken(String accessToken) {
+        if (accessToken == null || accessToken.isBlank()) {
+            throw new InvalidAccessTokenException(accessToken);
+        }
+
+        try {
+            Claims claims = jwtUtil.decode(accessToken);
+            return claims.get("userId", Long.class);
+        } catch (SignatureException e) {
+            throw new InvalidAccessTokenException(accessToken);
+        }
     }
 }

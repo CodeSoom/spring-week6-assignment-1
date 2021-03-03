198f645baf08432887097cdd32b16e3956c4a0b0
mikekang47 <gyeong0308@gmail.com>
2021-03-03 17:31:49 +0900

    Created AuthenticationServiceTest Added Key and secret code to check whether it is authenticated in AuthenticationService

--- app/src/main/java/com/codesoom/assignment/application/AuthenticationService.java
+++ app/src/main/java/com/codesoom/assignment/application/AuthenticationService.java
@@ -1,10 +1,23 @@
 package com.codesoom.assignment.application;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
 
 @Service
 public class AuthenticationService {
+
     public String login() {
-        return null;
+        String secret = "zxcvqwer";
+        Key key = Keys.hmacShaKeyFor(secret.getBytes());
+        
+        return Jwts.builder()
+                .claim("userId", 1L)
+                .signWith(key)
+                .compact();
     }
 }
---
+++ app/src/test/java/com/codesoom/assignment/application/AuthenticationServiceTest.java
@@ -0,0 +1,24 @@
+package com.codesoom.assignment.application;
+
+import org.checkerframework.checker.units.qual.A;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class AuthenticationServiceTest {
+    private AuthenticationService authenticationService;
+
+    @BeforeEach
+    void setUp() {
+        authenticationService = new AuthenticationService();
+    }
+    @Test
+    void login() {
+        String accessToken = authenticationService.login();
+
+        assertThat(accessToken).contains(".");
+
+    }
+
+}

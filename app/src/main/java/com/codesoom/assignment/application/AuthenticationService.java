2a8667b5bc337aad63b15becbe71b3f223df03ee
thkim <thkim2017@gmail.com>
2021-03-03 17:44:55 +0900

    Apply secret and claim in JWT In login() of AuthenticationService

--- app/src/main/java/com/codesoom/assignment/application/AuthenticationService.java
+++ app/src/main/java/com/codesoom/assignment/application/AuthenticationService.java
@@ -1,7 +1,6 @@
 package com.codesoom.assignment.application;
 
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
 
@@ -10,8 +9,11 @@ import java.security.Key;
 @Service
 public class AuthenticationService {
     public String login() {
-        Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+        String secret = "12345678901234567890123456789012";
+        Key key = Keys.hmacShaKeyFor(secret.getBytes());
 
-        return Jwts.builder().signWith(key).compact();
+        return Jwts.builder()
+                .claim("userId", 1L)
+                .signWith(key).compact();
     }
 }

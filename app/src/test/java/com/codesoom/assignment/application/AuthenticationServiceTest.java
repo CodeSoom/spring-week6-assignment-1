0df998f59de743d0ca4c2b323e7a760db02271a4
Lee Dong Wook <dwlee@crscube.io>
2021-03-04 08:29:51 +0900

    TEST: test with service code

--- app/src/test/java/com/codesoom/assignment/application/AuthenticationServiceTest.java
+++ app/src/test/java/com/codesoom/assignment/application/AuthenticationServiceTest.java
@@ -1,13 +1,20 @@
 package com.codesoom.assignment.application;
 
+import com.codesoom.assignment.errors.InvalidAccessTokenException;
 import com.codesoom.assignment.utils.JwtUtil;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 class AuthenticationServiceTest {
     private static final String SECRET = "12345678901234567890123456789012";
+
+    private static final String VALID_TOKEN = "eyJhbGciOiJIUzI1NiJ9." +
+        "eyJ1c2VySWQiOjF9.ZZ3CUl0jxeLGvQ1Js5nG2Ty5qGTlqai5ubDMXZOdaDk";
+    private static final String INVALID_TOKEN = "eyJhbGciOiJIUzI1NiJ9." +
+        "eyJ1c2VySWQiOjF9.ZZ3CUl0jxeLGvQ1Js5nG2Ty5qGTlqai5ubDMXZOdaD0";
 
     private AuthenticationService authenticationService;
 
@@ -21,7 +28,42 @@ class AuthenticationServiceTest {
     void login() {
         String accessToken = authenticationService.login();
 
-        assertThat(accessToken).contains(".");
+        assertThat(accessToken).isEqualTo(VALID_TOKEN);
     }
 
+    @Test
+    void parseToken() {
+        Long userId = authenticationService.parseToken(VALID_TOKEN);
+
+        assertThat(userId).isEqualTo(1L);
+    }
+
+    @Test
+    void parseTokenWithValidToken() {
+        Long userId = authenticationService.parseToken(VALID_TOKEN);
+
+        assertThat(userId).isEqualTo(1L);
+    }
+
+    @Test
+    void parseTokenWithInValidToken() {
+        assertThatThrownBy(
+            () -> authenticationService.parseToken(INVALID_TOKEN))
+            .isInstanceOf(InvalidAccessTokenException.class);
+    }
+
+    @Test
+    void parseTokenWithEmptyToken() {
+        assertThatThrownBy(
+            () -> authenticationService.parseToken(null))
+            .isInstanceOf(InvalidAccessTokenException.class);
+
+        assertThatThrownBy(
+            () -> authenticationService.parseToken(""))
+            .isInstanceOf(InvalidAccessTokenException.class);
+
+        assertThatThrownBy(
+            () -> authenticationService.parseToken("  "))
+            .isInstanceOf(InvalidAccessTokenException.class);
+    }
 }

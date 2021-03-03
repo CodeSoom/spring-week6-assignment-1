4c6934cb377c760f8766c6c054679d0301709354
Minwook Park <mwpark0975@gmail.com>
2021-03-03 18:14:18 +0900

    add jwtUtil decode function

--- app/src/main/java/com/codesoom/assignment/utils/JwtUtil.java
+++ app/src/main/java/com/codesoom/assignment/utils/JwtUtil.java
@@ -1,7 +1,10 @@
 package com.codesoom.assignment.utils;
 
+import com.codesoom.assignment.errors.InvalidTokenException;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SignatureException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -12,9 +15,6 @@ import java.security.Key;
  */
 @Component
 public class JwtUtil {
-//    1. construct with secret and store as a Key
-//    2. encode with given id
-//    3. decode returns Claims
 
     private final Key key;
 
@@ -27,5 +27,21 @@ public class JwtUtil {
                 .claim("userId", userId)
                 .signWith(key)
                 .compact();
+    }
+
+    public Claims decode(String token) {
+        if (token == null || token.isBlank()) {
+            throw new InvalidTokenException(token);
+        }
+
+        try {
+            return Jwts.parserBuilder()
+                    .setSigningKey(key)
+                    .build()
+                    .parseClaimsJws(token)
+                    .getBody();
+        } catch (SignatureException ex) {
+            throw new InvalidTokenException(token);
+        }
     }
 }
--- app/src/test/java/com/codesoom/assignment/utils/JwtUtilTest.java
+++ app/src/test/java/com/codesoom/assignment/utils/JwtUtilTest.java
@@ -1,18 +1,27 @@
 package com.codesoom.assignment.utils;
 
+import com.codesoom.assignment.errors.InvalidTokenException;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.security.SignatureException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+/**
+ * JwtUtil 클래스
+ */
 class JwtUtilTest {
 
     private final String SECRET = "12345678901234567890123456789012";
 
     private final String VALID_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjF9" +
             ".ZZ3CUl0jxeLGvQ1Js5nG2Ty5qGTlqai5ubDMXZOdaDk";
+
+    private final String INVALID_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjF9" +
+            ".ZZ3CUl0jxeLGvQ1Js5nG2Ty5qGTlqai5ubDMXZOdakD";
 
     private JwtUtil jwtUtil;
 
@@ -29,5 +38,27 @@ class JwtUtilTest {
 
         String token = jwtUtil.encode(USER_ID);
         assertThat(token).isEqualTo(VALID_TOKEN);
+    }
+
+    @Test
+    @DisplayName("Jwt decode()는 유효한 토큰이 주어지면 Claim을 리턴한다")
+    void decode_with_valid_token() {
+
+        Claims claims = jwtUtil.decode(VALID_TOKEN);
+        assertThat(claims.get("userId", Long.class)).isEqualTo(USER_ID);
+    }
+
+    @Test
+    @DisplayName("Jwt decode()는 유효하지 않은 토큰이 주어지면 InvalidToken 예외를 던진다")
+    void decode_with_invalid_token() {
+
+        assertThatThrownBy(() -> jwtUtil.decode(INVALID_TOKEN))
+                .isInstanceOf(InvalidTokenException.class);
+
+        assertThatThrownBy(() -> jwtUtil.decode(""))
+                .isInstanceOf(InvalidTokenException.class);
+
+        assertThatThrownBy(() -> jwtUtil.decode("  "))
+                .isInstanceOf(InvalidTokenException.class);
     }
 }

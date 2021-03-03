3bf7f4eda1ba1d20f50bce8ce994278364c6e8b6
양효주 <yhyojoo@icloud.com>
2021-03-04 00:40:34 +0900

    ProductController의 create 메소드에 @RequestHeader 추가

---
+++ app/src/main/java/com/codesoom/assignment/controllers/ProductController.java
@@ -0,0 +1,101 @@
+package com.codesoom.assignment.controllers;
+
+import com.codesoom.assignment.application.AuthenticationService;
+import com.codesoom.assignment.application.ProductService;
+import com.codesoom.assignment.domain.Product;
+import com.codesoom.assignment.dto.ProductData;
+import com.codesoom.assignment.utils.JwtUtil;
+import io.jsonwebtoken.Claims;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.util.List;
+
+/**
+ * 상품 관련 요청을 처리합니다.
+ */
+@RestController
+@RequestMapping("/products")
+@CrossOrigin
+@RequiredArgsConstructor
+public class ProductController {
+    private final ProductService productService;
+
+    private final AuthenticationService authenticationService;
+
+    /**
+     * 등록된 상품 목록 조회 요청을 처리합니다.
+     *
+     * @return 등록된 상품 목록
+     */
+    @GetMapping
+    public List<Product> list() {
+        return productService.getProducts();
+    }
+
+    /**
+     * 상품 조회 요청을 처리하고, 해당 상품 정보를 반환합니다.
+     *
+     * @param id
+     * @return 해당 상품 정보
+     */
+    @GetMapping("{id}")
+    public Product detail(@PathVariable Long id) {
+        return productService.getProduct(id);
+    }
+
+    /**
+     * 상품 등록 요청을 처리하고, 등록된 상품 정보를 반환합니다.
+     *
+     * @param productData
+     * @return 등록된 상품 정보
+     */
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Product create(
+            @RequestHeader("Authorization") String authorization,
+            @RequestBody @Valid ProductData productData
+    ) {
+        String accessToken = authorization.substring("Bearer ".length());
+        Long userId = authenticationService.parseToken(accessToken);
+
+        System.out.println("***** userId: " + userId);
+
+        return productService.createProduct(productData);
+    }
+
+    /**
+     * 상품 정보 수정 요청을 처리하고, 수정된 상품 정보를 반환합니다.
+     *
+     * @param id
+     * @param productData
+     * @return
+     */
+    @RequestMapping(value = "{id}", method = { RequestMethod.PUT, RequestMethod.PATCH })
+    public Product update(@PathVariable Long id, @RequestBody @Valid ProductData productData) {
+        return productService.updateProduct(id, productData);
+    }
+
+    /**
+     * 상품 삭제 요청을 처리합니다.
+     *
+     * @param id
+     */
+    @DeleteMapping("{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public void delete(@PathVariable Long id) {
+        productService.deleteProduct(id);
+    }
+}

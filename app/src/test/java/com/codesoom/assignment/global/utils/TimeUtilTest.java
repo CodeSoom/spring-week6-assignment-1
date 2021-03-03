a5ae90ebe834616252a428ca431bd2a2a1f71943
etff <wish2rich@gmail.com>
2021-03-03 12:01:08 +0900

    test: add time util
    test convert localdatetime to date

---
+++ app/src/test/java/com/codesoom/assignment/global/utils/TimeUtilTest.java
@@ -0,0 +1,36 @@
+package com.codesoom.assignment.global.utils;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.util.Calendar;
+import java.util.Date;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+class TimeUtilTest {
+    private Date dateTime;
+    private TimeUtil timeUtil;
+
+    @BeforeEach
+    void setUp() {
+        Calendar cal = Calendar.getInstance();
+        cal.set(2022, Calendar.JANUARY, 1, 0, 0, 0);
+        cal.set(Calendar.MILLISECOND, 0);
+        dateTime = cal.getTime();
+
+        timeUtil = new TimeUtil();
+    }
+
+    @DisplayName("LocalDatetime을 Date로 변경한 값이 같은지 비교합니다")
+    @Test
+    void convertLocalDatetime() {
+        LocalDateTime localDateTime = LocalDateTime.of(2022, Month.JANUARY, 1, 0, 0);
+
+        assertThat(timeUtil.convertLocalDateTime(localDateTime)).hasSameTimeAs(dateTime);
+    }
+}

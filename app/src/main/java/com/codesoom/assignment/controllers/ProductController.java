b6fb0017d173fe13a4d95c5097446af3362be23e
Lee Dong Wook <dwlee@crscube.io>
2021-03-04 08:31:13 +0900

    FEATURE: add ProductController

--- app/src/main/java/com/codesoom/assignment/controllers/ProductController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/ProductController.java
@@ -1,29 +1,40 @@
 package com.codesoom.assignment.controllers;
 
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MissingRequestHeaderException;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.codesoom.assignment.application.AuthenticationService;
 import com.codesoom.assignment.application.ProductService;
 import com.codesoom.assignment.domain.Product;
 import com.codesoom.assignment.dto.ProductData;
-import com.codesoom.assignment.utils.JwtUtil;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
-
-import javax.validation.Valid;
-import java.util.List;
-
-import io.jsonwebtoken.Claims;
 
 @RestController
 @RequestMapping("/products")
 @CrossOrigin
 public class ProductController {
+
     private final ProductService productService;
+    private final AuthenticationService authenticationService;
 
-    private final JwtUtil jwtUtil;
-
-    public ProductController(ProductService productService, JwtUtil jwtUtil) {
+    public ProductController(ProductService productService, AuthenticationService authenticationService) {
         this.productService = productService;
-        this.jwtUtil = jwtUtil;
+        this.authenticationService = authenticationService;
     }
 
     @GetMapping
@@ -42,9 +53,10 @@ public class ProductController {
             @RequestHeader("Authorization") String authorization,
             @RequestBody @Valid ProductData productData
     ) {
-        String accessToken = authorization.substring("Bearer ".length());
-        Claims claims = jwtUtil.decode(accessToken);
-        System.out.println("******* userId: " + claims.get("userId"));
+        String accessToken = authorization.substring("Bearer".length());
+        Long userId = authenticationService.parseToken(accessToken);
+
+        System.out.println("******* userId: " + userId);
         return productService.createProduct(productData);
     }
 
@@ -62,5 +74,11 @@ public class ProductController {
             @PathVariable Long id
     ) {
         productService.deleteProduct(id);
+    }
+
+    @ExceptionHandler(MissingRequestHeaderException.class)
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    public void handleMissingRequestHeaderException() {
+
     }
 }

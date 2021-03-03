2617b7653617ffe7fa584ece1a771c2779fd5abe
thkim <thkim2017@gmail.com>
2021-03-03 17:39:18 +0900

    Implement accessToken in authenticationService By using JWT

--- app/src/main/java/com/codesoom/assignment/application/AuthenticationService.java
+++ app/src/main/java/com/codesoom/assignment/application/AuthenticationService.java
@@ -1,10 +1,17 @@
 package com.codesoom.assignment.application;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
 
 @Service
 public class AuthenticationService {
     public String login() {
-        return null;
+        Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+
+        return Jwts.builder().signWith(key).compact();
     }
 }

4ddeae4e587f6c3f2aac1bf37884d9c9952803d6
wenodev <logicalstark1105@gmail.com>
2021-03-05 00:23:11 +0900

    feat: decode 메소드를 생성하라

--- app/src/test/java/com/codesoom/assignment/utils/JwtUtilTest.java
+++ app/src/test/java/com/codesoom/assignment/utils/JwtUtilTest.java
@@ -1,5 +1,6 @@
 package com.codesoom.assignment.utils;
 
+import io.jsonwebtoken.Claims;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -9,7 +10,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class JwtUtilTest {
 
     private static final String SECRET ="12345678901234567890123456789010";
-    private static final String ACCESS_TOKEN= "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjF9.neCsyNLzy3lQ4o2yliotWT06FwSGZagaHpKdAkjnGGw";
+    private static final String TOKEN= "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjF9.neCsyNLzy3lQ4o2yliotWT06FwSGZagaHpKdAkjnGGw";
     private JwtUtil jwtUtil;
 
     @BeforeEach
@@ -19,7 +20,14 @@ class JwtUtilTest {
 
     @Test
     void encode() {
-        String accessToken = jwtUtil.encode(1L);
-        assertThat(accessToken).isEqualTo(ACCESS_TOKEN);
+        String token = jwtUtil.encode(1L);
+        assertThat(token).isEqualTo(TOKEN);
     }
+
+    @Test
+    void decode(){
+        Claims claims = jwtUtil.decode(TOKEN);
+        assertThat(claims.get("userId", Long.class)).isEqualTo(1L);
+    }
+
 }

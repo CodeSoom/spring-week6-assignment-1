36d17819c9c48a1da5f1baf1b990a61856137840
Lee Dong Wook <sh95119@gmail.com>
2021-03-04 23:10:15 +0900

    FIX: fix to pass the test

--- app/src/test/java/com/codesoom/assignment/utils/JwtUtilTest.java
+++ app/src/test/java/com/codesoom/assignment/utils/JwtUtilTest.java
@@ -15,7 +15,7 @@ class JwtUtilTest {
 
     private static final String SECRET = "12345678901234567890123456789012";
     private static final String VALID_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjF9.ZZ3CUl0jxeLGvQ1Js5nG2Ty5qGTlqai5ubDMXZOdaDk";
-    private static final String INVALID_TOKEN = VALID_TOKEN + "...";
+    private static final String INVALID_TOKEN = VALID_TOKEN + "INVALID";
 
     private JwtUtil jwtUtil;
 

cfe9974731bdd40352ce3ac8cc51768846c875c4
etff <wish2rich@gmail.com>
2021-03-04 14:20:06 +0900

    refactor: remove method using old api

--- app/src/test/java/com/codesoom/assignment/product/controller/ProductControllerTest.java
+++ app/src/test/java/com/codesoom/assignment/product/controller/ProductControllerTest.java
@@ -122,7 +122,6 @@ class ProductControllerTest {
     void createWithValidAttributes() throws Exception {
         mockMvc.perform(
                 post("/products")
-                        .accept(MediaType.APPLICATION_JSON_UTF8)
                         .contentType(MediaType.APPLICATION_JSON)
                         .content("{\"name\":\"쥐돌이\",\"maker\":\"냥이월드\"," +
                                 "\"price\":5000}")

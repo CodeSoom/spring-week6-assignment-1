bf54d2ede4d4c62b7607dc1dedd01fb20407168c
Lee Dong Wook <sh95119@gmail.com>
2021-03-04 21:14:32 +0900

    TEST: fix to look easy

--- app/src/test/java/com/codesoom/assignment/utils/JwtUtilTest.java
+++ app/src/test/java/com/codesoom/assignment/utils/JwtUtilTest.java
@@ -14,7 +14,7 @@ class JwtUtilTest {
 
     private static final String SECRET = "12345678901234567890123456789012";
     private static final String VALID_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjF9.ZZ3CUl0jxeLGvQ1Js5nG2Ty5qGTlqai5ubDMXZOdaDk";
-    private static final String INVALID_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjF9.ZZ3CUl0jxeLGvQ1Js5nG2Ty5qGTlqai5ubDMXZOdaDa";
+    private static final String INVALID_TOKEN = VALID_TOKEN + "...";
 
     private JwtUtil jwtUtil;
 

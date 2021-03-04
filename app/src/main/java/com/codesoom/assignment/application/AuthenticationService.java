21389ba528f39f2c5badd9187669a70c8dafa99e
wenodev <logicalstark1105@gmail.com>
2021-03-04 20:37:59 +0900

    fix: hmacShaKeyFor을 사용하라

--- app/src/main/java/com/codesoom/assignment/application/AuthenticationService.java
+++ app/src/main/java/com/codesoom/assignment/application/AuthenticationService.java
@@ -5,13 +5,17 @@ import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
 
+import java.nio.charset.StandardCharsets;
 import java.security.Key;
 
 @Service
 public class AuthenticationService {
 
     public String login() {
-        Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+
+        String secret="12345678901234567890123456789012";
+
+        Key key = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
         return Jwts.builder()
                 .claim("userId", 1L)
                 .signWith(key)

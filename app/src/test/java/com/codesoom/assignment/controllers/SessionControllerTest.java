327a4dd4735193a87527864dd42bcd9d99891ebf
developerOlive <chloehiho@gmail.com>
2021-03-04 19:24:12 +0900

    Refactor : sessionControllerTest 테스트코드 추가

--- app/src/test/java/com/codesoom/assignment/controllers/SessionControllerTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/SessionControllerTest.java
@@ -2,19 +2,25 @@ package com.codesoom.assignment.controllers;
 
 import com.codesoom.assignment.application.AuthenticationService;
 import com.codesoom.assignment.dto.UserLoginData;
+import com.codesoom.assignment.errors.AuthenticationFailException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.containsString;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(SessionController.class)
@@ -23,21 +29,44 @@ class SessionControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
     @MockBean
     private AuthenticationService authenticationService;
 
     private UserLoginData userLoginData;
 
     @BeforeEach
-    void setup() {
-        given(authenticationService.login(userLoginData)).willReturn("a.b.c");
+    void setUp() {
+        userLoginData = UserLoginData.builder()
+                .email("olive@gmail.com")
+                .password("password1234")
+                .build();
     }
 
-    @Test
-    void login() throws Exception {
-        mockMvc.perform(post("/session"))
-                .andExpect(status().isCreated())
-                .andExpect(content().string(containsString(".")));
-    }
+    @Nested
+    @DisplayName("POST 요청은")
+    class Describe_POST {
+        @Nested
+        @DisplayName("유효한 정보가 주어지면")
+        class Context_with_valid_data {
+            @BeforeEach
+            void setUp() {
+                given(authenticationService.login(any(UserLoginData.class)))
+                        .willReturn("a.b.c");
+            }
 
+            @DisplayName("201코드와 토큰을 응답한다.")
+            @Test
+            void it_responds_201_and_token() throws Exception {
+                mockMvc.perform(post("/session")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(userLoginData)))
+                        .andExpect(status().isCreated())
+                        .andExpect(jsonPath("accessToken").exists())
+                        .andExpect(content().string(containsString(".")));
+            }
+        }
+    }
 }

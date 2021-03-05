eb573cd9963dd9386fa39ea935baf3e44d72db63
thkim <thkim2017@gmail.com>
2021-03-05 19:50:56 +0900

    Add a test for JwtUtil Add a test for encode() in JwtUtil

---
+++ app/src/test/java/com/codesoom/assignment/utils/JwtUtilTest.java
@@ -0,0 +1,27 @@
+package com.codesoom.assignment.utils;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class JwtUtilTest {
+    private static final String SECRET = "12345678901234567890123456789012";
+
+    private static final String TOKEN = "eyJhbGciOiJIUzI1NiJ9." +
+            "eyJ1c2VySWQiOjF9.ZZ3CUl0jxeLGvQ1Js5nG2Ty5qGTlqai5ubDMXZOdaDk";
+
+    private JwtUtil jwtUtil;
+
+    @BeforeEach
+    void setUp() {
+        jwtUtil = new JwtUtil(SECRET);
+    }
+
+    @Test
+    void encode() {
+        String token = jwtUtil.encode(1L);
+
+        assertThat(token).isEqualTo(TOKEN);
+    }
+}

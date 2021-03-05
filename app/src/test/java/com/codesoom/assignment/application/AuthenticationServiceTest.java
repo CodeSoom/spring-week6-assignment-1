e1b55768acf396b35473ad9c02e33ecd7f81eaae
wenodev <logicalstark1105@gmail.com>
2021-03-05 21:52:58 +0900

    test: vaildToken을 테스트하여 성공하라

--- app/src/test/java/com/codesoom/assignment/application/AuthenticationServiceTest.java
+++ app/src/test/java/com/codesoom/assignment/application/AuthenticationServiceTest.java
@@ -10,6 +10,8 @@ class AuthenticationServiceTest {
 
     private AuthenticationService authenticationService;
     private static final String SECRET ="12345678901234567890123456789010";
+    private static final String VALID_TOKEN= "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjF9.neCsyNLzy3lQ4o2yliotWT06FwSGZagaHpKdAkjnGGw";
+    private static final String INVALID_TOKEN= "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjF9.neCsyNLzy3lQ4o2yliotWT06FwSGZagaHpKdAkjnGG0";
 
 
     @BeforeEach
@@ -21,7 +23,13 @@ class AuthenticationServiceTest {
     @Test
     void login(){
         String accessToken = authenticationService.login();
-        assertThat(accessToken).contains(".");
+        assertThat(accessToken).isEqualTo(VALID_TOKEN);
     }
 
+    @Test
+    void parseTokenWithValid(){
+        Long userId = authenticationService.parseToken(VALID_TOKEN);
+        assertThat(userId).isEqualTo(1L);
+    }
+    
 }

9f0c09cd84c6f2e983957bf8888673c4d16b20cc
newoo <newoo4297@naver.com>
2021-03-06 17:07:54 +0900

    유효하지 않은 비밀번호 예외 처리할 수 있는 ControllerAdvice 추가

--- app/src/main/java/com/codesoom/assignment/controllers/ControllerErrorAdvice.java
+++ app/src/main/java/com/codesoom/assignment/controllers/ControllerErrorAdvice.java
@@ -1,10 +1,7 @@
 package com.codesoom.assignment.controllers;
 
 import com.codesoom.assignment.dto.ErrorResponse;
-import com.codesoom.assignment.errors.InvalidAccessTokenException;
-import com.codesoom.assignment.errors.ProductNotFoundException;
-import com.codesoom.assignment.errors.UserEmailDuplicationException;
-import com.codesoom.assignment.errors.UserNotFoundException;
+import com.codesoom.assignment.errors.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -32,9 +29,15 @@ public class ControllerErrorAdvice {
         return new ErrorResponse("User's email address is already existed");
     }
 
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(InvalidPasswordException.class)
+    public ErrorResponse handleInvalidPassword() {
+        return new ErrorResponse("Password is wrong");
+    }
+
     @ResponseStatus(HttpStatus.UNAUTHORIZED)
     @ExceptionHandler(InvalidAccessTokenException.class)
-    public void handleInvalidAccessTokenException() {
+    public void handleInvalidAccessToken() {
         //
     }
 }

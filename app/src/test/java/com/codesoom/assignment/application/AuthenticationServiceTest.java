f229339b77b24c44995e54e826715a8b6e89efbd
newoo <newoo4297@naver.com>
2021-03-06 11:27:23 +0900

    이메일 또는 비밀번호가 유효하지 않을 때의 테스트 코드 추가

--- app/src/test/java/com/codesoom/assignment/application/AuthenticationServiceTest.java
+++ app/src/test/java/com/codesoom/assignment/application/AuthenticationServiceTest.java
@@ -4,6 +4,7 @@ import com.codesoom.assignment.domain.User;
 import com.codesoom.assignment.domain.UserRepository;
 import com.codesoom.assignment.dto.AccountData;
 import com.codesoom.assignment.errors.InvalidAccessTokenException;
+import com.codesoom.assignment.errors.UserNotFoundException;
 import com.codesoom.assignment.utils.JwtUtil;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -21,8 +22,10 @@ class AuthenticationServiceTest {
             = "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjF9.ZZ3CUl0jxeLGvQ1Js5nG2Ty5qGTlqai5ubDMXZOdaDk";
     final String invalidToken
             = "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjF9.ZZ3CUl0jxeLGvQ1Js5nG2Ty5qGTlqai5ubDMXZOdaD0";
-    final String givenEmail = "newoo4297@codesoom.com";
-    final String givenPassword = "1234567890";
+    final String givenSavedEmail = "newoo4297@codesoom.com";
+    final String givenUnsavedEmail = "law@codesoom.com";
+    final String givenValidPassword = "1234567890";
+    final String givenInvalidPassword = "12345";
 
     private AuthenticationService authenticationService;
 
@@ -35,23 +38,43 @@ class AuthenticationServiceTest {
 
         authenticationService = new AuthenticationService(jwtUtil, userRepository);
 
-        given(userRepository.findByEmail(givenEmail))
+        given(userRepository.findByEmail(givenSavedEmail))
                 .willReturn(
                         Optional.of(User.builder()
                                 .id(1L)
-                                .email(givenEmail)
+                                .email(givenSavedEmail)
                                 .name("Tester")
-                                .password(givenPassword)
+                                .password(givenValidPassword)
                                 .build())
                 );
     }
 
     @Test
-    void login() {
-        AccountData accountData = AccountData.builder().email(givenEmail).password(givenPassword).build();
+    void loginWithValidAccountData() {
+        AccountData accountData = AccountData.builder().email(givenSavedEmail).password(givenValidPassword).build();
         String accessToken = authenticationService.login(accountData);
 
         assertThat(accessToken).isEqualTo(validToken);
+    }
+
+    @Test
+    void loginWithInvalidAccountData() {
+        // With unsaved email
+        AccountData accountDataWithUnsavedEmail =
+                AccountData.builder()
+                .email(givenUnsavedEmail)
+                .password(givenValidPassword)
+                .build();
+        assertThatThrownBy(
+                () -> authenticationService.login(accountDataWithUnsavedEmail)
+        ).isInstanceOf(UserNotFoundException.class);
+
+        // With invalid password
+        AccountData accountDataWithInvalidPassword =
+                AccountData.builder().email(givenSavedEmail).password(givenInvalidPassword).build();
+        assertThatThrownBy(
+                () -> authenticationService.login(accountDataWithInvalidPassword)
+        ).isInstanceOf(InvalidPasswordException.class);
     }
 
     @Test

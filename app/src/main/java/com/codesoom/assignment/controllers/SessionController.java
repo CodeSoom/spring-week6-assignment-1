231c7ae60df35e87b03083598c9da2cece136c7a
Las <haho013039@gmail.com>
2021-03-07 15:40:17 +0900

    Set CQRS setting to session controller

--- app/src/main/java/com/codesoom/assignment/controllers/SessionController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/SessionController.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/session")
 public class SessionController {
     private final AuthenticationService authenticationService;

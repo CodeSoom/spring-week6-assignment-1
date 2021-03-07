e25ecfff8c955a236256f364ab3a086a805ca0cc
Lee Dong Wook <dwlee@crscube.io>
2021-03-07 14:22:00 +0900

    TEST: add login test of AuthenticationService

--- app/src/test/java/com/codesoom/assignment/application/AuthenticationServiceTest.java
+++ app/src/test/java/com/codesoom/assignment/application/AuthenticationServiceTest.java
@@ -1,6 +1,9 @@
 package com.codesoom.assignment.application;
 
+import com.codesoom.assignment.domain.User;
+import com.codesoom.assignment.domain.UserRepository;
 import com.codesoom.assignment.errors.InvalidAccessTokenException;
+import com.codesoom.assignment.errors.LoginFailException;
 import com.codesoom.assignment.utils.JwtUtil;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -8,6 +11,11 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+import java.util.Optional;
 
 class AuthenticationServiceTest {
     private static final String SECRET = "12345678901234567890123456789012";
@@ -21,18 +29,46 @@ class AuthenticationServiceTest {
 
     private AuthenticationService authenticationService;
 
+    private UserRepository userRepository = mock(UserRepository.class);
+
     @BeforeEach
     void setUp() {
         JwtUtil jwtUtil = new JwtUtil(SECRET);
-        authenticationService = new AuthenticationService(jwtUtil);
+        authenticationService = new AuthenticationService(jwtUtil, userRepository);
+
+        User user = User.builder()
+            .password("test")
+            .build();
+        given(userRepository.findByEmail("tester@example.com")).willReturn(Optional.of(user));
     }
 
     @DisplayName("서비스에 로그인을 요청하면, 유효한 토큰을 반환한다.")
     @Test
-    void login() {
-        String accessToken = authenticationService.login();
+    void loginWithRightEmailAndPassword() {
+        String accessToken = authenticationService
+            .login("tester@example.com", "test");
 
         assertThat(accessToken).isEqualTo(VALID_TOKEN);
+
+        verify(userRepository).findByEmail("tester@example.com");
+    }
+
+    @DisplayName("서비스에 올바르지 않은 이메일로 로그인을 요청하면, 예외를 던진다.")
+    @Test
+    void loginWithWrongEmail() {
+        assertThatThrownBy(() -> authenticationService.login("badguy@example.com", "test"))
+            .isInstanceOf(LoginFailException.class);
+
+        verify(userRepository).findByEmail("badguy@example.com");
+    }
+
+    @DisplayName("서비스에 올바르지 않은 패스워드로 로그인을 요청하면, 예외를 던진다.")
+    @Test
+    void loginWithWrongPassword() {
+        assertThatThrownBy(() -> authenticationService.login("tester@example.com", "xxx"))
+            .isInstanceOf(LoginFailException.class);
+
+        verify(userRepository).findByEmail("tester@example.com");
     }
 
     @DisplayName("서비스에 값이 유효한 토큰 분석을 요청 하면, 유저 아이디를 반환한다.")

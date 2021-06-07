efa90f99c17a476170181b8aa9fe4f82d964f1a1
Changsu Im <imcxsu@gmail.com>
2021-06-07 22:07:32 +0900

    Add app test for test coverage

--- app/src/test/java/com/codesoom/assignment/AppTest.java
+++ app/src/test/java/com/codesoom/assignment/AppTest.java
@@ -1,5 +1,18 @@
 package com.codesoom.assignment;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("App 클래스")
 class AppTest {
-    // TODO: 언젠가 필요하면 다시 돌아온다. - I'll be back!
+
+    @Test
+    @DisplayName("main 메서드는 스프링 애플리케이션을 실행한다")
+    void springApplicationContextTest() {
+        String[] args = new String[]{
+                "--spring.main.banner-mode=off",
+                "--logging.level.root=ERROR"
+        };
+        App.main(args);
+    }
 }

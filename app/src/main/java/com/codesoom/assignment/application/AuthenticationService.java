363a4ab11e5b4e304912dc1dd27b6617daa08734
Joowan Kim <rlawndhks217@gmail.com>
2021-06-09 08:05:04 +0900

    Add JavaDocs for controller & service about session

--- app/src/main/java/com/codesoom/assignment/application/AuthenticationService.java
+++ app/src/main/java/com/codesoom/assignment/application/AuthenticationService.java
@@ -1,22 +1,25 @@
 package com.codesoom.assignment.application;
 
 import com.codesoom.assignment.utils.JwtUtil;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
 
-import java.security.Key;
-
+/**
+ * 자원에 접근할 수 있는 권한을 관리합니다.
+ */
 @Service
 public class AuthenticationService {
 
-    private JwtUtil jwtUtil;
+    private final JwtUtil jwtUtil;
 
     public AuthenticationService(JwtUtil jwtUtil) {
         this.jwtUtil = jwtUtil;
     }
 
+    /**
+     * 자원에 접근할 수 있는 권한을 발급하고 반환합니다.
+     *
+     * @return 자원에 접근할 수 있는 권한을 부여하는 토큰
+     */
     public String login() {
         return this.jwtUtil.encode(1L);
     }
--- app/src/main/java/com/codesoom/assignment/controllers/SessionController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/SessionController.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * 자원에 접근할 수 있는 권한에 대한 요청을 처리합니다.
+ */
 @RestController
 @RequestMapping("/session")
 public class SessionController {
@@ -18,6 +21,11 @@ public class SessionController {
         this.authenticationService = authenticationService;
     }
 
+    /**
+     * 권한을 부여받기 위한 요청을 처리합니다.
+     *
+     * @return 권한을 부여하는 토큰을 포함한 응답
+     */
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public SessionResponseData login() {

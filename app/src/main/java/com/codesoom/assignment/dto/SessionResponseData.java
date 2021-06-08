ada6cd0d72ad371fc8c76a7bad281227ac982fe0
sedin <sedin@kakao.com>
2021-06-08 17:33:19 +0900

    Feat: SessionController 작성 및 관련 Dto 추가
    - SessionController 작성
    - 로그인에 필요한 요청과 응답에 필요한 Dto 작성

---
+++ app/src/main/java/com/codesoom/assignment/dto/SessionResponseData.java
@@ -0,0 +1,14 @@
+package com.codesoom.assignment.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+/**
+ * 엑세스 토큰 응답객체 입니다.
+ */
+@Getter
+@Builder
+public class SessionResponseData {
+
+    private String accessToken;
+}

6e3c2de23fb1824d9637d2f8fe0bd75095fa96dc
Joowan Kim <rlawndhks217@gmail.com>
2021-06-08 20:46:27 +0900

    Add a feature for session

---
+++ app/src/main/java/com/codesoom/assignment/application/AuthenticationService.java
@@ -0,0 +1,23 @@
+package com.codesoom.assignment.application;
+
+import com.codesoom.assignment.utils.JwtUtil;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+
+@Service
+public class AuthenticationService {
+
+    private JwtUtil jwtUtil;
+
+    public AuthenticationService(JwtUtil jwtUtil) {
+        this.jwtUtil = jwtUtil;
+    }
+
+    public String login() {
+        return this.jwtUtil.encode(1L);
+    }
+}

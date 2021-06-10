d0c7bed055d164578c1a4f1ac824c45fdc502f66
youdongkwan <dkyou7@naver.com>
2021-06-10 23:44:46 +0900

    decode test

--- app/src/main/java/com/codesoom/assignment/utils/JwtUtil.java
+++ app/src/main/java/com/codesoom/assignment/utils/JwtUtil.java
@@ -1,5 +1,6 @@
 package com.codesoom.assignment.utils;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
@@ -9,6 +10,7 @@ import java.security.Key;
 
 @Component
 public class JwtUtil {
+
 
     private final Key key;
 
@@ -20,5 +22,13 @@ public class JwtUtil {
 
 
         return Jwts.builder().signWith(key).claim("userId", 1L).compact();
+    }
+
+    public Claims customDecode(String accessToken) {
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(accessToken)
+                .getBody();
     }
 }
--- app/src/test/java/com/codesoom/assignment/utils/JwtUtilTest.java
+++ app/src/test/java/com/codesoom/assignment/utils/JwtUtilTest.java
@@ -1,7 +1,34 @@
 package com.codesoom.assignment.utils;
 
+import io.jsonwebtoken.Claims;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class JwtUtilTest {
 
+    private JwtUtil jwtUtil;
+    private static final String SECRET = "12345678901234567890123456789010";
+    private static final String ACCESS_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjF9.neCsyNLzy3lQ4o2yliotWT06FwSGZagaHpKdAkjnGGw";
+
+    @BeforeEach
+    public void setUp(){
+        jwtUtil = new JwtUtil(SECRET);
+    }
+
+    @Test
+    public void encode(){
+        String accessToken = jwtUtil.customEncode(1L);
+
+        assertThat(accessToken).isEqualTo(ACCESS_TOKEN);
+    }
+
+    @Test
+    public void decode(){
+        Claims claims = jwtUtil.customDecode(ACCESS_TOKEN);
+
+        assertThat(claims.get("userId",Long.class)).isEqualTo(1L);
+    }
 }

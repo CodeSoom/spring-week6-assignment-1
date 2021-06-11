50be79d3da2f10c8016fd77b83c1b3ba272b38ee
Joowan Kim <rlawndhks217@gmail.com>
2021-06-11 18:24:11 +0900

    Resolve cross origin at SessionController

--- app/src/main/java/com/codesoom/assignment/controllers/SessionController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/SessionController.java
@@ -13,6 +13,7 @@ import javax.validation.Valid;
  */
 @RestController
 @RequestMapping("/session")
+@CrossOrigin
 public class SessionController {
 
     private AuthenticationService authenticationService;

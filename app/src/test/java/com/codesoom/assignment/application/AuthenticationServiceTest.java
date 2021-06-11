e683d0673e9e5bf51ddee358d737d2788e41ec38
EHOTO <ktaewoong46@gmail.com>
2021-06-11 16:30:22 +0900

    `AuthenticationService` 테스트 리팩토링.
    - `@Nested` 적용.

--- app/src/test/java/com/codesoom/assignment/application/AuthenticationServiceTest.java
+++ app/src/test/java/com/codesoom/assignment/application/AuthenticationServiceTest.java
@@ -3,6 +3,8 @@ package com.codesoom.assignment.application;
 import com.codesoom.assignment.errors.InvalidTokenException;
 import com.codesoom.assignment.utils.JwtUtil;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EmptySource;
@@ -12,8 +14,11 @@ import org.junit.jupiter.params.provider.ValueSource;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+@DisplayName("AuthenticationService 클래스")
 class AuthenticationServiceTest {
     private final String SECRET = "12345678901234567890123456789010";
+
+    private final Long VALID_TOKEN_USER_ID = 1L;
 
     private final String VALID_TOKEN = "eyJhbGciOiJIUzI1NiJ9." +
             "eyJ1c2VySWQiOjF9.neCsyNLzy3lQ4o2yliotWT06FwSGZagaHpKdAkjnGGw";
@@ -29,33 +34,44 @@ class AuthenticationServiceTest {
         authenticationService = new AuthenticationService(jwtUtil);
     }
 
-    @Test
-    void login() {
-        String accessToken = authenticationService.login(1L);
+    @Nested
+    @DisplayName("parseToken 메서드")
+    class DescribeLogin {
+        @Test
+        @DisplayName("회원 식별자에 따른 인증토큰을 반환한다")
+        void withUserId() {
+            String accessToken = authenticationService.login(VALID_TOKEN_USER_ID);
 
-        assertThat(accessToken).isEqualTo(VALID_TOKEN);
+            assertThat(accessToken).isEqualTo(VALID_TOKEN);
+        }
     }
 
-    @Test
-    void parseWithValidToken() {
-        Long userId = authenticationService.parseToken(VALID_TOKEN);
+    @Nested
+    @DisplayName("parseToken 메서드")
+    class DescribeParseToken {
+        @Test
+        @DisplayName("유효한 토큰이라면 회원 식별자를 반환한다")
+        void withValidToken() {
+            Long userId = authenticationService.parseToken(VALID_TOKEN);
 
-        assertThat(userId).isEqualTo(1L);
+            assertThat(userId).isEqualTo(VALID_TOKEN_USER_ID);
+        }
+
+        @Test
+        @DisplayName("잘못된 토큰이라면 예외를 발생시킨다")
+        void withInvalidToken() {
+            assertThatThrownBy(() -> authenticationService.parseToken(INVALID_TOKEN))
+                    .isInstanceOf(InvalidTokenException.class);
+        }
+
+        @ParameterizedTest(name = "{index}: <{0}>")
+        @NullSource
+        @EmptySource
+        @ValueSource(strings = {" "})
+        @DisplayName("빈 토큰이라면 예외를 발생시킨다")
+        void withEmptyToken(String input) {
+            assertThatThrownBy(() -> authenticationService.parseToken(input))
+                    .isInstanceOf(InvalidTokenException.class);
+        }
     }
-
-    @Test
-    void parseWithInvalidToken() {
-        assertThatThrownBy(() -> authenticationService.parseToken(INVALID_TOKEN))
-                .isInstanceOf(InvalidTokenException.class);
-    }
-
-    @ParameterizedTest(name = "{index}: [{0}]")
-    @NullSource
-    @EmptySource
-    @ValueSource(strings = {" "})
-    void parseWithEmptyToken(String input) {
-        assertThatThrownBy(() -> authenticationService.parseToken(input))
-                .isInstanceOf(InvalidTokenException.class);
-    }
-
 }

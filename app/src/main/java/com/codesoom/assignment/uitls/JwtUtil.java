2c1a3502664d60ebf8ddc766427ee1425d8adf33
Flying-Fisherman <ghdud7037@gmail.com>
2021-06-11 23:01:45 +0900

    JwtUtilTest 수정

--- app/src/main/java/com/codesoom/assignment/uitls/JwtUtil.java
+++ app/src/main/java/com/codesoom/assignment/uitls/JwtUtil.java
@@ -29,6 +29,5 @@ public class JwtUtil {
                 .build()
                 .parseClaimsJws(token)
                 .getBody();
-
     }
 }
--- app/src/test/java/com/codesoom/assignment/uitls/JwtUtilTest.java
+++ app/src/test/java/com/codesoom/assignment/uitls/JwtUtilTest.java
@@ -1,42 +1,74 @@
 package com.codesoom.assignment.uitls;
 
-
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.security.SignatureException;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+@DisplayName("JwtUtil 클래스")
 class JwtUtilTest {
     private JwtUtil jwtUtil;
+
     private static final String SECRET = "gkskenfdutkeidktjeifnturldksiekt";
-    private static final String VALID_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjF9.5vWKIu_dVvDx0_K39RSWmrfkyNNsae0lJE3HauIMI5I";
-    private static final String INVALID_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjF9.5vWKIu_dVvDx0_K39RSWmrfkyNNsae0lJE3HauIMI51";
+    private static final String TEST_EMAIL = "test@test.com";
+    private static final String VALID_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6InRlc3RAdGVzdC5jb20ifQ.4HbNGIW9PaXCVuALXZxuqF__XkQYD6BpXndE7Cll4yU";
+    private static final String INVALID_TOKEN = VALID_TOKEN.replace("5", "6");
 
     @BeforeEach
     void setUp() {
         jwtUtil = new JwtUtil(SECRET);
     }
 
-    @Test
-    void encode() {
-        String token = jwtUtil.encode("a");
+    @Nested
+    @DisplayName("encode 메서드는")
+    class Describe_encode {
 
-        assertThat(token).isEqualTo(VALID_TOKEN);
+        @Nested
+        @DisplayName("적합한 정보를 받으면")
+        class Context_with_valid_source {
+
+            @Test
+            @DisplayName("토큰을 생성합니다.")
+            void it_encode_right_token() {
+                String token = jwtUtil.encode(TEST_EMAIL);
+
+                assertThat(token).isEqualTo(VALID_TOKEN);
+            }
+        }
     }
 
-    @Test
-    void decode() {
-        Claims claims = jwtUtil.decode(VALID_TOKEN);
+    @Nested
+    @DisplayName("decode 메서드는")
+    class Describe_decode {
 
-        assertThat(claims.get("userId", Long.class)).isEqualTo(1L);
-    }
+        @Nested
+        @DisplayName("적합한 토큰을 받으면")
+        class Context_with_valid_token {
 
-    @Test
-    void decodeWithInvalidToken() {
-        assertThatThrownBy(() -> jwtUtil.decode(INVALID_TOKEN))
-                .isInstanceOf(SignatureException.class);
+            @Test
+            @DisplayName("토큰을 해독한 값을 리턴합니다.")
+            void it_return_decoded_data() {
+                Claims claims = jwtUtil.decode(VALID_TOKEN);
+
+                assertThat(claims.get("email")).isEqualTo(TEST_EMAIL);
+            }
+        }
+
+        @Nested
+        @DisplayName("적합하지 않은 토큰을 받으면")
+        class Context_with_invalid_token {
+
+            @Test
+            @DisplayName("SignatureExcepiton을 던집니다.")
+            void it_return_SignatureException() {
+                assertThatThrownBy(() -> jwtUtil.decode(INVALID_TOKEN))
+                        .isInstanceOf(SignatureException.class);
+            }
+        }
     }
 }

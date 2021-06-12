f616d256ce254af44f90bb31836bb6abc2a9799a
Woong <37302935+TaeWoongKim2@users.noreply.github.com>
2021-06-12 22:30:39 +0900

    테스트 코드 리팩토링
    - `SessionController` 테스트 리팩토링.
      - `@Nested` 적용.
      - 이메일에 해당하는 회원이 있을 경우 없을 경우에 대한 테스트 추가.
      - 패스워드가 틀린 경우에 대한 테스트 추가.
    
    - UserService 테스트 리팩토링.
      - `@Nested` 적용.
      - Test coverage 100% 만들기.

--- app/src/main/java/com/codesoom/assignment/errors/UserNotFoundException.java
+++ app/src/main/java/com/codesoom/assignment/errors/UserNotFoundException.java
@@ -1,10 +1,6 @@
 package com.codesoom.assignment.errors;
 
 public class UserNotFoundException extends RuntimeException {
-    public UserNotFoundException() {
-        super("User not found.");
-    }
-
     public UserNotFoundException(Long id) {
         super("User not found by id: " + id);
     }
--- app/src/test/java/com/codesoom/assignment/controllers/SessionControllerTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/SessionControllerTest.java
@@ -3,7 +3,11 @@ package com.codesoom.assignment.controllers;
 import com.codesoom.assignment.application.AuthenticationService;
 import com.codesoom.assignment.application.UserService;
 import com.codesoom.assignment.domain.User;
+import com.codesoom.assignment.errors.UserNotFoundException;
+import com.codesoom.assignment.errors.UserPasswordMismatchException;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -13,16 +17,23 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@DisplayName("SessionController 테스트")
 @WebMvcTest(SessionController.class)
 class SessionControllerTest {
     private static final Long ID = 1L;
+
     private static final String EMAIL = "sample@google.com";
+    private static final String UNKNOWN_EMAIL = "UNKNOWN"+ EMAIL;
+
     private static final String PASSWORD = "sample_password";
+    private static final String INVALID_PASSWORD = "INVALID" + PASSWORD;
+
     private static final String NAME = "sample";
 
     @Autowired
@@ -36,7 +47,7 @@ class SessionControllerTest {
 
     @BeforeEach
     void setup() {
-        given(userService.findUserByEmailPassword(any(String.class), any(String.class)))
+        given(userService.findUserByEmailPassword(eq(EMAIL), eq(PASSWORD)))
                 .willReturn(User.builder()
                         .id(ID)
                         .email(EMAIL)
@@ -44,17 +55,60 @@ class SessionControllerTest {
                         .name(NAME)
                         .build());
 
+        given(userService.findUserByEmailPassword(eq(UNKNOWN_EMAIL), eq(PASSWORD)))
+                .willThrow(new UserNotFoundException(UNKNOWN_EMAIL));
+
+        given(userService.findUserByEmailPassword(eq(EMAIL), eq(INVALID_PASSWORD)))
+                .willThrow(new UserPasswordMismatchException());
+
         given(authenticationService.login(ID)).willReturn("aaa.bbb.ccc");
     }
 
-    @Test
-    void login() throws Exception {
-        mockMvc.perform(post("/session")
-                    .accept(MediaType.APPLICATION_JSON)
-                    .contentType(MediaType.APPLICATION_JSON)
-                    .content(String.format("{\"email\":\"%s\"," +
-                            "\"password\":\"%s\"}", EMAIL, PASSWORD)))
-                .andExpect(status().isCreated())
-                .andExpect(content().string(containsString(".")));
+    @Nested
+    @DisplayName("login 메서드")
+    class DescribeFindUserByEmailPassword {
+        @Nested
+        @DisplayName("이메일에 해당하는 회원이 있을 경우")
+        class ContextWithExistedUserEmail {
+            @Test
+            @DisplayName("비밀번호가 일치한다면, ACCESS TOKEN과 CREATED 응답코드를 반환한다")
+            void itReturnsCreatedWithToken() throws Exception {
+                mockMvc.perform(post("/session")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(String.format("{\"email\":\"%s\"," +
+                                "\"password\":\"%s\"}", EMAIL, PASSWORD)))
+                        .andExpect(status().isCreated())
+                        .andExpect(content().string(containsString(".")));
+            }
+
+            @Test
+            @DisplayName("비밀번호가 틀리다면, BAD REQUEST를 응답한다")
+            void itReturnsNotFound() throws Exception {
+                mockMvc.perform(post("/session")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(String.format("{\"email\":\"%s\"," +
+                                "\"password\":\"%s\"}", EMAIL, INVALID_PASSWORD)))
+                        .andExpect(status().isBadRequest());
+            }
+        }
+
+        @Nested
+        @DisplayName("이메일에 해당하는 회원이 없을 경우")
+        class ContextWithNotExistedUserEmail {
+            @Test
+            @DisplayName("NOT FOUND를 응답한다")
+            void itReturnsNotFound() throws Exception {
+                mockMvc.perform(post("/session")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(String.format("{\"email\":\"%s\"," +
+                                "\"password\":\"%s\"}", UNKNOWN_EMAIL, PASSWORD)))
+                        .andExpect(status().isNotFound());
+            }
+        }
     }
+
+
 }

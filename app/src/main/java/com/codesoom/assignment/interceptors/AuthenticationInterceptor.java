d62279956e7e4f74357cd91862efe99350b497bb
Lim <40200760+tbnsok40@users.noreply.github.com>
2021-06-13 16:58:44 +0900

    AuthenticationInterceptor 생성

--- app/src/main/java/com/codesoom/assignment/interceptors/AuthenticationInterceptor.java
+++ app/src/main/java/com/codesoom/assignment/interceptors/AuthenticationInterceptor.java
@@ -1,2 +1,56 @@
-package com.codesoom.assignment.interceptors;public class AuthenticationInterceptor {
+package com.codesoom.assignment.interceptors;
+
+import com.codesoom.assignment.application.AuthenticationService;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component
+public class AuthenticationInterceptor implements HandlerInterceptor {
+
+    private AuthenticationService authenticationService;
+
+    public AuthenticationInterceptor(AuthenticationService authenticationService) {
+        this.authenticationService = authenticationService;
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest request,
+                             HttpServletResponse response,
+                             Object handler)
+            throws Exception {
+
+        String path = request.getRequestURI();
+        if (!path.equals("/products")) {
+            return true;
+        }
+
+        String method = request.getMethod();
+        if (method.equals("GET")) {
+            return true;
+        }
+
+        return doAuthentication(request, response);
+    }
+
+    public boolean doAuthentication(
+            HttpServletRequest request,
+            HttpServletResponse response)
+            throws Exception {
+
+        String authorization = request.getHeader("Authorization");
+
+        if (authorization == null) {
+            response.sendError(HttpStatus.UNAUTHORIZED.value());
+            return false;
+        }
+
+        String accessToken = authorization.substring("Bearer".length());
+        authenticationService.parseToken(accessToken);
+
+        return true;
+    }
 }

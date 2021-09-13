8e027739a387cc3cda177760c5d22efddd44fc95
bloomspes <bloomspes@gmail.com>
2021-09-13 17:55:56 +0900

    add : ToDo 정리

---
+++ app/src/main/java/com/codesoom/assignment/controllers/SessionController.java
@@ -0,0 +1,13 @@
+// REST API 작성하기
+// 1. CREATE -> 로그인 -> Token 발행
+// 2. READ (w/ Token) -> 세션 정보 전달 -> 세션 정보가 유효한가?, 유저 정보
+// 3. UPDATE (w/ Token) -> Token 재발행
+// 4. DELETE -> 로그아웃 -> Token 무효화
+
+// session은 여러개가 존재할 수 없으므로, 단수형을 사용한다.
+
+
+package com.codesoom.assignment.controllers;
+
+public class SessionController {
+}

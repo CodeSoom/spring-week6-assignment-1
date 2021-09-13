ef5e9e4464ab8d428868ddc5ee7b4b1722076386
bloomspes <bloomspes@gmail.com>
2021-09-13 19:42:46 +0900

    add : WebMvCTest 어노테이션의 단점을 보완하라.
    Service에서 생기는 에러를 볼 수 없기 때문에
    아래와 같이 테스트 파일에 해당 유효성 검사를 설계한다.
    
    - AuthenticationService의 MockBean 생성
    - MockBean 생성 이후 로그인 세션을 받을때 "a,b,c" 토큰을 대조할 수 있도록 BeforeEach 어노테이션을 SetUp 메소드에 선언.

--- app/src/test/java/com/codesoom/assignment/controllers/SessionControllerTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/SessionControllerTest.java
@@ -1,11 +1,16 @@
 package com.codesoom.assignment.controllers;
 
+import com.codesoom.assignment.application.AuthenticationService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.containsString;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -14,6 +19,14 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class SessionControllerTest {
     @Autowired
     private MockMvc mockMvc;
+
+    @MockBean
+    private AuthenticationService authenticationService;
+
+    @BeforeEach
+    void setUp() {
+        given(authenticationService.login()).willReturn("a.b.c");
+    }
 
     @Test
     void login() throws Exception {

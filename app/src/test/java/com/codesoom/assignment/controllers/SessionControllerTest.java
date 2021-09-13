efbbaccf0a99576e518dbf9f3a0e1f1b85963441
bloomspes <bloomspes@gmail.com>
2021-09-13 18:16:19 +0900

    add : POST /session 201 status 유효성 검사 작성중

--- app/src/test/java/com/codesoom/assignment/controllers/SessionControllerTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/SessionControllerTest.java
@@ -1,8 +1,22 @@
 package com.codesoom.assignment.controllers;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
 
-@WebMvcTest
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(SessionController.class)
 class SessionControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void login() throws Exception {
+        mockMvc.perform(post("/session"))
+                .andExpect(status().isCreated());
+    }
 
 }

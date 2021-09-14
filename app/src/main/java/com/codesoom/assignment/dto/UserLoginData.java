02cff9b3fe3f8f5905f683453fc5ed78f0092409
wjy5446 <wjy5446@gmail.com>
2021-09-14 23:35:16 +0900

    feat) UserLoginData dto 추가

--- app/src/main/java/com/codesoom/assignment/dto/UserLoginData.java
+++ app/src/main/java/com/codesoom/assignment/dto/UserLoginData.java
@@ -1,2 +1,23 @@
-package com.codesoom.assignment.dto;public class UserLoginData {
+package com.codesoom.assignment.dto;
+
+import com.github.dozermapper.core.Mapping;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserLoginData {
+    @NotBlank
+    @Mapping("email")
+    private String email;
+
+    @NotBlank
+    @Mapping("password")
+    private String password;
 }

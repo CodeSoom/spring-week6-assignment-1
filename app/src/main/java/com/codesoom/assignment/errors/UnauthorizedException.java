b663c960dd086993f1045f5566d76b35e88ee74c
Sungeheon <elon4856@gmail.com>
2021-09-14 12:51:28 +0900

    fix: 오타를 수정하세요

--- app/src/main/java/com/codesoom/assignment/errors/UnauthorizedException.java
+++ app/src/main/java/com/codesoom/assignment/errors/UnauthorizedException.java
@@ -3,7 +3,7 @@ package com.codesoom.assignment.errors;
 public class UnauthorizedException extends RuntimeException {
 
     public UnauthorizedException(String accessToken) {
-        super("UNautorized :" + accessToken);
+        super("Unauthorized :" + accessToken);
     }
 
 }

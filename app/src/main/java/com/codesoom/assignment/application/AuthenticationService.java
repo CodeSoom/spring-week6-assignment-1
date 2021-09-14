dcd4e9fe37243d1b0dd87b1dc7c1922dcd79e777
bloomspes <bloomspes@gmail.com>
2021-09-14 20:18:32 +0900

    add : jsob web token 추가

--- app/src/main/java/com/codesoom/assignment/application/AuthenticationService.java
+++ app/src/main/java/com/codesoom/assignment/application/AuthenticationService.java
@@ -1,10 +1,15 @@
 package com.codesoom.assignment.application;
 
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
 
 @Service
 public class AuthenticationService {
     public String login() {
+        Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
         return "a.b.c";
     }
 }

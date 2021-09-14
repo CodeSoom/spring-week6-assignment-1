ae5e09252889c46aba70da7383d5ed706f33945a
wjy5446 <wjy5446@gmail.com>
2021-09-14 23:36:49 +0900

    feat) SessionController 추가

--- app/src/main/java/com/codesoom/assignment/controllers/SessionController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/SessionController.java
@@ -1,9 +1,14 @@
 package com.codesoom.assignment.controllers;
 
 import com.codesoom.assignment.application.AuthenticationService;
+import com.codesoom.assignment.dto.ErrorResponse;
 import com.codesoom.assignment.dto.SessionResponseData;
+import com.codesoom.assignment.dto.UserLoginData;
+import com.codesoom.assignment.errors.LoginNotMatchPasswordException;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,11 +24,17 @@ public class SessionController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    SessionResponseData login() {
-        String accessToken = authenticationService.login();
+    SessionResponseData login(@RequestBody UserLoginData userLoginData) {
+        String accessToken = authenticationService.login(userLoginData);
 
         return SessionResponseData.builder()
                 .accessToken(accessToken)
                 .build();
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(LoginNotMatchPasswordException.class)
+    ErrorResponse handleLoginNotMatchPasswordException() {
+        return new ErrorResponse("로그인 실패: 이메일과 비밀번호가 매치되지 않습니다.");
     }
 }

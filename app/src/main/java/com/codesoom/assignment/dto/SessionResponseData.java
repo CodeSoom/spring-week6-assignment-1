7d924eddfc1fe65b6c446f87c75a6bee6c2565ec
wjy5446 <wjy5446@gmail.com>
2021-09-14 23:55:12 +0900

    feat) SessionResponseData의 Getter 제거

--- app/src/main/java/com/codesoom/assignment/dto/SessionResponseData.java
+++ app/src/main/java/com/codesoom/assignment/dto/SessionResponseData.java
@@ -1,10 +1,8 @@
 package com.codesoom.assignment.dto;
 
 import lombok.Builder;
-import lombok.Getter;
 
-@Getter
 @Builder
 public class SessionResponseData {
-    private String accessToken;
+    private final String accessToken;
 }

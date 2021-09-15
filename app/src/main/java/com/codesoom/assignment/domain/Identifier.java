a33888b8f24911437a953f342c3aec9a14aafd96
catsbi <killfine1988@gmail.com>
2021-09-16 01:01:37 +0900

    feat: Identifier 식별자 인터페이스 분리(EmailSupplier, PasswordSupplier)

--- app/src/main/java/com/codesoom/assignment/domain/Identifier.java
+++ app/src/main/java/com/codesoom/assignment/domain/Identifier.java
@@ -3,9 +3,8 @@ package com.codesoom.assignment.domain;
 import java.io.Serializable;
 
 
-public interface Identifier extends Serializable {
-
-    String getEmail();
-
-    String getPassword();
+/**
+ * 식별자 정보를 관리한다.
+ */
+public interface Identifier extends Serializable, EmailSupplier, PasswordSupplier {
 }

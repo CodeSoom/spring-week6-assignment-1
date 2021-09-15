f72ec6de448d334f6aaefa4ac9cb7633f1130b3a
ytkim4558@naver.com <R5vSP6djLOpfxXF>
2021-09-15 13:30:40 +0900

    jwt 테스트 생성

---
+++ app/src/main/java/com/codesoom/assignment/utils/JwtUtil.java
@@ -0,0 +1,27 @@
+package com.codesoom.assignment.utils;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.security.Key;
+
+@Component
+public class JwtUtil {
+    private final Key key;
+
+    public JwtUtil(@Value("${jwt.secret}") String secret) {
+        key = Keys.hmacShaKeyFor(secret.getBytes());
+
+    }
+
+    public String encode(Long userId) {
+
+        return Jwts.builder()
+                .claim("userId", 1L)
+                .signWith(key)
+                .compact();
+    }
+}
--- app/src/test/java/com/codesoom/assignment/controllers/SessionControllerTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/SessionControllerTest.java
@@ -1,12 +1,20 @@
 package com.codesoom.assignment.controllers;
 
 import antlr.build.Tool;
+import com.codesoom.assignment.application.AuthenticationService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.Matchers.containsString;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(SessionController.class)
@@ -14,10 +22,19 @@ class SessionControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @MockBean
+    private AuthenticationService authenticationService;
+
+    @BeforeEach
+    void setUp() {
+        given(authenticationService.login()).willReturn("a.b.c");
+    }
+
     @Test
     void login() throws Exception {
         mockMvc.perform(post("/session"))
-                .andExpect(status().isCreated());
+                .andExpect(status().isCreated())
+                .andExpect(content().string(containsString(".")));
     }
 
 }

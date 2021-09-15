e7000e4456db81b4cb619fc145ab932d7c91d48e
wjy5446 <wjy5446@gmail.com>
2021-09-15 22:44:36 +0900

    feat) AuthenticationService 추가

--- app/src/main/java/com/codesoom/assignment/application/AuthenticationService.java
+++ app/src/main/java/com/codesoom/assignment/application/AuthenticationService.java
@@ -1,19 +1,39 @@
 package com.codesoom.assignment.application;
 
+import com.codesoom.assignment.domain.User;
+import com.codesoom.assignment.domain.UserRepository;
+import com.codesoom.assignment.dto.UserLoginData;
+import com.codesoom.assignment.errors.LoginNotMatchPasswordException;
+import com.codesoom.assignment.errors.UserNotFoundException;
 import com.codesoom.assignment.utils.JwtUtil;
+import com.github.dozermapper.core.Mapper;
 import org.springframework.stereotype.Service;
 
 
 @Service
 public class AuthenticationService {
 
-    private JwtUtil jwtUtil;
+    private final JwtUtil jwtUtil;
+    private final Mapper mapper;
+    private final UserRepository userRepository;
 
-    public AuthenticationService(JwtUtil jwtUtil) {
+    public AuthenticationService(JwtUtil jwtUtil, Mapper mapper, UserRepository userRepository) {
         this.jwtUtil = jwtUtil;
+        this.mapper = mapper;
+        this.userRepository = userRepository;
     }
 
-    public String login() {
-        return jwtUtil.encode(1L);
+    public String login(UserLoginData userLoginData) {
+
+        User findedUser = userRepository.findByEmail(userLoginData.getEmail())
+                .orElseThrow(UserNotFoundException::new);
+
+        if(!findedUser.authenticate(userLoginData.getPassword())) {
+            throw new LoginNotMatchPasswordException();
+        }
+
+        System.out.println(jwtUtil.encode(findedUser.getId()));
+
+        return jwtUtil.encode(findedUser.getId());
     }
 }

8ae619385667ebc35bee9243482d10c89f16c5d8
Sungeheon <elon4856@gmail.com>
2021-09-15 16:16:36 +0900

    docs: AuthenticationController에대한 javadoc을 작성하세요

--- app/src/main/java/com/codesoom/assignment/controllers/AuthenticationController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/AuthenticationController.java
@@ -1,8 +1,8 @@
 package com.codesoom.assignment.controllers;
 
 import com.codesoom.assignment.application.AuthenticationService;
-import com.codesoom.assignment.dto.UserLoginData;
 import com.codesoom.assignment.dto.SessionResponseData;
+import com.codesoom.assignment.dto.UserLoginData;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,6 +22,12 @@ public class AuthenticationController {
         this.authenticationService = authenticationService;
     }
 
+    /**
+     * 로그인 데이터를 바탕으로 인증 토큰 생성후 반환한다.
+     *
+     * @param loginData 로그인 데이터
+     * @return 인증토큰
+     */
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public SessionResponseData create(@RequestBody @Valid UserLoginData loginData) {

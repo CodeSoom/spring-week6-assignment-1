f1a613b37ff26f46c4740b466c6bf17a9022eb97
Sungeheon <elon4856@gmail.com>
2021-09-16 18:55:09 +0900

    docs: javadoc을 작성하세요

--- app/src/main/java/com/codesoom/assignment/utils/JwtUtil.java
+++ app/src/main/java/com/codesoom/assignment/utils/JwtUtil.java
@@ -10,6 +10,9 @@ import org.springframework.stereotype.Component;
 
 import java.security.Key;
 
+/**
+ * JWT토큰을 관리합니다.
+ */
 @Component
 public class JwtUtil {
     private Key key;
@@ -19,13 +22,25 @@ public class JwtUtil {
     }
 
 
+    /**
+     * JWT토큰을 생성하고 return합니다.
+     *
+     * @param userId 회원 식별자
+     * @return 생성한 토큰
+     */
     public String encode(Long userId) {
         return Jwts.builder()
                 .claim("userId", userId)
                 .signWith(key).compact();
     }
 
-
+    /**
+     * JWT토큰을 decode하고, decode한 정보를 return합니다.
+     *
+     * @param token 토큰
+     * @return decode한 정보
+     * @throws UnauthorizedException 유효하지 않은 토큰일 경우
+     */
     public Claims decode(String token) {
         if (token == null || token.isBlank()) {
             throw new UnauthorizedException(token);

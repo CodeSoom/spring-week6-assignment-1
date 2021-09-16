d17eac6f388eaaeb7c302ed99e04828d4f01e06c
daeungkim <daeungkim93@gmail.com>
2021-09-16 12:34:46 +0900

    User 테스트 대상 메서드 캡슐화
    User 테스트 대상 메서드 캡슐화
    -> 메서드 시그네처 변경에 대응하기 쉬우므로
    <description>

--- app/src/test/java/com/codesoom/assignment/domain/UserTest.java
+++ app/src/test/java/com/codesoom/assignment/domain/UserTest.java
@@ -1,5 +1,6 @@
 package com.codesoom.assignment.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -11,11 +12,47 @@ public final class UserTest {
     public static final String USER_EMAIL = "test@email.com";
     public static final String USER_NAME = "test";
     public static final String USER_PASSWORD = "test";
+    public static final String INVALID_PASSWORD = USER_PASSWORD + "invalid";
     public static final User USER = User.builder()
         .email(USER_EMAIL)
         .password(USER_PASSWORD)
         .name(USER_NAME)
         .build();
+
+    private User user;
+
+    private User subjectInit() {
+        return User.builder().build();
+    }
+
+    private void subjectChangeWith() {
+        user.changeWith(USER);
+    }
+
+    private void subjectDestroy() {
+        user.destroy();
+    }
+
+    private String subjectGetName() {
+        return user.getName();
+    }
+
+    private String subjectGetPassword() {
+        return user.getPassword();
+    }
+
+    private boolean subjectIsDeleted() {
+        return user.isDeleted();
+    }
+
+    private boolean subjectAuthenticate(final String password) {
+        return user.authenticate(password);
+    }
+
+    @BeforeEach
+    private void beforeEach() {
+        user = subjectInit();
+    }
 
     @Nested
     @DisplayName("changeWith 메서드는")
@@ -23,12 +60,10 @@ public final class UserTest {
         @Test
         @DisplayName("사용자의 이름과 비밀번호를 업데이트한다.")
         public void it_updates_user_name_and_password() {
-            final User user = User.builder().build();
+            subjectChangeWith();
 
-            user.changeWith(USER);
-
-            assertThat(user.getName()).isEqualTo(USER_NAME);
-            assertThat(user.getPassword()).isEqualTo(USER_PASSWORD);
+            assertThat(subjectGetName()).isEqualTo(USER_NAME);
+            assertThat(subjectGetPassword()).isEqualTo(USER_PASSWORD);
         }
     }
 
@@ -38,44 +73,43 @@ public final class UserTest {
         @Test
         @DisplayName("사용자를 삭제한다.")
         public void it_deactivates_user() {
-            final User user = User.builder().build();
+            assertThat(subjectIsDeleted()).isFalse();
 
-            assertThat(user.isDeleted()).isFalse();
+            subjectDestroy();
 
-            user.destroy();
-
-            assertThat(user.isDeleted()).isTrue();
+            assertThat(subjectIsDeleted()).isTrue();
         }
     }
 
     @Nested
     @DisplayName("authenticate 메서드는")
     public final class Describe_authenticate {
+        @BeforeEach
+        private void beforeEach() {
+            subjectChangeWith();
+        }
+
         @Nested
         @DisplayName("삭제된 유저인 경우")
         public final class Context_deletedUser {
+            @BeforeEach
+            private void beforeEach() {
+                subjectDestroy();
+            }
+
             @Test
             @DisplayName("비밀번호 일치여부와 상관없이 입력한 비밀번호가 유저의 비밀번호와 일치하지 않음을 알려준다.")
             public void it_returns_false_regardless_of_password_matches_or_not() {
-                User user = User.builder()
-                    .password("test")
-                    .deleted(true)
-                    .build();
-
-                assertThat(user.authenticate("test")).isFalse();
-                assertThat(user.authenticate("xxx")).isFalse();
+                assertThat(subjectAuthenticate(INVALID_PASSWORD)).isFalse();
+                assertThat(subjectAuthenticate(USER_PASSWORD)).isFalse();
             }
         }
 
         @Test
         @DisplayName("입력한 비밀번호가 유저의 비밀번호와 일치하는지 알려준다.")
         public void it_notifies_password_matches_or_not() {
-            User user = User.builder()
-                .password("test")
-                .build();
-
-            assertThat(user.authenticate("test")).isTrue();
-            assertThat(user.authenticate("xxx")).isFalse();
+            assertThat(subjectAuthenticate(INVALID_PASSWORD)).isFalse();
+            assertThat(subjectAuthenticate(USER_PASSWORD)).isTrue();
         }
     }
 }

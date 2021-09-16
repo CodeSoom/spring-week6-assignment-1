938ec8f4529cc45291955d93ed53418ddb2b0dc1
Sungeheon <elon4856@gmail.com>
2021-09-16 16:08:32 +0900

    fix: 에러메세지를 수정하세요

--- app/src/main/java/com/codesoom/assignment/errors/WrongPasswordException.java
+++ app/src/main/java/com/codesoom/assignment/errors/WrongPasswordException.java
@@ -4,7 +4,7 @@ import com.codesoom.assignment.dto.UserLoginData;
 
 public class WrongPasswordException extends RuntimeException{
     public WrongPasswordException(UserLoginData loginData){
-        super("잘못된 비밀번호입니다. : " +loginData.getEmail());
+        super("잘못된 비밀번호입니다. :  로그인에 실패한 이메일은 [" + loginData.getEmail() + "] 입니다.");
     }
 
 }

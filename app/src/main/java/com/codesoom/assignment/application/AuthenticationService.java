df11184e25c5a04d1659da4980b367284c4f8895
wjy5446 <wjy5446@gmail.com>
2021-09-17 00:00:22 +0900

    refac) AuthenticationService의 javadoc 작성

--- app/src/main/java/com/codesoom/assignment/application/AuthenticationService.java
+++ app/src/main/java/com/codesoom/assignment/application/AuthenticationService.java
@@ -5,11 +5,13 @@ import com.codesoom.assignment.domain.UserRepository;
 import com.codesoom.assignment.dto.UserLoginData;
 import com.codesoom.assignment.errors.LoginNotMatchPasswordException;
 import com.codesoom.assignment.errors.UserNotFoundException;
+import com.codesoom.assignment.errors.InvalidAccessTokenException;
 import com.codesoom.assignment.utils.JwtUtil;
-import com.github.dozermapper.core.Mapper;
 import org.springframework.stereotype.Service;
 
-
+/**
+ * 인증, 인가와 관련된 처리를 담당합니다.
+ */
 @Service
 public class AuthenticationService {
 
@@ -21,6 +23,14 @@ public class AuthenticationService {
         this.userRepository = userRepository;
     }
 
+    /**
+     * User 로그인 정보를 확인해 access token 발급합니다.
+     *
+     * @param userLoginData email과 password를 담은 User 정보
+     * @return access token
+     * @throws UserNotFoundException User 정보가 존재하지 않는 경우 던집니다.
+     * @throws LoginNotMatchPasswordException User의 password가 일치하지 않는 경우 던집니다.
+     */
     public String login(UserLoginData userLoginData) {
         User findedUser = userRepository.findByEmail(userLoginData.getEmail())
                 .orElseThrow(UserNotFoundException::new);
@@ -32,6 +42,12 @@ public class AuthenticationService {
         return jwtUtil.encode(findedUser.getId());
     }
 
+    /**
+     * access token가 유효한지 확인합니다.
+     *
+     * @param token 발행된 access token
+     * @throws InvalidAccessTokenException access token이 유효하지 않는 경우 던집니다.
+     */
     public void checkToken(String token) {
         jwtUtil.decode(token);
     }

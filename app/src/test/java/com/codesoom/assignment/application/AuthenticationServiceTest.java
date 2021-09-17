0d4f2960734a8903ea280bd5fc53fa7064ff2b60
shinsanghooon <shinsanghooon@gmail.com>
2021-09-17 20:39:37 +0900

    `@ParameterizedTest`를 테스트에 적용
    invalid token value에 대해서 `@ParameterizedTest`를 적용

--- app/src/test/java/com/codesoom/assignment/application/AuthenticationServiceTest.java
+++ app/src/test/java/com/codesoom/assignment/application/AuthenticationServiceTest.java
@@ -7,6 +7,13 @@ import com.codesoom.assignment.utils.JwtUtil;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EmptySource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -70,6 +77,17 @@ class AuthenticationServiceTest {
 
         assertThatThrownBy(() -> authenticationService.parseToken(null))
                 .isInstanceOf(InvalidAccessTokenException.class);
+
+        Stream.of("", null).forEach((it) -> {
+            assertThatThrownBy(() -> authenticationService.parseToken(it))
+                    .isInstanceOf(InvalidAccessTokenException.class);
+        });
     }
 
+    @ParameterizedTest
+    @NullAndEmptySource
+    void parseTokenWithBlankToken(String token) {
+        assertThatThrownBy(() -> authenticationService.parseToken(token))
+                .isInstanceOf(InvalidAccessTokenException.class);
+    }
 }

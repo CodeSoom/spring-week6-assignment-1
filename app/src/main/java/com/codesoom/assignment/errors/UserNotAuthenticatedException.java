99f13d50483fec012e927b7b41131b6f048e57f5
pangnem <sprnd645@gmail.com>
2021-09-17 11:38:58 +0900

    update: 에러 메시지 수정
    - 인증에 실패한 이메일임을 알리기

--- app/src/main/java/com/codesoom/assignment/errors/UserNotAuthenticatedException.java
+++ app/src/main/java/com/codesoom/assignment/errors/UserNotAuthenticatedException.java
@@ -3,6 +3,6 @@ package com.codesoom.assignment.errors;
 public class UserNotAuthenticatedException extends RuntimeException {
 
     public UserNotAuthenticatedException(String email) {
-        super(String.format("삭제되었거나 인증에 실패하였습니다: %s", email));
+        super(String.format("인증에 실패하였습니다. 이메일: %s", email));
     }
 }

a9cca59da7479b669d5dc82210feb4ed7c992344
Sungeheon <elon4856@gmail.com>
2021-09-17 20:46:31 +0900

    feat:parseToken메소드에 대해 테스트코드를 작성하세요

--- app/src/test/java/com/codesoom/assignment/application/AuthenticationServiceTest.java
+++ app/src/test/java/com/codesoom/assignment/application/AuthenticationServiceTest.java
@@ -1,63 +1,124 @@
 package com.codesoom.assignment.application;
 
+import com.codesoom.assignment.domain.User;
+import com.codesoom.assignment.domain.UserRepository;
+import com.codesoom.assignment.dto.UserLoginData;
 import com.codesoom.assignment.errors.UnauthorizedException;
 import com.codesoom.assignment.utils.JwtUtil;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.util.Optional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.mockito.BDDMockito.given;
 
 class AuthenticationServiceTest {
 
     private static final String VALID_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjF9.neCsyNLzy3lQ4o2yliotWT06FwSGZagaHpKdAkjnGGw";
     private static final String INVALID_TOKEN = VALID_TOKEN + "invalid";
-    private final static String SECRETE_KEY = "12345678901234567890123456789010";
 
+    @Value("${jwt.secret}")
+    private static String SECRETE_KEY;
+    private User user;
+    private UserLoginData loginData;
     private AuthenticationService authenticationService;
 
     @BeforeEach
     void setUp() {
         JwtUtil jwtUtil = new JwtUtil(SECRETE_KEY);
-        authenticationService = new AuthenticationService(jwtUtil);
-    }
+        UserRepository userRepository = Mockito.mock(UserRepository.class);
 
+        authenticationService = new AuthenticationService(jwtUtil, userRepository);
 
-    @Test
-    void login() {
+        loginData = UserLoginData.builder()
+                .email("test@test.codesoom")
+                .password("password")
+                .build();
 
-        String accessToken = authenticationService.login();
-        System.out.println("accessToken = " + accessToken);
-        assertThat(accessToken).contains("");
-    }
-
-    @Test
-    void parseTokenWithValidToken() {
-        Long userId = authenticationService.parsetoken(VALID_TOKEN);
-        System.out.println("userId = " + userId);
-        assertThat(userId).isEqualTo(1L);
-    }
-
-    @Test
-    void parseTokenWithInValidToken() {
-
-        assertThatThrownBy(
-                () -> authenticationService.parsetoken(INVALID_TOKEN)
-        ).isInstanceOf(UnauthorizedException.class);
+        user = User.builder()
+                .email("test@test.codesoom")
+                .password("password")
+                .build();
 
     }
 
-    @Test
-    void parseTokenWithBlank() {
 
-        assertThatThrownBy(
-                () -> authenticationService.parsetoken(null)
-        ).isInstanceOf(UnauthorizedException.class);
+    @Nested
+    @DisplayName("parseToken메소드는")
+    class Describe_parseToken {
+        @Nested
+        @DisplayName("유효한 토큰이 주어질 때")
+        class Context_WithValidToken {
+
+            @DisplayName("userId를 반환한다.")
+            @Test
+            void it_ReturnsUserId() {
+                Long userId = authenticationService.parsetoken(VALID_TOKEN);
+                System.out.println("userId = " + userId);
+                assertThat(userId).isEqualTo(1L);
+            }
+        }
+
+        @Nested
+        @DisplayName("유효하지않은 토큰이 주어질 때")
+        class Context_WithInValidToken {
+
+            @DisplayName("UnauthorizedException을 던진다.")
+            @Test
+            void it_ReturnsUserId() {
+                assertThatThrownBy(
+                        () -> authenticationService.parsetoken(INVALID_TOKEN)
+                ).isInstanceOf(UnauthorizedException.class);
+            }
+        }
+
+        @Nested
+        @DisplayName("토큰이 비어있을 때")
+        class Context_WithBlank {
+
+            @DisplayName("UnauthorizedException을 던진다.")
+            @Test
+            void parseTokenWithBlank() {
+                assertThatThrownBy(
+                        () -> authenticationService.parsetoken(null)
+                ).isInstanceOf(UnauthorizedException.class);
 
 
-        assertThatThrownBy(
-                () -> authenticationService.parsetoken("")
-        ).isInstanceOf(UnauthorizedException.class);
+                assertThatThrownBy(
+                        () -> authenticationService.parsetoken("")
+                ).isInstanceOf(UnauthorizedException.class);
+            }
+        }
+    }
 
+
+
+    @Nested
+    @DisplayName("createToken 메소드는 ")
+    class Describe_createToken {
+
+        @Nested
+        @DisplayName("유효한 로그인 데이터가 주어졌을 때")
+        class Context_WithValidUserLoginData {
+            @BeforeEach
+            void setUp() {
+                given(userRepository.findByEmail(loginData.getEmail()))
+                        .willReturn(Optional.of(user));
+            }
+
+
+            @DisplayName("jwt토큰을 반환한다.")
+            @Test
+            void createToken() {
+                String accessToken = authenticationService.createToken(loginData);
+                assertThat(accessToken).isEqualTo(VALID_TOKEN);
+            }
+        }
     }
 }

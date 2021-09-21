c022731d6624f1d74799e2215f158d8fc50ee0b6
pangnem <sprnd645@gmail.com>
2021-09-22 00:10:43 +0900

    test: JwtEncoder 회원 케이스 추가

--- app/src/test/java/com/codesoom/assignment/application/JwtEncoderTest.java
+++ app/src/test/java/com/codesoom/assignment/application/JwtEncoderTest.java
@@ -1,9 +1,11 @@
 package com.codesoom.assignment.application;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
 import com.codesoom.assignment.domain.User;
 import com.codesoom.assignment.dto.AccessToken;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Pattern;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -26,7 +28,7 @@ public class JwtEncoderTest {
     @DisplayName("encode 메서드는")
     class Describe_encode {
 
-        private User user;
+        private List<User> users = new ArrayList<>();
 
         @Nested
         @DisplayName("id가 존재하는 회원이 주어지면")
@@ -34,18 +36,19 @@ public class JwtEncoderTest {
 
             @BeforeEach
             void setUp() {
-                user = User.builder()
-                    .id(1L)
-                    .build();
+                for (long i = 1; i <= 10; i++) {
+                    users.add(User.builder()
+                        .id(i)
+                        .build());
+                }
             }
 
             @Test
             @DisplayName("유효한 액세스 토큰을 리턴한다")
             void it_returns_validAccessToken() {
-                AccessToken accessToken = jwtEncoder.encode(user);
-
-                assertThat(Pattern.matches(JWT_REGEX, accessToken.getAccessToken()))
-                    .isTrue();
+                for (User validUser : users) {
+                    assertThat(checkUserIsValid(validUser)).isTrue();
+                }
             }
         }
 
@@ -55,18 +58,25 @@ public class JwtEncoderTest {
 
             @BeforeEach
             void setUp() {
-                user = User.builder()
-                    .build();
+                for (int i = 1; i <= 10; i++) {
+                    users.add(User.builder()
+                        .build());
+                }
             }
 
             @Test
             @DisplayName("유효하지 않은 액세스 토큰을 리턴한다")
             void it_returns_notValidAccessToken() {
-                AccessToken accessToken = jwtEncoder.encode(user);
-
-                assertThat(Pattern.matches(JWT_REGEX, accessToken.getAccessToken()))
-                    .isFalse();
+                for (User invalidUser : users) {
+                    assertThat(checkUserIsValid(invalidUser)).isFalse();
+                }
             }
+        }
+
+        private boolean checkUserIsValid(User validUser) {
+            AccessToken accessToken = jwtEncoder.encode(validUser);
+
+            return Pattern.matches(JWT_REGEX, accessToken.getAccessToken());
         }
     }
 }

c34cfc7ecc8f45596e1eb68c5e5366ea696cb4c1
kyuwon53 <lkw2v@naver.com>
2022-01-03 20:30:21 +0900

    [Add] 사용자 아이디를 인코딩하라

--- app/src/main/java/com/codesoom/assignment/utills/JwtUtill.java
+++ app/src/main/java/com/codesoom/assignment/utills/JwtUtill.java
@@ -1,9 +1,27 @@
 package com.codesoom.assignment.utills;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+
 /**
  * Jwt에대한 추가기능을 제공
- * */
+ */
+@Component
 public class JwtUtill {
-    public String encode() {
+    private final Key key;
 
+    public JwtUtill(@Value("${jwt.secret}") String secret) {
+        key = Keys.hmacShaKeyFor(secret.getBytes());
+    }
+
+    public String encode(Long userId) {
+        return Jwts.builder()
+                .claim("userId", 1L)
+                .signWith(key)
+                .compact();
     }
 }

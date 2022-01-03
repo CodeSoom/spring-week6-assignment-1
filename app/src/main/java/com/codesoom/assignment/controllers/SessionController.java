e76244c239c0696d7af970208a2b41148d758cc6
kyuwon53 <lkw2v@naver.com>
2022-01-03 20:36:44 +0900

    [Add] 로그인 요청시 사용자 정보를 보호하라

--- app/src/main/java/com/codesoom/assignment/controllers/SessionController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/SessionController.java
@@ -1,17 +1,32 @@
 package com.codesoom.assignment.controllers;
 
+import com.codesoom.assignment.application.AuthenticationService;
+import com.codesoom.assignment.dto.SessionResponseData;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * session에 대한 요청을 처리
+ */
 @RestController
 @RequestMapping("/session")
 public class SessionController {
+    private AuthenticationService authenticationService;
+
+    public SessionController(AuthenticationService authenticationService) {
+        this.authenticationService = authenticationService;
+    }
+
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public void login() {
+    public SessionResponseData login() {
+        String accessToken = authenticationService.login();
 
+        return SessionResponseData.builder()
+                .accessToken(accessToken)
+                .build();
     }
 }

61954e3aee14c0420d308e34b7759b90033ecf50
ririti <xodnzlzl@naver.com>
2022-01-03 09:22:51 +0900

    SessionController Create Test작성

---
+++ app/src/test/java/com/codesoom/assignment/controllers/SessionControllerTest.java
@@ -0,0 +1,23 @@
+package com.codesoom.assignment.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(SessionController.class)
+class SessionControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void login() throws Exception {
+        mockMvc.perform(post("/session"))
+                .andExpect(status().isCreated());
+    }
+}

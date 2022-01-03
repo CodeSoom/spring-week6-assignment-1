468d7e654dbd942a4f5d8f74f62140472f3d07a2
kyuwon53 <lkw2v@naver.com>
2022-01-03 20:31:28 +0900

    [Add] 사용자가 login 할때 인코딩을 테스트하라

--- app/src/test/java/com/codesoom/assignment/application/AuthenticationServiceTest.java
+++ app/src/test/java/com/codesoom/assignment/application/AuthenticationServiceTest.java
@@ -1,5 +1,38 @@
 package com.codesoom.assignment.application;
 
-class AuthenticationServiceTest {
+import com.codesoom.assignment.utills.JwtUtill;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("사용자 권한 서비스 클래스 ")
+class AuthenticationServiceTest {
+    private static final String SECRET = "12345678901234567890123456789010";
+    private AuthenticationService authenticationSrvice;
+
+    @BeforeEach
+    void setUp() {
+
+        JwtUtill jwtUtil = new JwtUtill(SECRET);
+        authenticationSrvice = new AuthenticationService(jwtUtil);
+    }
+
+    @Nested
+    @DisplayName("login 메소드는 ")
+    class Decribe_login {
+        @Nested
+        @DisplayName("")
+        class Context {
+            @Test
+            @DisplayName("")
+            void it_return() {
+                String accessToken = authenticationSrvice.login();
+
+                assertThat(accessToken).contains(".");
+            }
+        }
+    }
 }

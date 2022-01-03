2f6e786414eed18d7498d2efeaf06478c2a509a1
kyuwon53 <lkw2v@naver.com>
2022-01-03 20:34:31 +0900

    [Add] 로그인시 사용자 정보를 보호하라

--- app/src/main/java/com/codesoom/assignment/application/AuthenticationService.java
+++ app/src/main/java/com/codesoom/assignment/application/AuthenticationService.java
@@ -1,6 +1,20 @@
 package com.codesoom.assignment.application;
+
+import com.codesoom.assignment.utills.JwtUtill;
+import org.springframework.stereotype.Service;
+
 /**
  * 사용자 권한을 관리
- * */
+ */
+@Service
 public class AuthenticationService {
+    private JwtUtill jwtUtill;
+
+    public AuthenticationService(JwtUtill jwtUtill) {
+        this.jwtUtill = jwtUtill;
+    }
+
+    public String login() {
+        return jwtUtill.encode(1L);
+    }
 }

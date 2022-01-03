2b18de2adc37d79f7cb4661d5accd7b1b8c4ec96
kyuwon53 <lkw2v@naver.com>
2022-01-03 20:33:50 +0900

    [Add] 사용자가 로그인 할 때 인코딩을 하여 무결성을 보호하나 테스트

--- app/src/test/java/com/codesoom/assignment/application/AuthenticationServiceTest.java
+++ app/src/test/java/com/codesoom/assignment/application/AuthenticationServiceTest.java
@@ -8,26 +8,25 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@DisplayName("사용자 권한 서비스 클래스 ")
+@DisplayName("사용자 권한 서비스 클래스")
 class AuthenticationServiceTest {
     private static final String SECRET = "12345678901234567890123456789010";
     private AuthenticationService authenticationSrvice;
 
     @BeforeEach
     void setUp() {
-
         JwtUtill jwtUtil = new JwtUtill(SECRET);
         authenticationSrvice = new AuthenticationService(jwtUtil);
     }
 
     @Nested
-    @DisplayName("login 메소드는 ")
+    @DisplayName("login 메소드는")
     class Decribe_login {
         @Nested
         @DisplayName("")
         class Context {
             @Test
-            @DisplayName("")
+            @DisplayName("사용자 권한 토큰이 인코딩된다.")
             void it_return() {
                 String accessToken = authenticationSrvice.login();
 

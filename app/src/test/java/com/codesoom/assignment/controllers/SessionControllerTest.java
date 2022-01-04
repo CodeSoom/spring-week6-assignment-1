6c5ddb223fa3254cbcfaae0f3a1536dbe2b2d0dc
hyuk <pjh08190819@naver.com>
2022-01-05 03:11:35 +0900

    add : Login 정보 있을 때 access token 응답 테스트 작성

--- app/src/test/java/com/codesoom/assignment/controllers/SessionControllerTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/SessionControllerTest.java
@@ -1,6 +1,9 @@
 package com.codesoom.assignment.controllers;
 
 import com.codesoom.assignment.application.AuthenticationService;
+import com.codesoom.assignment.dto.LoginData;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -9,13 +12,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.ArgumentMatchers.contains;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -26,25 +33,44 @@ class SessionControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
     @SpyBean
     private AuthenticationService authenticationService;
+
+    LoginData loginData;
+
+    @BeforeEach
+    void setUp() {
+        loginData = LoginData.builder()
+                .email("pjh0819@codesom.com")
+                .password("123456")
+                .build();
+    }
 
     @Nested
     @DisplayName("POST /session 요청은")
     class Describe_post {
 
-        @BeforeEach
-        void setUp() {
-            given(authenticationService.login()).willReturn("a.b.c");
-        }
+        @Nested
+        @DisplayName("LoginData가 주어진다면")
+        class Context_with_loginData {
 
-        @Test
-        @DisplayName("access token을 응답합니다.")
-        void login() throws Exception {
-            mockMvc.perform(post("/session"))
-                    .andExpect(status().isCreated())
-                    .andExpect(content().string(containsString(".")))
-                    .andDo(print());
+            @Test
+            @DisplayName("access token을 응답합니다.")
+            void login() throws Exception {
+                mockMvc.perform(post("/session")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(loginDataToContent(loginData)))
+                        .andExpect(status().isCreated())
+                        .andExpect(jsonPath("$.accessToken").value(containsString(".")))
+                        .andDo(print());
+            }
         }
+    }
+
+    private String loginDataToContent(LoginData loginData) throws JsonProcessingException {
+        return objectMapper.writeValueAsString(loginData);
     }
 }

3882bf418b77387f7c224f8dac41807076ed6174
kyuwon53 <lkw2v@naver.com>
2022-01-04 22:12:10 +0900

    [Add] JavaDoc 주석으로 메소드를 설명하라

--- app/src/main/java/com/codesoom/assignment/application/AuthenticationService.java
+++ app/src/main/java/com/codesoom/assignment/application/AuthenticationService.java
@@ -15,10 +15,20 @@ public class AuthenticationService {
         this.jwtUtill = jwtUtill;
     }
 
+    /**
+     * 사용자가 로그인하면 아이디를 인코딩하여 리턴합니다.
+     *
+     * @return 인코딩된 아이디
+     */
     public String login() {
         return jwtUtill.encode(1L);
     }
 
+    /**
+     * 인증된 토큰을 받아 아이디를 디코딩하여 리턴합니다.
+     *
+     * @return 사용자 아이디
+     */
     public Long parseToken(String accessToken) {
         Claims claims = jwtUtill.decode(accessToken);
         return claims.get("userId", Long.class);

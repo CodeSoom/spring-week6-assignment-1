3edd73712fdf95fb79e6574cee860a3918fe895c
hyuk <pjh08190819@naver.com>
2022-01-05 02:25:35 +0900

    sessionController 테스트 작성

--- app/src/test/java/com/codesoom/assignment/controllers/SessionControllerTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/SessionControllerTest.java
@@ -2,10 +2,11 @@ package com.codesoom.assignment.controllers;
 
 import com.codesoom.assignment.application.AuthenticationService;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.test.web.servlet.MockMvc;
@@ -13,10 +14,13 @@ import org.springframework.test.web.servlet.MockMvc;
 import static org.hamcrest.Matchers.containsString;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(SessionController.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+@DisplayName("SessionController 클래스")
 class SessionControllerTest {
 
     @Autowired
@@ -25,16 +29,22 @@ class SessionControllerTest {
     @SpyBean
     private AuthenticationService authenticationService;
 
-    @BeforeEach
-    void setUp() {
-        given(authenticationService.login()).willReturn("a.b.c");
-    }
+    @Nested
+    @DisplayName("POST /session 요청은")
+    class Describe_post {
 
-    @Test
-    void login() throws Exception {
-        mockMvc.perform(post("/session"))
-                .andExpect(status().isCreated())
-                .andExpect(content().string(containsString(".")));
-    }
+        @BeforeEach
+        void setUp() {
+            given(authenticationService.login()).willReturn("a.b.c");
+        }
 
+        @Test
+        @DisplayName("access token을 응답합니다.")
+        void login() throws Exception {
+            mockMvc.perform(post("/session"))
+                    .andExpect(status().isCreated())
+                    .andExpect(content().string(containsString(".")))
+                    .andDo(print());
+        }
+    }
 }

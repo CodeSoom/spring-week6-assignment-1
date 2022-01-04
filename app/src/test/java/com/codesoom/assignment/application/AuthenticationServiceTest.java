25f6c45bf8f9794380b61f09b2043effef4e0901
kyuwon53 <lkw2v@naver.com>
2022-01-04 21:00:43 +0900

    [Add] 인증된 토큰을 검사하라

--- app/src/test/java/com/codesoom/assignment/application/AuthenticationServiceTest.java
+++ app/src/test/java/com/codesoom/assignment/application/AuthenticationServiceTest.java
@@ -1,5 +1,6 @@
 package com.codesoom.assignment.application;
 
+import com.codesoom.assignment.errors.InvalidAccessTokenException;
 import com.codesoom.assignment.utills.JwtUtill;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -7,10 +8,17 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @DisplayName("사용자 권한 서비스")
 class AuthenticationServiceTest {
     private static final String SECRET = "12345678901234567890123456789010";
+
+    private static final String VALID_TOKEN = "eyJhbGciOiJIUzI1NiJ9." +
+            "eyJ1c2VySWQiOjF9.ZZ3CUl0jxeLGvQ1Js5nG2Ty5qGTlqai5ubDMXZOdaDk";
+    private static final String INVALID_TOKEN = "eyJhbGciOiJIUzI1NiJ9." +
+            "eyJ1c2VySWQiOjF9.ZZ3CUl0jxeLGvQ1Js5nG2Ty5qGTlqai5ubDMXZOdaD0";
+
     private AuthenticationService authenticationSrvice;
 
     @BeforeEach
@@ -27,10 +35,50 @@ class AuthenticationServiceTest {
         class Context {
             @Test
             @DisplayName("엑세스 토큰을 리턴한다.")
-            void it_return() {
+            void it_return_accessToken() {
                 String accessToken = authenticationSrvice.login();
 
-                assertThat(accessToken).contains(".");
+                assertThat(accessToken).isEqualTo(VALID_TOKEN);
+            }
+        }
+    }
+
+    @Nested
+    @DisplayName("parseToken 메소드는")
+    class Decribe_parseToken {
+        @Nested
+        @DisplayName("인증된 토큰이 들어오면")
+        class Context_with_valid_token {
+            @Test
+            @DisplayName("아이디를 디코딩하고 리턴한다.")
+            void it_return() {
+                Long userId = authenticationSrvice.parseToken(VALID_TOKEN);
+
+                assertThat(userId).isEqualTo(1L);
+            }
+        }
+
+        @Nested
+        @DisplayName("인증되지않은 토큰이 들어오면")
+        class Context_with_Invalid_token {
+            @Test
+            @DisplayName("InvalidAccessTokenException 예외를 던진다.")
+            void it_return() {
+                assertThatThrownBy(
+                        () -> authenticationSrvice.parseToken(INVALID_TOKEN)
+                ).isInstanceOf(InvalidAccessTokenException.class);
+            }
+        }
+
+        @Nested
+        @DisplayName("비어있는 토큰이 들어오면")
+        class Context_with_blank_token {
+            @Test
+            @DisplayName("InvalidAccessTokenException 예외를 던진다.")
+            void it_return() {
+                assertThatThrownBy(
+                        () -> authenticationSrvice.parseToken("")
+                ).isInstanceOf(InvalidAccessTokenException.class);
             }
         }
     }

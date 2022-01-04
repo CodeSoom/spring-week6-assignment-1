16b54fa7933cbebb9fa0389b1078e490fa934013
kyuwon53 <lkw2v@naver.com>
2022-01-04 21:01:08 +0900

    [Add] 아이디를 디코딩하는 함수를 만들어라

--- app/src/main/java/com/codesoom/assignment/application/AuthenticationService.java
+++ app/src/main/java/com/codesoom/assignment/application/AuthenticationService.java
@@ -1,6 +1,9 @@
 package com.codesoom.assignment.application;
 
+import com.codesoom.assignment.errors.InvalidAccessTokenException;
 import com.codesoom.assignment.utills.JwtUtill;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.security.SignatureException;
 import org.springframework.stereotype.Service;
 
 /**
@@ -16,5 +19,18 @@ public class AuthenticationService {
 
     public String login() {
         return jwtUtill.encode(1L);
+    }
+
+    public Long parseToken(String accessToken) {
+        if (accessToken.isEmpty()) {
+            //TODO isBlank로 변경 java 버전 변경
+            throw new InvalidAccessTokenException(accessToken);
+        }
+        try {
+            Claims claims = jwtUtill.decode(accessToken);
+            return claims.get("userId", Long.class);
+        } catch (SignatureException e) {
+            throw new InvalidAccessTokenException(accessToken);
+        }
     }
 }

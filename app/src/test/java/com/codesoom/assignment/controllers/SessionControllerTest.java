06da0c6f6b73784ff34ea6210052c31256af0691
kyuwon53 <lkw2v@naver.com>
2022-01-04 19:46:59 +0900

    [Add] 암호화를 테스트하라

--- app/src/test/java/com/codesoom/assignment/controllers/SessionControllerTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/SessionControllerTest.java
@@ -1,14 +1,19 @@
 package com.codesoom.assignment.controllers;
 
+import com.codesoom.assignment.application.AuthenticationService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.Matchers.containsString;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(SessionController.class)
@@ -16,17 +21,27 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class SessionControllerTest {
     @Autowired
     private MockMvc mockMvc;
+
+    @MockBean
+    private AuthenticationService authenticationService;
+
     @Nested
     @DisplayName("post 요청시")
-    class Describe_post{
+    class Describe_post {
+        @BeforeEach
+        void setUp() {
+            given(authenticationService.login()).willReturn("a.b.c");
+        }
+
         @Nested
         @DisplayName("")
-        class Context{
+        class Context {
             @Test
             @DisplayName("요청이 성공적으로 처리되었음을 응답한다.")
             void it_status_created() throws Exception {
                 mockMvc.perform(post("/session"))
-                        .andExpect(status().isCreated());
+                        .andExpect(status().isCreated())
+                        .andExpect(content().string(containsString(".")));
 
             }
         }

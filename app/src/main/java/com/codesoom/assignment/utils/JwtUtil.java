02e90fde19e1b65f0dfe509295d10594c921ca58
Kwakcena <rhkrgudwh@gmail.com>
2022-01-05 18:41:55 +0900

    jwtUtil 구현
    jwtUtil을 구현합니다

--- app/src/main/java/com/codesoom/assignment/utils/JwtUtil.java
+++ app/src/main/java/com/codesoom/assignment/utils/JwtUtil.java
@@ -1,5 +1,6 @@
 package com.codesoom.assignment.utils;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
@@ -20,5 +21,13 @@ public class JwtUtil {
                 .claim("userId", userId)
                 .signWith(key)
                 .compact();
+    }
+
+    public Claims decode(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
     }
 }

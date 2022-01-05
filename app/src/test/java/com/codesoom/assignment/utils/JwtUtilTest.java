f622c2b0af11d711a4f02e87a2b78c6302f3a72d
hyuk <pjh08190819@naver.com>
2022-01-05 22:19:50 +0900

    add : JwtUtil 테스트 코드 작성

--- app/src/test/java/com/codesoom/assignment/utils/JwtUtilTest.java
+++ app/src/test/java/com/codesoom/assignment/utils/JwtUtilTest.java
@@ -2,61 +2,109 @@ package com.codesoom.assignment.utils;
 
 import com.codesoom.assignment.errors.InvalidTokenException;
 import io.jsonwebtoken.Claims;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+@SpringBootTest
+@DisplayName("JwtUtil 클래스")
 class JwtUtilTest {
 
     private static final String SECRET = "12345678901234567890123456789010";
 
     private static final String VALID_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjF9." +
             "neCsyNLzy3lQ4o2yliotWT06FwSGZagaHpKdAkjnGGw";
+
     private static final String INVALID_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjF9." +
             "neCsyNLzy3lQ4o2yliotWT06FwSGZagaHpKdAkjnG29304";
 
+    private static final Long USER_ID = 1L;
 
+    @Autowired
     private JwtUtil jwtUtil;
 
-    @BeforeEach
-    void setUp() {
-        jwtUtil = new JwtUtil(SECRET);
+    @Nested
+    @DisplayName("encode() 메소드는")
+    class Describe_encode {
+
+        @Nested
+        @DisplayName("userId 값이 주어진다면")
+        class Context_with_userId {
+
+            final Long givenId = USER_ID;
+            final String givenToken = VALID_TOKEN;
+
+            @Test
+            @DisplayName("token을 리턴합니다.")
+            void it_return_token() {
+                String token = jwtUtil.encode(givenId);
+
+                assertThat(token).isEqualTo(givenToken);
+            }
+        }
     }
 
-    @Test
-    void encode() {
-        String token = jwtUtil.encode(1L);
+    @Nested
+    @DisplayName("decode() 메소드는")
+    class Describe_decode {
 
-        System.out.println(token);
+        final Long givenId = USER_ID;
+        final String givenToken = VALID_TOKEN;
 
-        assertThat(token).isEqualTo(VALID_TOKEN);
-    }
+        @Nested
+        @DisplayName("token이 주어진다면")
+        class Context_with_token {
 
-    @Test
-    void decode() {
-        Claims claims = jwtUtil.decode(VALID_TOKEN);
+            @Test
+            @DisplayName("userId를 리턴합니다.")
+            void it_return_userId() {
+                Claims claims = jwtUtil.decode(givenToken);
 
-        assertThat(claims.get("userId", Long.class)).isEqualTo(1L);
-    }
+                assertThat(claims.get("userId", Long.class)).isEqualTo(givenId);
+            }
+        }
 
-    @Test
-    void decodeWithInvalidToken() {
-        assertThatThrownBy(() -> jwtUtil.decode(INVALID_TOKEN))
-                .isInstanceOf(InvalidTokenException.class);
-    }
+        @Nested
+        @DisplayName("유효하지 않는 token이 주어진다면")
+        class Context_with_invalid_token {
 
-    @Test
-    void decodeWithEmptyToken() {
-        assertThatThrownBy(() -> jwtUtil.decode(null))
-                .isInstanceOf(InvalidTokenException.class);
+            final String givenInvalidToken = INVALID_TOKEN;
 
-        assertThatThrownBy(() -> jwtUtil.decode(""))
-                .isInstanceOf(InvalidTokenException.class);
+            @Test
+            @DisplayName("토큰이 유효하지 않다는 예외를 던진다.")
+            void it_return_InvalidTokenException() {
+                assertThatThrownBy(() -> jwtUtil.decode(givenInvalidToken))
+                        .isInstanceOf(InvalidTokenException.class);
+            }
+        }
 
-        assertThatThrownBy(() -> jwtUtil.decode("   "))
-                .isInstanceOf(InvalidTokenException.class);
+        @Nested
+        @DisplayName("null이 주어진다면")
+        class Context_with_null {
+
+            @Test
+            @DisplayName("토큰이 유효하지 않다는 예외를 던진다.")
+            void it_return_InvalidTokenException() {
+                assertThatThrownBy(() -> jwtUtil.decode(null))
+                        .isInstanceOf(InvalidTokenException.class);
+            }
+        }
+
+        @Nested
+        @DisplayName("빈값이 주어진다면")
+        class Context_with_blank {
+
+            @Test
+            @DisplayName("토큰이 유효하지 않다는 예외를 던진다.")
+            void it_return_InvalidTokenException() {
+                assertThatThrownBy(() -> jwtUtil.decode(""))
+                        .isInstanceOf(InvalidTokenException.class);
+            }
+        }
     }
 }

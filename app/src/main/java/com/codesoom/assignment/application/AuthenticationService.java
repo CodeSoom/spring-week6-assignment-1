75cf90ac75320e3c3015d2f690c2782eb0667ff7
Hyunjune Lee <aphlox@naver.com>
2022-01-06 01:20:09 +0900

    [Refactor] AuthenticationService 1L 대신 TOKEN_USER_ID로 사용하다록 수정

--- app/src/main/java/com/codesoom/assignment/application/AuthenticationService.java
+++ app/src/main/java/com/codesoom/assignment/application/AuthenticationService.java
@@ -7,13 +7,14 @@ import org.springframework.stereotype.Service;
 @Service
 public class AuthenticationService {
     private JwtCodec jwtCodec;
+    private static final Long TOKEN_USER_ID = 1L;
 
     public AuthenticationService(JwtCodec jwtCodec) {
         this.jwtCodec = jwtCodec;
     }
 
     public String login(){
-        return jwtCodec.encode(1L);
+        return jwtCodec.encode(TOKEN_USER_ID);
     }
 
     public Long parseToken(String accessToken) {

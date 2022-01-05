c1868331c3272e16dd8c42526912ab9720758034
Kwakcena <rhkrgudwh@gmail.com>
2022-01-05 14:04:27 +0900

    Application resource의 secret key 사용
    resource에 등록한 jwt.secret를 사용하도록 합니다.

--- app/src/main/java/com/codesoom/assignment/utils/JwtUtil.java
+++ app/src/main/java/com/codesoom/assignment/utils/JwtUtil.java
@@ -2,6 +2,7 @@ package com.codesoom.assignment.utils;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.security.Key;
@@ -10,12 +11,11 @@ import java.security.Key;
 public class JwtUtil {
     public final Key key;
 
-    public JwtUtil(String secret) {
+    public JwtUtil(@Value("${jwt.secret}") String secret) {
         key = Keys.hmacShaKeyFor(secret.getBytes());
     }
 
     public String encode(Long userId) {
-
         return Jwts.builder()
                 .claim("userId", userId)
                 .signWith(key)

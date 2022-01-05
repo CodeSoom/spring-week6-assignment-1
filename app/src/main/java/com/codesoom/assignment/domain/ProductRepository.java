7b8c0aa268997a973879c825f3cdd3ec24890cda
Kwakcena <rhkrgudwh@gmail.com>
2022-01-06 02:41:19 +0900

    repository에 deleteAll 추가
    전체 product를 삭제하는 deleteAll을 추가합니다

--- app/src/main/java/com/codesoom/assignment/domain/ProductRepository.java
+++ app/src/main/java/com/codesoom/assignment/domain/ProductRepository.java
@@ -1,8 +1,11 @@
 package com.codesoom.assignment.domain;
 
+import org.springframework.stereotype.Repository;
+
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface ProductRepository {
     List<Product> findAll();
 
@@ -11,4 +14,6 @@ public interface ProductRepository {
     Product save(Product product);
 
     void delete(Product product);
+
+    void deleteAll();
 }

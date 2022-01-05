941acb4fbbc0c0cdc8fdec9848bb1e06a87c5f86
Kwakcena <rhkrgudwh@gmail.com>
2022-01-05 13:31:28 +0900

    AuthenticationService의 테스트 코드 작성
    AuthenticationService 메소드 중 login에 관한 메소드의
    테스트코드를 작성한다

---
+++ app/src/test/java/com/codesoom/assignment/application/AuthenticationServiceTest.java
@@ -0,0 +1,22 @@
+package com.codesoom.assignment.application;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class AuthenticationServiceTest {
+    private AuthenticationService authenticationService;
+
+    @BeforeEach
+    void setUp() {
+        authenticationService = new AuthenticationService();
+    }
+
+    @Test
+    void login() {
+        String accessToken = authenticationService.login();
+
+        assertThat(accessToken).contains(".");
+    }
+}

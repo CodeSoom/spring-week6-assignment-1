b5d9bf9f72ace82d21be73b2371147f2dd5b79e9
Kwakcena <rhkrgudwh@gmail.com>
2022-01-05 13:30:58 +0900

    AuthenticationService를 구현한다
    인증에 관한 서비스 클래스에서 로그인 메소드는 토큰을 반환한다.
    토큰 내에 '.' 이 있는지 확인한다

--- app/src/main/java/com/codesoom/assignment/controllers/SessionController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/SessionController.java
@@ -1,5 +1,6 @@
 package com.codesoom.assignment.controllers;
 
+import com.codesoom.assignment.application.AuthenticationService;
 import com.codesoom.assignment.dto.SessionResponseData;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,11 +11,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/session")
 public class SessionController {
+    private AuthenticationService authenticationService;
+
+    public SessionController(AuthenticationService authenticationService) {
+        this.authenticationService = authenticationService;
+    }
+
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public SessionResponseData login() {
+        String accessToken = authenticationService.login();
+
         return SessionResponseData.builder()
-                .accessToken("a.b.c.")
+                .accessToken(accessToken)
                 .build();
     }
 }
--- app/src/test/java/com/codesoom/assignment/controllers/SessionControllerTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/SessionControllerTest.java
@@ -1,8 +1,10 @@
 package com.codesoom.assignment.controllers;
 
+import com.codesoom.assignment.application.AuthenticationService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.containsString;
@@ -15,6 +17,9 @@ class SessionControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @SpyBean
+    private AuthenticationService authenticationService;
 
     @Test
     void login() throws Exception {

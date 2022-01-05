38f69edbdd3180da60577d2b5b849e09e4b31fa7
Kwakcena <rhkrgudwh@gmail.com>
2022-01-05 13:41:18 +0900

    Jwt 토큰을 발급하라
    테스트용으로 Jwt 토큰을 발급합니다

--- app/src/main/java/com/codesoom/assignment/application/AuthenticationService.java
+++ app/src/main/java/com/codesoom/assignment/application/AuthenticationService.java
@@ -1,10 +1,20 @@
 package com.codesoom.assignment.application;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
 
 @Service
 public class AuthenticationService {
     public String login() {
-        return "a.b.c";
+        String secret = "01234567890123456789012345678901";
+        Key key = Keys.hmacShaKeyFor(secret.getBytes());
+
+        return Jwts.builder()
+                .claim("userId", 1)
+                .signWith(key)
+                .compact();
     }
 }

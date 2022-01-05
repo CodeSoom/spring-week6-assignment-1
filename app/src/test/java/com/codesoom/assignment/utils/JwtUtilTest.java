8b0fd78e79b58128f1b8d6ba63442141fdf51925
Kwakcena <rhkrgudwh@gmail.com>
2022-01-06 00:22:12 +0900

    필요없는 코드를 제거하고 테스트 데이터를 개선한다
    필요없는 코드인 `@Nested`를 제거하고 INVALID_TOKEN은 VALID_TOKEN에 0을 붙이도록 한다

--- app/src/test/java/com/codesoom/assignment/utils/JwtUtilTest.java
+++ app/src/test/java/com/codesoom/assignment/utils/JwtUtilTest.java
@@ -10,14 +10,12 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-@Nested
 @DisplayName("JwtUtil 테스트")
 class JwtUtilTest {
     private static final String SECRET = "01234567890123456789012345678901";
     private static final String VALID_TOKEN = "eyJhbGciOiJIUzI1NiJ9" +
             ".eyJ1c2VySWQiOjF9._sqAnLqnuii5tTri0u8AAwGJpI4PF6WRT9wkOLyxWaw";
-    private static final String INVALID_TOKEN = "eyJhbGciOiJIUzI1NiJ9" +
-            ".eyJ1c2VySWQiOjF9._sqAnLqnuii5tTri0u8AAwGJpI4PF6WRT9wkOLyxWa0";
+    private static final String INVALID_TOKEN = VALID_TOKEN + "0";
 
     private JwtUtil jwtUtil;
 

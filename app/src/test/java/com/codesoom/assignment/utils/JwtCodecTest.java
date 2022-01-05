ec6903951d3014016e94edeac6251a3e6a51951d
Hyunjune Lee <aphlox@naver.com>
2022-01-06 00:52:27 +0900

    [Refactor] JwtCodec 테스트 구조화

--- app/src/test/java/com/codesoom/assignment/utils/JwtCodecTest.java
+++ app/src/test/java/com/codesoom/assignment/utils/JwtCodecTest.java
@@ -4,7 +4,10 @@ import com.codesoom.assignment.errors.InvalidTokenException;
 import io.jsonwebtoken.Claims;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -12,44 +15,102 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 @DisplayName("JwtCodec 클래스")
 class JwtCodecTest {
     private static final String SECRET = "12345678901234567890123456789012";
+    private static final Long TOKEN_USER_ID = 1L;
     private static final String VALID_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjF9.ZZ3CUl0jxeLGvQ1Js5nG2Ty5qGTlqai5ubDMXZOdaDk";
     private static final String INVALID_TOKEN = VALID_TOKEN + "abc";
 
-    private JwtCodec jwtCodec;
+    @Nested
+    @DisplayName("encode 메소드는")
+    class Describe_encode{
 
-    @BeforeEach
-    void setUp(){
-        jwtCodec = new JwtCodec(SECRET);
+        @Nested
+        @DisplayName("id가 주어지면")
+        class Context_with_a_user_id{
+
+            JwtCodec jwtCodec;
+            Long givenUserId;
+
+            @BeforeEach
+            void setUp(){
+                jwtCodec = new JwtCodec(SECRET);
+                givenUserId = TOKEN_USER_ID;
+            }
+
+            @Test
+            @DisplayName("id를 인코딩한 토큰을 리턴한다.")
+            void it_returns_token(){
+                String accessToken = jwtCodec.encode(givenUserId);
+                assertThat(accessToken).isEqualTo(VALID_TOKEN);
+            }
+
+        }
     }
 
-    @Test
-    void encode(){
-        String accessToken = jwtCodec.encode(1L);
-        assertThat(accessToken).isEqualTo(VALID_TOKEN);
+    @Nested
+    @DisplayName("decode 메소드는")
+    class Describe_decode{
+
+        @Nested
+        @DisplayName("유효한 토큰이 주어지면")
+        class Context_with_a_valid_token{
+
+            JwtCodec jwtCodec;
+            String validToken;
+
+            @BeforeEach
+            void setUp(){
+                jwtCodec = new JwtCodec(SECRET);
+                validToken = VALID_TOKEN;
+            }
+
+            @Test
+            @DisplayName("토큰을 디코딩한 id를 리턴한다.")
+            void it_returns_decoded_id(){
+                Claims claims = jwtCodec.decode(validToken);
+                assertThat(claims.get("userId", Long.class)).isEqualTo(TOKEN_USER_ID);
+            }
+        }
+
+        @Nested
+        @DisplayName("유효하지 않은 토큰이 주어지면")
+        class Context_with_a_invalid_token{
+
+            JwtCodec jwtCodec;
+            String invalidToken;
+
+            @BeforeEach
+            void setUp(){
+                jwtCodec = new JwtCodec(SECRET);
+                invalidToken = INVALID_TOKEN;
+            }
+
+            @Test
+            @DisplayName("InvalidTokenException 예외를 던진다.")
+            void it_returns_string(){
+                assertThatThrownBy(() -> jwtCodec.decode(INVALID_TOKEN))
+                        .isInstanceOf(InvalidTokenException.class);
+            }
+        }
+
+        @Nested
+        @DisplayName("빈 토큰이 주어지면")
+        class Context_with_a_empty_token{
+
+            JwtCodec jwtCodec;
+
+            @BeforeEach
+            void setUp(){
+                jwtCodec = new JwtCodec(SECRET);
+            }
+
+            @Test
+            @DisplayName("InvalidTokenException 예외를 던진다.")
+            void it_returns_string(){
+                Stream.of("", "   ", null).forEach((emptyToken) -> {
+                    assertThatThrownBy(() -> jwtCodec.decode(emptyToken))
+                            .isInstanceOf(InvalidTokenException.class);
+                });
+            }
+        }
     }
-
-    @Test
-    void decodeWithValidToken(){
-        Claims claims = jwtCodec.decode(VALID_TOKEN);
-        assertThat(claims.get("userId", Long.class)).isEqualTo(1L);
-    }
-
-    @Test
-    void decodeWithInvalidToken(){
-        assertThatThrownBy(() -> jwtCodec.decode(INVALID_TOKEN))
-                .isInstanceOf(InvalidTokenException.class);
-    }
-
-    @Test
-    void decodeWithEmptyToken(){
-        assertThatThrownBy(() -> jwtCodec.decode(null))
-                .isInstanceOf(InvalidTokenException.class);
-
-        assertThatThrownBy(() -> jwtCodec.decode(""))
-                .isInstanceOf(InvalidTokenException.class);
-
-        assertThatThrownBy(() -> jwtCodec.decode("    "))
-                .isInstanceOf(InvalidTokenException.class);
-    }
-
 }

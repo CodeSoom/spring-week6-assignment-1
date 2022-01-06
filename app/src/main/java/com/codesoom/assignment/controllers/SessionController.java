57809ed44a2d48309f63c185e6472f7e11052393
Kwakcena <rhkrgudwh@gmail.com>
2022-01-07 03:58:15 +0900

    CrossOrigin 어노테이션을 추가하라
    로그인 요청에 CORS 에러가 나지 않도록 CrossOrigin 어노테이션을 추가합니다

--- app/src/main/java/com/codesoom/assignment/controllers/SessionController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/SessionController.java
@@ -3,12 +3,14 @@ package com.codesoom.assignment.controllers;
 import com.codesoom.assignment.application.AuthenticationService;
 import com.codesoom.assignment.dto.SessionResponseData;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/session")
 public class SessionController {
     private AuthenticationService authenticationService;

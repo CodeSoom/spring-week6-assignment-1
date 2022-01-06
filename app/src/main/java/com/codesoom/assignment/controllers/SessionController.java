eae45d4cde989cc7ece4d960bc3e61c97d238563
kyuwon53 <lkw2v@naver.com>
2022-01-06 20:56:19 +0900

    [Add] 사용자 로그인 정보를 받아라

--- app/src/main/java/com/codesoom/assignment/controllers/SessionController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/SessionController.java
@@ -1,16 +1,25 @@
 package com.codesoom.assignment.controllers;
 
 import com.codesoom.assignment.application.AuthenticationService;
+import com.codesoom.assignment.dto.ErrorResponse;
 import com.codesoom.assignment.dto.SessionResponseData;
+import com.codesoom.assignment.dto.UserLoginData;
+import com.codesoom.assignment.errors.LoginNotMatchPasswordException;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+
 /**
  * session과 관련된 HTTP 요청을 처리합니다.
  */
+@CrossOrigin
 @RestController
 @RequestMapping("/session")
 public class SessionController {
@@ -27,11 +36,17 @@ public class SessionController {
      */
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public SessionResponseData login() {
-        String accessToken = authenticationService.login();
+    public SessionResponseData login(@RequestBody @Valid UserLoginData userLoginData) {
+        String accessToken = authenticationService.login(userLoginData);
 
         return SessionResponseData.builder()
                 .accessToken(accessToken)
                 .build();
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(LoginNotMatchPasswordException.class)
+    ErrorResponse handleLoginNotMatchPasswordException(LoginNotMatchPasswordException e) {
+        return new ErrorResponse(e.getMessage());
     }
 }

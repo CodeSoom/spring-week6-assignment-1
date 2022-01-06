59932cb53f41a03567e08102ce790d350422bce6
Kwakcena <rhkrgudwh@gmail.com>
2022-01-07 00:56:44 +0900

    accessToken이 비어있거나 null인 경우 테스트, 구현
    authenticationService 내에서 token을 parsing 하는 메소드에 대해
    테스트 코드를 작성하고 이를 구현하였습니다

--- app/src/test/java/com/codesoom/assignment/application/AuthenticationServiceTest.java
+++ app/src/test/java/com/codesoom/assignment/application/AuthenticationServiceTest.java
@@ -1,13 +1,18 @@
 package com.codesoom.assignment.application;
 
+import com.codesoom.assignment.errors.InvalidAccessTokenException;
 import com.codesoom.assignment.utils.JwtUtil;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class AuthenticationServiceTest {
     private static final String SECRET = "01234567890123456789012345678901";
+    private static final String VALID_TOKEN = "eyJhbGciOiJIUzI1NiJ9" +
+            ".eyJ1c2VySWQiOjF9._sqAnLqnuii5tTri0u8AAwGJpI4PF6WRT9wkOLyxWaw";
+    private static final String INVALID_TOKEN = VALID_TOKEN + "0";
 
     private AuthenticationService authenticationService;
 
@@ -24,4 +29,26 @@ class AuthenticationServiceTest {
 
         assertThat(accessToken).contains(".");
     }
+
+    @Test
+    void parseTokenWithValidToken() {
+        Long userId = authenticationService.parseToken(VALID_TOKEN);
+
+        assertThat(userId).isEqualTo(1L);
+    }
+
+    @Test
+    void paresTokenWithInvalidToken() {
+        assertThatThrownBy(() -> authenticationService.parseToken(INVALID_TOKEN))
+                .isInstanceOf(InvalidAccessTokenException.class);
+    }
+
+    @Test
+    void parseTokenWithEmptyToken() {
+        assertThatThrownBy(() -> authenticationService.parseToken(null))
+                .isInstanceOf(InvalidAccessTokenException.class);
+        assertThatThrownBy(() -> authenticationService.parseToken(""))
+                .isInstanceOf(InvalidAccessTokenException.class);
+    }
+
 }

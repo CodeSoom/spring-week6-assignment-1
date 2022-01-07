25d35bdffce3cac79284e542dddfb4c3fca3362c
Kwakcena <rhkrgudwh@gmail.com>
2022-01-08 03:19:42 +0900

    WrongPasswordException JavaDoc 작성
    잘못된 비밀번호로 요청할 경우 발생함을 명시한다

--- app/src/main/java/com/codesoom/assignment/errors/WrongPasswordException.java
+++ app/src/main/java/com/codesoom/assignment/errors/WrongPasswordException.java
@@ -1,5 +1,8 @@
 package com.codesoom.assignment.errors;
 
+/**
+ * 로그인 시 잘못된 비밀번호로 요청했을 경우 발생한다.
+ */
 public class WrongPasswordException extends RuntimeException {
     public WrongPasswordException() {
         super("Wrong password");

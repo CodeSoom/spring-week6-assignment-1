81c0efb13bb76afd07195102ce8d536ccf9ac2c9
Kwakcena <rhkrgudwh@gmail.com>
2022-01-08 03:13:16 +0900

    InvalidTokenException에 대한 JavaDoc 작성
    어떤 경우에 던지는지 설명한다

--- app/src/main/java/com/codesoom/assignment/errors/InvalidTokenException.java
+++ app/src/main/java/com/codesoom/assignment/errors/InvalidTokenException.java
@@ -1,5 +1,8 @@
 package com.codesoom.assignment.errors;
 
+/**
+ * 토큰이 유효하지 않거나, null, white space, 공백일 경우 던집니다.
+ */
 public class InvalidTokenException extends RuntimeException {
     public InvalidTokenException(String token) {
         super("Invalid access token : " + token);

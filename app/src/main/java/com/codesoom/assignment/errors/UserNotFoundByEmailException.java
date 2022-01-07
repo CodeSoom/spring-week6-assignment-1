6abe00d97f96d291d2235dd53ad4c9843489bbcf
Kwakcena <rhkrgudwh@gmail.com>
2022-01-08 03:17:04 +0900

    UserNotFoundByEmailException JavaDoc 작성
    UserNotFoundByEmailException JavaDoc을 작성하였습니다.

--- app/src/main/java/com/codesoom/assignment/errors/UserNotFoundByEmailException.java
+++ app/src/main/java/com/codesoom/assignment/errors/UserNotFoundByEmailException.java
@@ -1,5 +1,8 @@
 package com.codesoom.assignment.errors;
 
+/**
+ * email을 이용해 회원을 조회했을 때 찾지 못했을 경우 발생한다.
+ */
 public class UserNotFoundByEmailException extends RuntimeException {
     public UserNotFoundByEmailException(String email) {
         super("User not found: " + email);

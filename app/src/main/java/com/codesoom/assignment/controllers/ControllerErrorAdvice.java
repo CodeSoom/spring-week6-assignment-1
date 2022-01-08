6cd2f4619a0631136083b2ece674a09307151525
hyuk <pjh08190819@naver.com>
2022-01-09 01:42:20 +0900

    modify : LoginFailException 예외 구현

--- app/src/main/java/com/codesoom/assignment/controllers/ControllerErrorAdvice.java
+++ app/src/main/java/com/codesoom/assignment/controllers/ControllerErrorAdvice.java
@@ -2,6 +2,7 @@ package com.codesoom.assignment.controllers;
 
 import com.codesoom.assignment.dto.ErrorResponse;
 import com.codesoom.assignment.errors.InvalidTokenException;
+import com.codesoom.assignment.errors.LoginFailException;
 import com.codesoom.assignment.errors.ProductNotFoundException;
 import com.codesoom.assignment.errors.UserEmailDuplicationException;
 import com.codesoom.assignment.errors.UserNotFoundException;
@@ -35,8 +36,14 @@ public class ControllerErrorAdvice {
 
     @ResponseStatus(HttpStatus.UNAUTHORIZED)
     @ExceptionHandler(InvalidTokenException.class)
-    public void handleInvalidAccessTokenException() {
+    public ErrorResponse handleInvalidAccessTokenException() {
+        return new ErrorResponse("Invalid access token");
+    }
 
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(LoginFailException.class)
+    public ErrorResponse handleLoginFailException() {
+        return new ErrorResponse("Log-in fail");
     }
 
     @ExceptionHandler(MissingRequestHeaderException.class)

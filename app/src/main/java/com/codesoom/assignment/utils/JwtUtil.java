20afa58f425481ef41af36aa189fe056e879d867
장승철 <jangsc0000@gmail.com>
2022-01-09 11:47:26 +0900

    [추가] JwtUtil 구현 추가

--- app/src/main/java/com/codesoom/assignment/utils/JwtUtil.java
+++ app/src/main/java/com/codesoom/assignment/utils/JwtUtil.java
@@ -1,7 +1,9 @@
 package com.codesoom.assignment.utils;
 
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 
 import java.security.Key;
 
@@ -9,14 +11,21 @@ import java.security.Key;
  * JWT 토큰의 생성과, 디코딩을 담당합니다.
  */
 public class JwtUtil {
+
+    private final Key key;
+
+    public JwtUtil(@Value("${jwt.secret}") String secret) {
+        key = Keys.hmacShaKeyFor(secret.getBytes());
+    }
+
     /**
      * 유저 id를 입력받아 JWT 토큰을 생성하고 리턴합니다.
      *
      * @param id 유저 id
      * @return JWT Token
      */
-    public String encode(Long id){
-        return null;
+    public String encode(Long id) {
+        return Jwts.builder().claim("userId", 1L).signWith(key).compact();
     }
 
     /**
@@ -25,7 +34,7 @@ public class JwtUtil {
      * @param token JWT 토큰
      * @return Claims
      */
-    public Claims decode(String token){
-        return null;
+    public Claims decode(String token) {
+        return Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token).getBody();
     }
 }

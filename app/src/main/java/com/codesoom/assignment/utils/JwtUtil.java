f5eacecb21e2a25fffc5029285513e30d3933d18
SUBIN606 <gaemilee606@gmail.com>
2022-04-25 22:22:33 +0900

    JwtUtil 클래스 작성
    - JWT를 생성/검증 및 JWT 관련 역하을 담당할 클래스입니다.
    - 토큰 검증에 실패할 경우 던질 예외를 만듭니다.

---
+++ app/src/main/java/com/codesoom/assignment/utils/JwtUtil.java
@@ -0,0 +1,50 @@
+package com.codesoom.assignment.utils;
+
+import com.codesoom.assignment.application.auth.InvalidTokenException;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SignatureException;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.security.Key;
+
+public class JwtUtil {
+
+    private final Key KEY;
+    private static final String CLAIM_KEY = "userId";
+
+    public JwtUtil(@Value("${jwt.secret}") String secretKey) {
+        this.KEY = Keys.hmacShaKeyFor(secretKey.getBytes());
+    }
+
+    /**
+     * 인코딩 된 토큰을 생성해 반환합니다.
+     */
+    public String encode(Long id) {
+        return Jwts.builder()
+                .claim(CLAIM_KEY, id)
+                .signWith(KEY)
+                .compact();
+    }
+
+    /**
+     * 주어진 토큰의 유효성을 검증 후 Claims를 반환합니다.
+     *
+     * @param token 유효성을 검증할 토큰
+     * @return 유효한 토큰에 담긴 정보
+     * @throws InvalidTokenException 토큰 검증에 실패한 경우
+     */
+    public Claims decode(String token) {
+        try {
+            return Jwts.parserBuilder()
+                    .setSigningKey(KEY)
+                    .build()
+                    .parseClaimsJws(token)
+                    .getBody();
+        } catch (SignatureException e) {
+            throw new InvalidTokenException("유효하지 않은 토큰입니다.");
+        }
+    }
+
+}

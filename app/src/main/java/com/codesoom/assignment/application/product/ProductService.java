1d55da28d477a6563cd9e65458f72c83710eefcf
Lee Byoung Seong <dev.bslee@gmail.com>
2022-04-26 20:10:05 +0900

    refact:product DIP를 지킬수 있도록 역활(인터페이스)와 구현(클래스)로 구분

--- app/src/main/java/com/codesoom/assignment/application/product/ProductService.java
+++ app/src/main/java/com/codesoom/assignment/application/product/ProductService.java
@@ -1,60 +1,15 @@
 package com.codesoom.assignment.application.product;
 
 import com.codesoom.assignment.domain.product.Product;
-import com.codesoom.assignment.domain.product.ProductRepository;
 import com.codesoom.assignment.dto.product.ProductData;
-import com.codesoom.assignment.errors.ProductNotFoundException;
-import com.github.dozermapper.core.Mapper;
-import org.springframework.stereotype.Service;
 
-import javax.transaction.Transactional;
 import java.util.List;
 
-@Service
-@Transactional
-public class ProductService {
-    private final Mapper mapper;
-    private final ProductRepository productRepository;
-
-    public ProductService(
-            Mapper dozerMapper,
-            ProductRepository productRepository
-    ) {
-        this.mapper = dozerMapper;
-        this.productRepository = productRepository;
-    }
-
-    public List<Product> getProducts() {
-        return productRepository.findAll();
-    }
-
-    public Product getProduct(Long id) {
-        return findProduct(id);
-    }
-
-    public Product createProduct(ProductData productData) {
-        Product product = mapper.map(productData, Product.class);
-        return productRepository.save(product);
-    }
-
-    public Product updateProduct(Long id, ProductData productData) {
-        Product product = findProduct(id);
-
-        product.changeWith(mapper.map(productData, Product.class));
-
-        return product;
-    }
-
-    public Product deleteProduct(Long id) {
-        Product product = findProduct(id);
-
-        productRepository.delete(product);
-
-        return product;
-    }
-
-    private Product findProduct(Long id) {
-        return productRepository.findById(id)
-                .orElseThrow(() -> new ProductNotFoundException(id));
-    }
+public interface ProductService {
+    List<Product>  getProducts();
+    Product getProduct(ProductData.SearchOneProductRequest request);
+    Product createProduct(ProductData.CreateProductRequest request);
+    Product updateProduct(ProductData.UpdateProductRequest request);
+    Product deleteProduct(ProductData.RemoveProductRequest request);
+    Product findProduct(long productId);
 }
---
+++ app/src/main/java/com/codesoom/assignment/application/product/ProductServiceImpl.java
@@ -0,0 +1,66 @@
+package com.codesoom.assignment.application.product;
+
+import com.codesoom.assignment.domain.product.Product;
+import com.codesoom.assignment.domain.product.ProductRepository;
+import com.codesoom.assignment.dto.product.ProductData;
+import com.codesoom.assignment.errors.ProductNotFoundException;
+import com.github.dozermapper.core.Mapper;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Service
+@Transactional
+public class ProductServiceImpl implements ProductService  {
+    private final Mapper mapper;
+    private final ProductRepository productRepository;
+
+    public ProductServiceImpl(
+            Mapper dozerMapper,
+            ProductRepository productRepository
+    ) {
+        this.mapper = dozerMapper;
+        this.productRepository = productRepository;
+    }
+
+    public List<Product> getProducts() {
+        return productRepository.findAll();
+    }
+
+    @Override
+    public Product getProduct(ProductData.SearchOneProductRequest request) {
+        long productId = request.getId();
+        return findProduct(productId);
+    }
+
+    @Override
+    public Product createProduct(ProductData.CreateProductRequest request) {
+        Product product = request.toEntity();
+        return productRepository.save(product);
+    }
+
+    @Override
+    public Product updateProduct(ProductData.UpdateProductRequest request) {
+        long productId = request.getId();
+        Product product = findProduct(productId);
+        Product tobeProduct = request.toEntity();
+        product = product.changeWith(tobeProduct);
+
+        return product;
+    }
+
+    @Override
+    public Product deleteProduct(ProductData.RemoveProductRequest request) {
+        Product product = findProduct(request.getId());
+        productRepository.delete(product);
+        return product;
+    }
+
+    @Override
+    public Product findProduct(long productId) {
+        return productRepository.findById(productId)
+                .orElseThrow(() -> new ProductNotFoundException(productId));
+    }
+
+}

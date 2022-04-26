c70a2120cfae65b1dde4bee247682c0e9bb26d14
shineoov <shineoov88@gmail.com>
2022-04-26 13:31:48 +0900

    test: 로그인 성공 테스트 작성

---
+++ app/src/main/java/com/codesoom/assignment/controllers/SessionController.java
@@ -0,0 +1,16 @@
+package com.codesoom.assignment.controllers;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/session")
+public class SessionController {
+
+    @PostMapping
+    public String signin() {
+
+        return null;
+    }
+}
---
+++ app/src/test/java/com/codesoom/assignment/controllers/SessionControllerTest.java
@@ -0,0 +1,62 @@
+package com.codesoom.assignment.controllers;
+
+import com.codesoom.assignment.domain.User;
+import com.codesoom.assignment.domain.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@DisplayName("로그인 HTTP 요청")
+class SessionControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Nested
+    @DisplayName("POST - /session ")
+    class Describe_signin {
+
+        @Nested
+        @DisplayName("유효한 아이디와 패스워드가 주어진다면")
+        class Context_valid {
+
+            final User validUser = User.builder()
+                    .email("test@naver.com")
+                    .password("1234")
+                    .build();
+
+            @BeforeEach
+            void setUp() {
+                userRepository.save(validUser);
+            }
+
+            @Test
+            @DisplayName("토큰을 생성하고 토큰을 응답합니다. [201] ")
+            void it_response_201() throws Exception {
+                mockMvc.perform(post("/session")
+                                .contentType(MediaType.APPLICATION_CBOR)
+                                .content("\"email\": \"test@naver.com\", \"password\": \"1234\"")
+                        )
+                        .andDo(print())
+                        .andExpect(status().isCreated())
+                        .andExpect(jsonPath("accessToken").exists());
+            }
+        }
+    }
+}

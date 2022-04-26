d266871f794d7244cfd1b27a002fb6b355d43d9b
shineoov <shineoov88@gmail.com>
2022-04-26 17:45:17 +0900

    test: 로그인시 이메일이 없을 떄 실패 테스트

--- app/src/test/java/com/codesoom/assignment/controllers/SessionControllerTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/SessionControllerTest.java
@@ -28,6 +28,11 @@ class SessionControllerTest {
     @Autowired
     UserRepository userRepository;
 
+    @BeforeEach
+    void setUp() {
+        userRepository.deleteAll();
+    }
+
     @Nested
     @DisplayName("POST - /session ")
     class Describe_signin {
@@ -35,7 +40,6 @@ class SessionControllerTest {
         @Nested
         @DisplayName("유효한 아이디와 패스워드가 주어진다면")
         class Context_valid {
-
             final User validUser = User.builder()
                     .email("test@naver.com")
                     .password("1234")
@@ -56,6 +60,22 @@ class SessionControllerTest {
                         .andDo(print())
                         .andExpect(status().isCreated())
                         .andExpect(jsonPath("accessToken").exists());
+            }
+        }
+
+        @Nested
+        @DisplayName("이메일이 존재하지 않다면")
+        class Context_notExistEmail {
+
+            @Test
+            @DisplayName("Bad Request 를 응답한다. [400]")
+            void it_response_400() throws Exception {
+                mockMvc.perform(post("/session")
+                                .contentType(MediaType.APPLICATION_CBOR)
+                                .content("\"email\": \"test@naver.com\", \"password\": \"1234\"")
+                        )
+                        .andDo(print())
+                        .andExpect(status().isBadRequest());
             }
         }
     }

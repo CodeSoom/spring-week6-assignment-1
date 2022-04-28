76f4472f27d7a2b874563af49680871f84d8026a
Jeon Gyedong <jeongyedong@gmail.com>
2022-04-28 13:52:50 +0900

    JwtUtil 추가

---
+++ app/src/main/java/com/codesoom/assignment/utils/JwtUtil.java
@@ -0,0 +1,25 @@
+package com.codesoom.assignment.utils;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+
+@Component
+public class JwtUtil {
+    private final SecretKey key;
+
+    public JwtUtil(@Value("${jwt.secret}") String secret) {
+        key = Keys.hmacShaKeyFor(secret.getBytes());
+    }
+
+    public String encode(Long userId) {
+        String jws = Jwts.builder()
+                .claim("userId", userId)
+                .signWith(key)
+                .compact();
+        return jws;
+    }
+}

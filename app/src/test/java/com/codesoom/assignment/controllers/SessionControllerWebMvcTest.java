a2d008c83b9d3c5a6f536f43ee057672a34dab7a
5Hanui <gkdud644@naver.com>
2022-04-28 17:44:18 +0900

    SessionController mvc /session 테스트 작성

--- app/src/test/java/com/codesoom/assignment/controllers/SessionControllerWebMvcTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/SessionControllerWebMvcTest.java
@@ -2,20 +2,21 @@ package com.codesoom.assignment.controllers;
 
 import com.codesoom.assignment.application.AuthenticationService;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.hamcrest.Matchers.containsString;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(SessionController.class)
-class SessionControllerTest {
+@DisplayName("SessionController mvc 테스트")
+class SessionControllerWebMvcTest {
 
     @Autowired
     private MockMvc mockMvc;
@@ -28,6 +29,7 @@ class SessionControllerTest {
         given(authenticationService.login()).willReturn("a.b.c");
     }
     @Test
+    @DisplayName("[POST] /session 은 인증된 사용자에게 토큰을 생성하여 반환한다.")
     void login() throws Exception {
         mockMvc.perform(post("/session"))
                 .andExpect(status().isCreated())

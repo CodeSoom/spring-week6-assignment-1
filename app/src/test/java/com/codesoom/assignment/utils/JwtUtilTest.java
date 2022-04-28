82307b9429cd44a733324337fbdbaf4e3f77e368
subin606 <gaemilee606@gmail.com>
2022-04-28 14:27:03 +0900

    JwtUtil 테스트 수정
    - 유효하지 않은 토큰에 대한 테스트 케이스를 추가합니다.
    - `decode`메서드 테스트를 계층형 테스트 코드로 작성합니다.

--- app/src/test/java/com/codesoom/assignment/utils/JwtUtilTest.java
+++ app/src/test/java/com/codesoom/assignment/utils/JwtUtilTest.java
@@ -4,7 +4,11 @@ import com.codesoom.assignment.application.auth.InvalidTokenException;
 import io.jsonwebtoken.Claims;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -14,10 +18,8 @@ public class JwtUtilTest {
     private static final String SECRET_KEY = "NcRfUjXn2r5u8x/A?D(G+KbPdSgVkYp3";
     private JwtUtil jwtUtil;
 
-    private String VALID_TOKEN;
-    private static final String INVALID_TOKEN
-            = "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjF9.ze4dJmmF4peSe1uo9-ug019VAwzhr0WO8H3iHroSO890";
     private static final Long USER_ID = 1L;
+    private String VALID_TOKEN;
 
     @BeforeEach
     void setup() {
@@ -34,19 +36,52 @@ public class JwtUtilTest {
         assertThat(token).isEqualTo(VALID_TOKEN);
     }
 
-    @DisplayName("유효한 토큰이 주어지면 검증에 성공한다.")
-    @Test
-    void decodeWithValidToken() {
-        Claims claims = jwtUtil.decode(VALID_TOKEN);
+    @DisplayName("decode 메서드는")
+    @Nested
+    class Describe_decode {
 
-        assertThat(claims.get("userId", Long.class)).isEqualTo(USER_ID);
+        @DisplayName("유효한 토큰이 주어지면")
+        @Nested
+        class Context_with_valid_token {
+
+            private String VALID_TOKEN;
+
+            @BeforeEach
+            void setup() {
+                this.VALID_TOKEN = jwtUtil.encode(USER_ID);
+            }
+
+            @DisplayName("검증에 성공하며 claims를 반환한다.")
+            @Test
+            void it_returns_claims() {
+                Claims claims = jwtUtil.decode(VALID_TOKEN);
+
+                assertThat(claims.get("userId", Long.class)).isEqualTo(USER_ID);
+            }
+        }
+
+        @DisplayName("유효하지 않은 토큰이 주어지면")
+        @Nested
+        class Context_with_invalid_token {
+
+            @DisplayName("예외를 던진다.")
+            @ParameterizedTest(name = "[{index}] " +
+                    "{arguments} => {displayName}")
+            @NullAndEmptySource
+            @ValueSource(strings
+                    = {"eyJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjF9.ze4dJmmF4peSe1uo9-ug019VAwzhr0WO8H3iHroSO890"
+                    , "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjF9"
+                    , "eyJhbGciOiJIUzI1NiJ9"
+                    , "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjEf.3290392!@#"})
+            void it_throws_invalid_token_exception(String invalidToken) {
+                assertThatThrownBy(() -> jwtUtil.decode(invalidToken))
+                        .isInstanceOf(InvalidTokenException.class);
+            }
+        }
     }
 
-    @DisplayName("유효하지 않은 토큰이 주어지면 예외를 던진다.")
-    @Test
-    void decodeWithInvalidToken() {
-        assertThatThrownBy(() -> jwtUtil.decode(INVALID_TOKEN))
-                .isInstanceOf(InvalidTokenException.class);
-    }
+
+
+
 
 }

3a7acade993d428a812c2ec5665db3319fbd30f0
Jeon Gyedong <jeongyedong@gmail.com>
2022-04-29 21:15:12 +0900

    "로그인 테스트" DisplayName 추가

--- app/src/test/java/com/codesoom/assignment/controllers/SessionControllerTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/SessionControllerTest.java
@@ -2,11 +2,10 @@ package com.codesoom.assignment.controllers;
 
 import com.codesoom.assignment.application.AuthenticationService;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -30,6 +29,7 @@ class SessionControllerTest {
     }
 
     @Test
+    @DisplayName("로그인 테스트")
     void login() throws Exception {
         mockMvc.perform(post("/session")).andExpect(status().isCreated())
                 .andExpect(content().string(containsString(".")));

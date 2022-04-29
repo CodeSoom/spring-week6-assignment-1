2bda48bd58495ced5c588d6ce0453b9161b34c64
Jeon Gyedong <jeongyedong@gmail.com>
2022-04-29 23:16:48 +0900

    UserService에 이메일로 사용자 찾기 추가 구현

--- app/src/main/java/com/codesoom/assignment/application/UserService.java
+++ app/src/main/java/com/codesoom/assignment/application/UserService.java
@@ -51,4 +51,9 @@ public class UserService {
         return userRepository.findByIdAndDeletedIsFalse(id)
                 .orElseThrow(() -> new UserNotFoundException(id));
     }
+
+    public User findUserByEmail(String email){
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new UserNotFoundException(email));
+    }
 }

03906fdd95fd714a3d95d0432aef576574856f78
Jeon Gyedong <jeongyedong@gmail.com>
2022-04-29 21:11:37 +0900

    VALID_TOKEN을 사용해서 INVALID_TOKEN 표현 다시 구현

--- app/src/test/java/com/codesoom/assignment/utils/JwtUtilTest.java
+++ app/src/test/java/com/codesoom/assignment/utils/JwtUtilTest.java
@@ -2,7 +2,6 @@ package com.codesoom.assignment.utils;
 
 import com.codesoom.assignment.errors.InvalidTokenException;
 import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.security.SignatureException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -13,7 +12,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 class JwtUtilTest {
     private static final String SECRET = "12345678901234567890123456789012";
     private static final String VALID_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjF9.ZZ3CUl0jxeLGvQ1Js5nG2Ty5qGTlqai5ubDMXZOdaDk";
-    private static final String INVALID_TOKEN = VALID_TOKEN + "!";
+    private static final String INVALID_TOKEN = VALID_TOKEN.substring(0, VALID_TOKEN.length() - 1) + "Z";
     private JwtUtil jwtUtil;
 
     @BeforeEach

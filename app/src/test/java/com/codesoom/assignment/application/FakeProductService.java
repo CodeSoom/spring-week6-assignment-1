869e1d1b3455bce3d393a7465b2e107ea7c46f65
Lee Byoung Seong <dev.bslee@gmail.com>
2022-04-30 18:04:21 +0900

    feature:in memory test를 위한 fake객체 생성

---
+++ app/src/test/java/com/codesoom/assignment/application/FakeProductService.java
@@ -0,0 +1,47 @@
+package com.codesoom.assignment.application;
+
+import com.codesoom.assignment.application.product.ProductService;
+import com.codesoom.assignment.domain.product.Product;
+import com.codesoom.assignment.dto.product.CreateProductRequest;
+import com.codesoom.assignment.dto.product.RemoveProductRequest;
+import com.codesoom.assignment.dto.product.SearchOneProductRequest;
+import com.codesoom.assignment.dto.product.UpdateProductRequest;
+import com.codesoom.assignment.infra.FakeProductStorage;
+
+import java.util.List;
+
+public class FakeProductService implements ProductService {
+    private FakeProductStorage fakeProductStorage = new FakeProductStorage();
+
+    @Override
+    public List<Product> getProducts() {
+        return fakeProductStorage.findAll();
+    }
+
+    @Override
+    public Product getProduct(SearchOneProductRequest request) {
+        return fakeProductStorage.findById(request.getProductId()).get();
+    }
+
+    @Override
+    public Product createProduct(CreateProductRequest request) {
+        Product product = request.entityMaker();
+        return fakeProductStorage.save(product);
+    }
+
+    @Override
+    public Product updateProduct(UpdateProductRequest request) {
+        return null;
+    }
+
+    @Override
+    public void deleteProduct(RemoveProductRequest request) {
+        Product product = findProduct(request.getProductId());
+        fakeProductStorage.delete(product);
+    }
+
+    @Override
+    public Product findProduct(long productId) {
+        return fakeProductStorage.findById(productId).get();
+    }
+}

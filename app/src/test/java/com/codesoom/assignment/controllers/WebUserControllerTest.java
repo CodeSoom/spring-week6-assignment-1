1dd6025dc84cd7797e7ebd04edcb9f8c329886f3
shineoov <shineoov88@gmail.com>
2022-05-01 15:47:04 +0900

    style: 사용하지 않은 import 제거

--- app/src/test/java/com/codesoom/assignment/controllers/WebUserControllerTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/WebUserControllerTest.java
@@ -13,7 +13,6 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest

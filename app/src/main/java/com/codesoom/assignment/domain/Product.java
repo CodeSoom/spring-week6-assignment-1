dcbd6f0d085be896a01caa760ed03041e2306725
HanJiyoung <jiyounghaan@gmail.com>
2022-06-07 02:41:55 +0900

    불필요한 주석을 제거하라

--- app/src/main/java/com/codesoom/assignment/domain/Product.java
+++ app/src/main/java/com/codesoom/assignment/domain/Product.java
@@ -1,19 +1,3 @@
-// 고양이 장난감 쇼핑몰
-// Product 모델
-// User 모델
-// Order 모델
-// ... 모델
-// Application (UseCase)
-// Product -> 관리자 등록/수정/삭제 -> list/detail
-// 주문 -> 확인 -> 배송 등 처리
-
-// Product
-// 0. 식별자 - identifier (ID)
-// 1. 이름 - 쥐돌이
-// 2. 제조사 - 냥이월드
-// 3. 가격 - 5,000원 (판매가)
-// 4. 이미지 - static, CDN => image URL
-
 package com.codesoom.assignment.domain;
 
 import lombok.AllArgsConstructor;

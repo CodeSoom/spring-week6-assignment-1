1bfea191efe010a433f204cbf6bfb339cab0cbcf
seunglo <devseung9056@gmail.com>
2022-06-07 16:48:56 +0900

    login시 이메일이 존재하지 않을 때 예외를 던지도록 구현해라

--- app/src/main/java/com/codesoom/assignment/application/AuthService.java
+++ app/src/main/java/com/codesoom/assignment/application/AuthService.java
@@ -1,7 +1,10 @@
 package com.codesoom.assignment.application;
 
+import com.codesoom.assignment.domain.User;
+import com.codesoom.assignment.domain.UserRepository;
 import com.codesoom.assignment.dto.LoginData;
 import com.codesoom.assignment.dto.LoginResult;
+import com.codesoom.assignment.errors.UserEmailNotFoundException;
 import com.codesoom.assignment.helper.AuthJwtHelper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -11,9 +14,17 @@ import org.springframework.stereotype.Service;
 public class AuthService {
 
     private final AuthJwtHelper authJwtHelper;
+    private final UserRepository userRepository;
 
     public LoginResult login(LoginData loginData) {
-        String accessToken = authJwtHelper.encode(1L);
+        User user = userRepository.findByEmail(loginData.getEmail())
+                .orElseThrow(() -> new UserEmailNotFoundException(loginData.getEmail()));
+
+
+
+
+        String accessToken = authJwtHelper.encode(user.getId());
+
         return new LoginResult(accessToken);
     }
 }

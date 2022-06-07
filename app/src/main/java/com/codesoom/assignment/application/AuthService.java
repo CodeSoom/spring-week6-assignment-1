753862d85f8884b5480d1f04042bbef09408ffe3
seunglo <devseung9056@gmail.com>
2022-06-07 17:01:25 +0900

    login시 패스워드가 틀린경우 예외를 던지도록 구현 해라

--- app/src/main/java/com/codesoom/assignment/application/AuthService.java
+++ app/src/main/java/com/codesoom/assignment/application/AuthService.java
@@ -4,6 +4,7 @@ import com.codesoom.assignment.domain.User;
 import com.codesoom.assignment.domain.UserRepository;
 import com.codesoom.assignment.dto.LoginData;
 import com.codesoom.assignment.dto.LoginResult;
+import com.codesoom.assignment.errors.BadPasswordException;
 import com.codesoom.assignment.errors.UserEmailNotFoundException;
 import com.codesoom.assignment.helper.AuthJwtHelper;
 import lombok.RequiredArgsConstructor;
@@ -20,8 +21,9 @@ public class AuthService {
         User user = userRepository.findByEmail(loginData.getEmail())
                 .orElseThrow(() -> new UserEmailNotFoundException(loginData.getEmail()));
 
-
-
+        if (!user.getPassword().equals(loginData.getPassword())) {
+            throw new BadPasswordException("패스워드가 일치하지 않습니다.");
+        }
 
         String accessToken = authJwtHelper.encode(user.getId());
 

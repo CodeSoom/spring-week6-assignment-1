99522bcf6acb1ec80e0eb04d51d2269c37672665
seunglo <devseung9056@gmail.com>
2022-06-07 21:44:23 +0900

    AuthJwtHelper 생성자 value 어노테이션을 올바르게 변경해라

--- app/src/main/java/com/codesoom/assignment/helper/AuthJwtHelper.java
+++ app/src/main/java/com/codesoom/assignment/helper/AuthJwtHelper.java
@@ -4,14 +4,16 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
 import java.nio.charset.StandardCharsets;
 import java.security.Key;
 
+@Component
 public class AuthJwtHelper {
     private final Key key;
 
-    public AuthJwtHelper(@Value("{jwt.secret}") String secret) {
+    public AuthJwtHelper(@Value("${jwt.secret}") String secret) {
         this.key = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
     }
 

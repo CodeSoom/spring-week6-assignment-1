6c58e097f9cc67749a4f31ffc7ba1662838dddb9
seunglo <devseung9056@gmail.com>
2022-06-07 15:38:17 +0900

    AuthService 로그인 테스트만 성공하는 기능을 구현해라

---
+++ app/src/main/java/com/codesoom/assignment/application/AuthService.java
@@ -0,0 +1,10 @@
+package com.codesoom.assignment.application;
+
+import com.codesoom.assignment.dto.LoginData;
+import com.codesoom.assignment.dto.LoginResult;
+
+public class AuthService {
+    public LoginResult login(LoginData loginData) {
+        return new LoginResult("asd.asd.sad");
+    }
+}

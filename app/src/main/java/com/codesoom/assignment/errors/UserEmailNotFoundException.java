7ab672b12308f63beaf18d365fd75f868e8aea85
seunglo <devseung9056@gmail.com>
2022-06-07 17:54:38 +0900

    UserEmailNotFoundException에 ResponseStatus 어노테이션을 추가해라

--- app/src/main/java/com/codesoom/assignment/errors/UserEmailNotFoundException.java
+++ app/src/main/java/com/codesoom/assignment/errors/UserEmailNotFoundException.java
@@ -1,5 +1,9 @@
 package com.codesoom.assignment.errors;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "이메일이 존재하지 않습니다 다시 확인해주세요")
 public class UserEmailNotFoundException extends RuntimeException {
 
     public UserEmailNotFoundException(String email) {

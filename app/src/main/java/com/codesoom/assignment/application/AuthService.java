470d53e129644fd6cca54a02571b844c0e700e85
seunglo <devseung9056@gmail.com>
2022-06-08 23:21:13 +0900

    로그인 실패시 AuthenticationException을 던지도록 구현

--- app/src/main/java/com/codesoom/assignment/application/AuthService.java
+++ app/src/main/java/com/codesoom/assignment/application/AuthService.java
@@ -4,8 +4,7 @@ import com.codesoom.assignment.domain.User;
 import com.codesoom.assignment.domain.UserRepository;
 import com.codesoom.assignment.dto.LoginData;
 import com.codesoom.assignment.dto.LoginResult;
-import com.codesoom.assignment.errors.BadPasswordException;
-import com.codesoom.assignment.errors.UserEmailNotFoundException;
+import com.codesoom.assignment.errors.AuthenticationException;
 import com.codesoom.assignment.helper.AuthJwtHelper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -19,10 +18,11 @@ public class AuthService {
 
     public LoginResult login(LoginData loginData) {
         User user = userRepository.findByEmail(loginData.getEmail())
-                .orElseThrow(() -> new UserEmailNotFoundException(loginData.getEmail()));
+                .orElseThrow(() ->
+                        new AuthenticationException(loginData.getEmail() + "은 존재하지 않는 이메일입니다."));
 
         if (!user.getPassword().equals(loginData.getPassword())) {
-            throw new BadPasswordException("패스워드가 일치하지 않습니다.");
+            throw new AuthenticationException("패스워드가 일치하지 않습니다.");
         }
 
         String accessToken = authJwtHelper.encode(user.getId());

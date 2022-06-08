2ad6f4c60efe8edebd776d79b8bed0d40ec99a1e
MinJoonLee <52944973+mj950425@users.noreply.github.com>
2022-06-08 13:12:27 +0900

    test : 실패하는 login test 코드 작성

---
+++ app/src/test/java/com/codesoom/assignment/controllers/SessionControllerTest.java
@@ -0,0 +1,26 @@
+package com.codesoom.assignment.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(SessionController.class)
+class SessionControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void loginTest() throws Exception {
+        mockMvc.perform(post("/session"))
+                .andExpect(status().isCreated())
+                .andExpect(content().string(containsString(".'")));
+    }
+
+}

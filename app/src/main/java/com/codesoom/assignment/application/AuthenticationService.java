bcd62e03db33b6e24ef0087d7c3a3a265344c976
minjoon_lee <minjoon1995@naver.com>
2022-06-09 21:49:52 +0900

    feat : AuthenticationService decode 구현

--- app/src/main/java/com/codesoom/assignment/application/AuthenticationService.java
+++ app/src/main/java/com/codesoom/assignment/application/AuthenticationService.java
@@ -1,21 +1,31 @@
 package com.codesoom.assignment.application;
 
-import com.codesoom.assignment.dto.SessionResponseData;
-import com.codesoom.assignment.utils.JwtUtil;
 import org.springframework.stereotype.Service;
+
+import com.codesoom.assignment.dto.SessionResponseData;
+import com.codesoom.assignment.errors.DecodingInValidTokenException;
+import com.codesoom.assignment.utils.JwtUtil;
 
 @Service
 public class AuthenticationService {
-    private JwtUtil jwtUtil;
+	private JwtUtil jwtUtil;
 
-    public AuthenticationService(JwtUtil jwtUtil) {
-        this.jwtUtil = jwtUtil;
-    }
+	public AuthenticationService(JwtUtil jwtUtil) {
+		this.jwtUtil = jwtUtil;
+	}
 
-    public SessionResponseData login(Long userId) {
-        return SessionResponseData
-                .builder()
-                .accessToken(jwtUtil.encode(userId))
-                .build();
-    }
+	public SessionResponseData login(Long userId) {
+		return SessionResponseData
+			.builder()
+			.accessToken(jwtUtil.encode(userId))
+			.build();
+	}
+
+	public Long decode(String token) {
+		try {
+			return jwtUtil.decode(token).get("userId", Long.class);
+		} catch (Exception e) {
+			throw new DecodingInValidTokenException(token);
+		}
+	}
 }

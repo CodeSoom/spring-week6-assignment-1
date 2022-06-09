1bc9e86957f0faa5973aa2ea065e67012f987794
minjoon_lee <minjoon1995@naver.com>
2022-06-09 21:49:08 +0900

    test : 실패하는 decoding 테스트 코드 추가

--- app/src/test/java/com/codesoom/assignment/application/AuthenticationServiceTest.java
+++ app/src/test/java/com/codesoom/assignment/application/AuthenticationServiceTest.java
@@ -1,26 +1,43 @@
 package com.codesoom.assignment.application;
 
-import com.codesoom.assignment.dto.SessionResponseData;
-import com.codesoom.assignment.utils.JwtUtil;
+import static org.assertj.core.api.Assertions.*;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
+import com.codesoom.assignment.dto.SessionResponseData;
+import com.codesoom.assignment.errors.DecodingInValidTokenException;
+import com.codesoom.assignment.utils.JwtUtil;
 
 public class AuthenticationServiceTest {
-    private AuthenticationService authenticationService;
-    private static String secret = "123456789012345678901234567890123456789012";
+	private AuthenticationService authenticationService;
+	private JwtUtil jwtUtil;
+	private static String secret = "123456789012345678901234567890123456789012";
+	private static String INVALID_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjF9.TysUfytUgm0Kc6zzhOwJPDes3U48wtv9S3qSwnKkkvo1";
+	private static String VALID_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjF9.TysUfytUgm0Kc6zzhOwJPDes3U48wtv9S3qSwnKkkvo";
 
-    @BeforeEach
-    void setUp() {
-        JwtUtil jwtUtil = new JwtUtil(secret);
-        authenticationService = new AuthenticationService(jwtUtil);
-    }
+	@BeforeEach
+	void setUp() {
+		jwtUtil = new JwtUtil(secret);
+		authenticationService = new AuthenticationService(jwtUtil);
+	}
 
-    @Test
-    void login() {
-        SessionResponseData sessionResponseData = authenticationService.login(1L);
-        assertThat(sessionResponseData.getAccessToken()).isEqualTo("eyJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjF9.4VSrshpJYgISpBrS2VeEk_uwz_djqdjBbcbaT0yfE9M");
-    }
+	@Test
+	void login() {
+		SessionResponseData sessionResponseData = authenticationService.login(1L);
+		assertThat(sessionResponseData.getAccessToken()).isEqualTo(
+			"eyJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjF9.4VSrshpJYgISpBrS2VeEk_uwz_djqdjBbcbaT0yfE9M");
+	}
+
+	@Test
+	void decodeWithValidToken() {
+		Long userId = authenticationService.decode(VALID_TOKEN);
+		assertThat(userId).isEqualTo(1L);
+	}
+
+	@Test
+	void decodeWithInValidToken() {
+		assertThatThrownBy(() -> authenticationService.decode(INVALID_TOKEN)).isInstanceOf(
+			DecodingInValidTokenException.class);
+	}
 }

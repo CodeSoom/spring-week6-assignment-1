c8d6b34c0ecd64af39c86b24db3fe269a5a7ba9f
MJbae <bmj5208@gmail.com>
2022-06-09 20:49:40 +0900

    SecretKey 인터페이스에 대한 주석 추가

--- app/src/main/java/com/codesoom/assignment/auth/SecretKey.java
+++ app/src/main/java/com/codesoom/assignment/auth/SecretKey.java
@@ -2,6 +2,19 @@ package com.codesoom.assignment.auth;
 
 import java.security.Key;
 
+/**
+ * 해쉬 알고리즘이 적용된 비밀키를 반환한다
+ * <p>
+ * All Known Extending Classes:
+ * JwtKey
+ * </p>
+ */
 public interface SecretKey {
+    /**
+     * 해쉬 알고리즘이 적용된 비밀키를 반환한다
+     * <p>
+     * @return Key 타입의 해쉬화된 비밀키
+     * </p>
+     */
     Key hashed();
 }

9d574af79a2cc0fc65d8ee81764633a6e2ba110f
minjoon_lee <minjoon1995@naver.com>
2022-06-09 21:11:48 +0900

    feat : JwtUtils 의 decode 메소드 구현

--- app/src/main/java/com/codesoom/assignment/utils/JwtUtil.java
+++ app/src/main/java/com/codesoom/assignment/utils/JwtUtil.java
@@ -1,21 +1,27 @@
 package com.codesoom.assignment.utils;
 
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.security.Keys;
+import java.security.Key;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import java.security.Key;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 
 @Component
 public class JwtUtil {
-    private final Key key;
+	private final Key key;
 
-    public JwtUtil(@Value("${jwt.secret}") String secret) {
-        this.key = Keys.hmacShaKeyFor(secret.getBytes());
-    }
+	public JwtUtil(@Value("${jwt.secret}") String secret) {
+		this.key = Keys.hmacShaKeyFor(secret.getBytes());
+	}
 
-    public String encode(Long userId) {
-        return Jwts.builder().claim("userId", userId).signWith(key).compact();
-    }
+	public String encode(Long userId) {
+		return Jwts.builder().claim("userId", userId).signWith(key).compact();
+	}
+
+	public Claims decode(String token) {
+		return Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token).getBody();
+	}
 }
--- app/src/test/java/com/codesoom/assignment/utils/JwtUtilTest.java
+++ app/src/test/java/com/codesoom/assignment/utils/JwtUtilTest.java
@@ -5,6 +5,8 @@ import static org.assertj.core.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import io.jsonwebtoken.Claims;
+
 public class JwtUtilTest {
 	private static String validToken = "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjF9.TysUfytUgm0Kc6zzhOwJPDes3U48wtv9S3qSwnKkkvo";
 	private static String SECRET = "12345678912345678912345678900012";
@@ -17,13 +19,11 @@ public class JwtUtilTest {
 
 	@Test
 	void encode() {
-		String jwt = jwtUtil.encode(1L);
-		assertThat(jwt).isEqualTo(validToken);
+		assertThat(jwtUtil.encode(1L)).isEqualTo(validToken);
 	}
 
 	@Test
 	void decode() {
-		Long userId = jwtUtil.parseUserId(validToken);
-		assertThat(userId).isEqualTo(1L);
+		assertThat(jwtUtil.decode(validToken).get("userId", Long.class)).isEqualTo(1L);
 	}
 }

c3bf9fa5c8cf04949947c032031b9c8d71e938cf
minjoon_lee <minjoon1995@naver.com>
2022-06-09 21:00:52 +0900

    test : JWTUtils 실패하는 parseUserId 코드 추가

--- app/src/test/java/com/codesoom/assignment/utils/JwtUtilTest.java
+++ app/src/test/java/com/codesoom/assignment/utils/JwtUtilTest.java
@@ -1,22 +1,29 @@
 package com.codesoom.assignment.utils;
 
+import static org.assertj.core.api.Assertions.*;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 public class JwtUtilTest {
-    private static String SECRET = "12345678912345678912345678900012";
-    private JwtUtil jwtUtil;
+	private static String validToken = "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjF9.TysUfytUgm0Kc6zzhOwJPDes3U48wtv9S3qSwnKkkvo";
+	private static String SECRET = "12345678912345678912345678900012";
+	private JwtUtil jwtUtil;
 
-    @BeforeEach
-    void setUp() {
-        jwtUtil = new JwtUtil(SECRET);
-    }
+	@BeforeEach
+	void setUp() {
+		jwtUtil = new JwtUtil(SECRET);
+	}
 
-    @Test
-    void encode() {
-        String jwt = jwtUtil.encode(1L);
-        assertThat(jwt).contains(".");
-    }
+	@Test
+	void encode() {
+		String jwt = jwtUtil.encode(1L);
+		assertThat(jwt).isEqualTo(validToken);
+	}
+
+	@Test
+	void decode() {
+		Long userId = jwtUtil.parseUserId(validToken);
+		assertThat(userId).isEqualTo(1L);
+	}
 }

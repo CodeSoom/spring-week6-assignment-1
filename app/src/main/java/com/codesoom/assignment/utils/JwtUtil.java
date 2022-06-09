633ba3e51c1314643917ac406d1237c53444643c
yongjunleeme <chesidyong@gmail.com>
2022-06-09 17:10:51 +0900

    JwtUtil의 decode 메서드를 추가하라

--- app/src/main/java/com/codesoom/assignment/utils/JwtUtil.java
+++ app/src/main/java/com/codesoom/assignment/utils/JwtUtil.java
@@ -6,9 +6,7 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import io.jsonwebtoken.security.SignatureException;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
 
-import java.nio.charset.StandardCharsets;
 import java.security.Key;
 import java.util.Date;
 
@@ -45,5 +43,28 @@ public class JwtUtil {
 
     private Date calculationExpiration(Date now, long validTime) {
         return new Date(now.getTime() + validTime);
+    }
+
+    /**
+     * 주어진 토큰을 복호화한 정보를 리턴한다.
+     *
+     * @param token - 토큰
+     * @return 복호화한 정보
+     * @throws InvalidTokenException 토큰이 유효하지 않을 경우
+     */
+    public Claims decode(String token) throws InvalidTokenException {
+        if (token == null || token.isBlank()) {
+            throw new InvalidTokenException(token);
+        }
+
+        try {
+            return Jwts.parserBuilder()
+                    .setSigningKey(key)
+                    .build()
+                    .parseClaimsJws(token)
+                    .getBody();
+        } catch (SignatureException e) {
+            throw new InvalidTokenException(token);
+        }
     }
 }
--- app/src/test/java/com/codesoom/assignment/utils/JwtUtilTest.java
+++ app/src/test/java/com/codesoom/assignment/utils/JwtUtilTest.java
@@ -1,16 +1,34 @@
 package com.codesoom.assignment.utils;
 
+import com.codesoom.assignment.errors.InvalidTokenException;
 import io.jsonwebtoken.Claims;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Date;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class JwtUtilTest {
 
     private final String SECRET = "12345678901234567890123456789012";
     private final long VALID_TIME = 60 * 60 * 1000;
+
+    private final String VALID_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjEsImlh" +
+            "dCI6MTY1NDc1NzM3MywiZXhwIjoxNjU0NzYwOTczfQ.eZceGfyNpy3FlVyzaxeIZashF-CUhcwYTDsS0zBZyVk";
+
+    private final String INVALID_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjEsIml" +
+            "hdCI6MTY1NDc1NzIxNSwiZXhwIjoxNjU0NzYwODE1fQ.NI_o2dC-go7A_IEJ34LOZPw5Ohg_KcVa48UUHDsKUpl";
+
     private final Long USER_ID = 1L;
 
     private JwtUtil jwtUtil;
@@ -28,4 +46,35 @@ class JwtUtilTest {
         assertThat(token).contains(".");
     }
 
+    @DisplayName("decode 메서드에 유효한 토큰이 주어지면 클레임을 리턴한다.")
+    @Test
+    void decodeWithValidToken() {
+        Claims claims = jwtUtil.decode(VALID_TOKEN);
+
+        assertThat(claims.get("userId", Long.class)).isEqualTo(USER_ID);
+    }
+
+    @DisplayName("decode 메서드에 유효하지 않은 토큰이 주어지면 예외를 던진다.")
+    @ParameterizedTest
+    @NullSource
+    @ValueSource(strings = {"", "  "})
+    @CsvSource(INVALID_TOKEN)
+    void decodeWithInvalidToken(String token) {
+        assertThrows(InvalidTokenException.class, () -> jwtUtil.decode(token));
+    }
+
+    @DisplayName("calculationExpiration 메서드에 현재 시간과 유효 기간이 주어지면 만료시간을 리턴한다.")
+    @Test
+    void calculateExpiration() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        Method method = jwtUtil.getClass()
+                .getDeclaredMethod("calculationExpiration", Date.class, long.class);
+
+        method.setAccessible(true);
+
+        Date now = new Date();
+        Date expiration = (Date) method.invoke(jwtUtil, now, VALID_TIME);
+
+        assertThat(expiration).isAfter(now);
+        assertThat(expiration.getTime()).isEqualTo(now.getTime() + VALID_TIME);
+    }
 }

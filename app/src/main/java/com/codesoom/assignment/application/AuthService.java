a7c0209b7b0c676463e031476035f5705ada657c
seunglo <devseung9056@gmail.com>
2022-06-09 19:57:19 +0900

    주어진 토큰이 올바른 유저인가 검증하는 verify 메서드 구현

--- app/src/main/java/com/codesoom/assignment/application/AuthService.java
+++ app/src/main/java/com/codesoom/assignment/application/AuthService.java
@@ -4,8 +4,12 @@ import com.codesoom.assignment.domain.User;
 import com.codesoom.assignment.domain.UserRepository;
 import com.codesoom.assignment.dto.LoginData;
 import com.codesoom.assignment.dto.LoginResult;
+import com.codesoom.assignment.dto.Authentication;
 import com.codesoom.assignment.errors.AuthenticationException;
+import com.codesoom.assignment.errors.InvalidTokenException;
+import com.codesoom.assignment.errors.VerificationException;
 import com.codesoom.assignment.helper.AuthJwtHelper;
+import io.jsonwebtoken.Claims;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -28,5 +32,27 @@ public class AuthService {
         String accessToken = authJwtHelper.encode(user.getId());
 
         return new LoginResult(accessToken);
+    }
+
+    /**
+     * @throws VerificationException 주어진 토큰으로 검증 실패할 때 던지는 예외
+     */
+    public Authentication verify(String token) {
+        Long userId;
+
+        try {
+            Claims claims = authJwtHelper.decode(token);
+            userId = claims.get("userId", Long.class);
+        } catch (InvalidTokenException e) {
+            throw new VerificationException(e.getMessage(), e);
+        }
+
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new VerificationException("아이디가 존재하지 않습니다."));
+
+        return Authentication.builder()
+                .email(user.getEmail())
+                .id(user.getId())
+                .build();
     }
 }

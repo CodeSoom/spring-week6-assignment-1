b770a93e429ef25d6d148c87d22b6e0255e56bf6
caoyu-dev <choyumin01@gmail.com>
2022-06-10 01:14:59 +0900

    SessionControllerTest 를 작성했습니다 해당 Test 를 토대로 jwt 인가 과정을 진행할 예정입니다

--- app/src/test/java/com/codesoom/assignment/controllers/SessionControllerTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/SessionControllerTest.java
@@ -2,57 +2,130 @@ package com.codesoom.assignment.controllers;
 
 import com.codesoom.assignment.Utf8WebTest;
 import com.codesoom.assignment.domain.User;
+import com.codesoom.assignment.domain.UserRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-//@WebMvcTest(SessionController.class)
-//@Utf8WebTest
-//@DisplayName("SessionController 클래스")
-//class SessionControllerTest {
-//    @Autowired
-//    private MockMvc mockMvc;
-//
-//    @MockBean
-//    private AuthenticationService authenticationService;
-//
-//    @Autowired
-//    private JwtUtil jwtUtil;
-//
-//    private ObjectMapper objectMapper;
-//
-//    private final SessionRequestData sessionRequestData;
-//
-//    @BeforeEach
-//    void setUp() {
-//        sessionRequestData.builer()
-//                .email("choyumin01@gmail.com")
-//                .password("1234!@#$")
-//                .build();
-//    }
-//
-//    @Nested
-//    @DisplayName("[POST] /session")
-//    class Describe_post {
-//
-//        @Nested
-//        @DisplayName("등록된 user 의 sessionRequestData 가 주어지면")
-//        class Context_with_valid_sessionRequestData {
-//
-//            @BeforeEach
-//            void setUp() {
-//                User user = User.builder()
-//                        .email(sessionRequestData.getEmail())
-//                        .password(sessionRequestData.getPassword())
-//                        .build();
-//
-//
-//            }
-//        }
-//    }
-//}
+import static org.hamcrest.Matchers.matchesPattern;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(SessionController.class)
+@Utf8WebTest
+@DisplayName("SessionController 클래스")
+class SessionControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    private ObjectMapper objectMapper;
+    private final String JWT_REGEX = "^[A-Za-z0-9-_=]+\\.[A-Za-z0-9-_=]+\\.[A-Za-z0-9-_.+/=]*$";
+    private final String EXIST_EMAIL = "codesoom1@gmail.com";
+    private final String NOT_EXIST_EMAIL = "codesoom2@gmail.com";
+    private final String VALID_PASSWORD = "1234!@#$";
+    private final String INVALID_PASSWORD = "!@#$1234";
+
+    @BeforeEach
+    void setUp() {
+        userRepository.save(User.builder()
+                .id(1L)
+                .email(EXIST_EMAIL)
+                .password(VALID_PASSWORD)
+                .build());
+    }
+
+    @Nested
+    @DisplayName("[POST] /session")
+    class Describe_post {
+
+        private UserLoginData userLoginData;
+
+        @Nested
+        @DisplayName("등록된 user 의 유효한 로그인 요청이 주어진다면")
+        class Context_with_valid_userLoginData {
+
+            @BeforeEach
+            void setUp() {
+                userLoginData = userLoginData.builder()
+                        .email(EXIST_EMAIL)
+                        .password(VALID_PASSWORD)
+                        .build();
+            }
+
+            @Test
+            @DisplayName("201 을 응답하고 액세스 토큰을 반환한다.")
+            void It_responses_201_and_returns_accessToken() throws Exception {
+                mockMvc.perform(
+                        post("/session")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(toJson(userLoginData))
+                )
+                        .andExpect(status().isCreated())
+                        .andExpect(jsonPath("$.accessToken", matchesPattern(JWT_REGEX)));
+            }
+        }
+
+        @Nested
+        @DisplayName("등록된 user 의 email 과 일치하지 않으면")
+        class Context_with_userLoginData_with_not_existed_email {
+
+            private UserLoginData UserLoginDataWithNotExistedEmail;
+
+            @BeforeEach
+            void setUp() {
+                UserLoginDataWithNotExistedEmail = userLoginData.builder()
+                        .email(NOT_EXIST_EMAIL)
+                        .password(VALID_PASSWORD)
+                        .build();
+            }
+
+            @Test
+            @DisplayName("404 를 응답한다.")
+            void It_responses_404() throws Exception {
+                mockMvc.perform(
+                                post("/session")
+                                        .contentType(MediaType.APPLICATION_JSON)
+                                        .content(toJson(userLoginData))
+                        )
+                        .andExpect(status().isNotFound());
+            }
+        }
+
+        @Nested
+        @DisplayName("등록된 user 의 패스워드와 일치하지 않으면")
+        class Context_with_userLoginData_with_invalid_password {
+
+            private UserLoginData userLoginDataWithInvalidPassword;
+
+            @BeforeEach
+            void setUp() {
+                userLoginDataWithInvalidPassword = userLoginData.builder()
+                        .email(EXIST_EMAIL)
+                        .password(INVALID_PASSWORD)
+                        .build();
+            }
+
+            @Test
+            @DisplayName("404 를 응답한다.")
+            void It_responses_404() throws Exception {
+                mockMvc.perform(
+                                post("/session")
+                                        .contentType(MediaType.APPLICATION_JSON)
+                                        .content(toJson(userLoginData))
+                        )
+                        .andExpect(status().isNotFound());
+            }
+        }
+    }
+}

c4329db3314ef343f4c531d7a8f7b1163b3047e4
MJbae <bmj5208@gmail.com>
2022-06-09 20:49:29 +0900

    TokenAuthService 인터페이스에 대한 주석 추가

--- app/src/main/java/com/codesoom/assignment/application/TokenAuthService.java
+++ app/src/main/java/com/codesoom/assignment/application/TokenAuthService.java
@@ -1,5 +1,20 @@
 package com.codesoom.assignment.application;
 
+/**
+ * 토큰 기반 사용자 인증에 대한 비지니스 로직을 처리한다
+ * <p>
+ * All Known Implementing Classes:
+ * JwtAuthService
+ * </p>
+ */
 public interface TokenAuthService {
+    /**
+     * 사용자 인증에 필요한 정보를 받아서 문자열 타입의 토큰으로 반환한다
+     * <p>
+     * @param email 사용자 이메일 정보
+     * @param password 사용자 비밀번호 정보
+     * @return 문자열 타입의 토큰
+     * </p>
+     */
     String login(String email, String password);
 }

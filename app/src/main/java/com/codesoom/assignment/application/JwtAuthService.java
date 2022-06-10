5be97f880ff798ea3b800a05ae08e5160957eabd
MJbae <bmj5208@gmail.com>
2022-06-10 15:57:43 +0900

    JwtAuthService 내 참조변수의 타입을 인터페이스로 수정

--- app/src/main/java/com/codesoom/assignment/application/JwtAuthService.java
+++ app/src/main/java/com/codesoom/assignment/application/JwtAuthService.java
@@ -1,14 +1,17 @@
 package com.codesoom.assignment.application;
 
+import com.codesoom.assignment.auth.ClaimTokenAuth;
 import com.codesoom.assignment.auth.JwtAuth;
 import com.codesoom.assignment.domain.User;
+import com.codesoom.assignment.domain.UserRepository;
 import com.codesoom.assignment.infra.JpaUserRepository;
+import io.jsonwebtoken.Claims;
 import org.springframework.stereotype.Service;
 
 @Service
 public class JwtAuthService implements TokenAuthService {
-    private final JwtAuth auth;
-    private final JpaUserRepository repository;
+    private final ClaimTokenAuth<Claims> auth;
+    private final UserRepository repository;
 
     public JwtAuthService(JwtAuth auth, JpaUserRepository repository) {
         this.auth = auth;

7c51cc3306aac1e3349af679c01505246df55e63
minjoon_lee <minjoon1995@naver.com>
2022-06-10 19:30:57 +0900

    feat : JwtUtil decode, parse 구분

--- app/src/main/java/com/codesoom/assignment/application/AuthenticationService.java
+++ app/src/main/java/com/codesoom/assignment/application/AuthenticationService.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Service;
 import com.codesoom.assignment.dto.SessionResponseData;
 import com.codesoom.assignment.utils.JwtUtil;
 
+import io.jsonwebtoken.Claims;
+
 @Service
 public class AuthenticationService {
 	private JwtUtil jwtUtil;
@@ -20,7 +22,11 @@ public class AuthenticationService {
 			.build();
 	}
 
-	public Long decode(String token) {
+	public Claims decode(String token) {
+		return jwtUtil.decode(token);
+	}
+
+	public Long parseUserId(String token) {
 		return jwtUtil.decode(token).get("userId", Long.class);
 	}
 }

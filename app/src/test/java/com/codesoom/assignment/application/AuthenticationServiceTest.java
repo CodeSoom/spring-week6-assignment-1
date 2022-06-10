f493e5756574b6ed4e5c2c18e3d8ff84f1ba10d3
minjoon_lee <minjoon1995@naver.com>
2022-06-10 19:30:14 +0900

    fix : test 코드 DCI 형식으로 수정

--- app/src/test/java/com/codesoom/assignment/application/AuthenticationServiceTest.java
+++ app/src/test/java/com/codesoom/assignment/application/AuthenticationServiceTest.java
@@ -3,11 +3,15 @@ package com.codesoom.assignment.application;
 import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import com.codesoom.assignment.dto.SessionResponseData;
 import com.codesoom.assignment.errors.DecodingInValidTokenException;
 import com.codesoom.assignment.utils.JwtUtil;
+
+import io.jsonwebtoken.Claims;
 
 public class AuthenticationServiceTest {
 	private AuthenticationService authenticationService;
@@ -28,15 +32,30 @@ public class AuthenticationServiceTest {
 		assertThat(sessionResponseData.getAccessToken()).isEqualTo(VALID_TOKEN);
 	}
 
-	@Test
-	void decodeWithValidToken() {
-		Long userId = authenticationService.decode(VALID_TOKEN);
-		assertThat(userId).isEqualTo(1L);
-	}
+	@Nested
+	@DisplayName("decode 메소드는")
+	class decode {
+		@Nested
+		@DisplayName("유저 아이디와 키로 인코딩한 유효한 토큰이 주어졌을 때")
+		class withValidToken {
+			@Test
+			@DisplayName("유저 아이디를 갖고 있는 클레임을 응답한다.")
+			void decodeWithValidToken() {
+				Claims claims = authenticationService.decode(VALID_TOKEN);
+				assertThat(claims.get("userId", Long.class)).isEqualTo(1L);
+			}
+		}
 
-	@Test
-	void decodeWithInValidToken() {
-		assertThatThrownBy(() -> authenticationService.decode(INVALID_TOKEN)).isInstanceOf(
-			DecodingInValidTokenException.class);
+		@Nested
+		@DisplayName("유효하지 않은 토큰이 주어졌을 때")
+		class withInValidToken {
+			@Test
+			@DisplayName("DecodingInValidTokenException 을 응답한다.")
+			void decodeWithValidToken() {
+				Claims claims = authenticationService.decode(INVALID_TOKEN);
+				assertThatThrownBy(() -> authenticationService.decode(INVALID_TOKEN))
+					.isInstanceOf(DecodingInValidTokenException.class);
+			}
+		}
 	}
 }

4d1344779ae784ddf1c333da54d560fa41d6d91d
HanJiyoung <jiyounghaan@gmail.com>
2022-06-10 21:16:19 +0900

    login() 메서드에서 회원 존재 여부를 확인하기 위한 코드를 작성하라
    존재하지 않는 id가 주어졌을 경우에 대한 테스트 코드를 통과하기 위해서, 회원 존재 여부를 확인하여 없으면 UserNotFoundException 예외를 던지도록 했습니다.

--- app/src/main/java/com/codesoom/assignment/application/AuthenticationService.java
+++ app/src/main/java/com/codesoom/assignment/application/AuthenticationService.java
@@ -1,5 +1,6 @@
 package com.codesoom.assignment.application;
 
+import com.codesoom.assignment.errors.UserNotFoundException;
 import com.codesoom.assignment.utils.JwtUtil;
 import org.springframework.stereotype.Service;
 
@@ -10,16 +11,27 @@ import org.springframework.stereotype.Service;
 public class AuthenticationService {
     private JwtUtil jwtUtil;
 
-    public AuthenticationService(JwtUtil jwtUtil) {
+    private final UserService userService;
+
+    public AuthenticationService(JwtUtil jwtUtil, UserService userService) {
         this.jwtUtil = jwtUtil;
+        this.userService = userService;
     }
 
     /**
      * userId로 토큰을 만들고, 이를 반환한다.
      * 
      * @return 토큰
+     * @throws UserNotFoundException
+     *          id로 User를 찾지 못한 경우
      */
     public String login(Long userId) {
+        try {
+            userService.getUser(userId);
+        } catch (NullPointerException e) {
+            throw new UserNotFoundException(userId);
+        }
+
         return jwtUtil.encode(userId);
     }
 }

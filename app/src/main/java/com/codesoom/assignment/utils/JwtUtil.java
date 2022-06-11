46ade0e6e2ebe379073a7abc2b2a9cafb62f519a
MinJoonLee <52944973+mj950425@users.noreply.github.com>
2022-06-11 18:18:07 +0900

    feat : JwtUtil Role 파싱 기능 추가

--- app/src/main/java/com/codesoom/assignment/utils/JwtUtil.java
+++ app/src/main/java/com/codesoom/assignment/utils/JwtUtil.java
@@ -1,34 +1,33 @@
 package com.codesoom.assignment.utils;
 
-import java.security.Key;
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
-
+import com.codesoom.assignment.domain.Role;
 import com.codesoom.assignment.exception.DecodingInValidTokenException;
-
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import io.jsonwebtoken.security.SecurityException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
 
 @Component
 public class JwtUtil {
-	private final Key key;
+    private final Key key;
 
-	public JwtUtil(@Value("${jwt.secret}") String secret) {
-		this.key = Keys.hmacShaKeyFor(secret.getBytes());
-	}
+    public JwtUtil(@Value("${jwt.secret}") String secret) {
+        this.key = Keys.hmacShaKeyFor(secret.getBytes());
+    }
 
-	public String encode(Long userId) {
-		return Jwts.builder().claim("userId", userId).signWith(key).compact();
-	}
+    public String encode(Long userId, Role role) {
+        return Jwts.builder().claim("userId", userId).claim("role", role).signWith(key).compact();
+    }
 
-	public Claims decode(String token) {
-		try {
-			return Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token).getBody();
-		} catch (SecurityException e) {
-			throw new DecodingInValidTokenException(token); // when decode invalid token
-		}
-	}
+    public Claims decode(String token) {
+        try {
+            return Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token).getBody();
+        } catch (SecurityException e) {
+            throw new DecodingInValidTokenException(token); // when decode invalid token
+        }
+    }
 }

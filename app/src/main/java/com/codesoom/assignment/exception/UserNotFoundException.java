8ad52367e7f9600fa0c96174d564e4f4efe8fc13
MinJoonLee <52944973+mj950425@users.noreply.github.com>
2022-06-11 18:17:09 +0900

    feat : NoRightPasswordException 클래스 추가 , UserNotFoundException 생성자 추가

--- app/src/main/java/com/codesoom/assignment/exception/UserNotFoundException.java
+++ app/src/main/java/com/codesoom/assignment/exception/UserNotFoundException.java
@@ -4,4 +4,7 @@ public class UserNotFoundException extends RuntimeException {
     public UserNotFoundException(Long id) {
         super("User not found: " + id);
     }
+    public UserNotFoundException(String email) {
+        super("User not found: " + email);
+    }
 }

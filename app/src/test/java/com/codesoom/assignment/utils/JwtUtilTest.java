80d1717d9a0c7d9ad26c2efdaa39a99c8329ed29
MinJoonLee <52944973+mj950425@users.noreply.github.com>
2022-06-11 18:17:32 +0900

    test : Role 파싱 기능 테스트 코드 추가

--- app/src/test/java/com/codesoom/assignment/utils/JwtUtilTest.java
+++ app/src/test/java/com/codesoom/assignment/utils/JwtUtilTest.java
@@ -1,36 +1,42 @@
 package com.codesoom.assignment.utils;
 
-import static org.assertj.core.api.Assertions.*;
-
+import com.codesoom.assignment.domain.Role;
+import com.codesoom.assignment.exception.DecodingInValidTokenException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import com.codesoom.assignment.exception.DecodingInValidTokenException;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class JwtUtilTest {
-	private static String VALID_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjF9.neCsyNLzy3lQ4o2yliotWT06FwSGZagaHpKdAkjnGGw";
-	private static String INVALID_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjF9.neCsyNLzy3lQ4o2yliotWT06FwSGZagaHpKdAkjnGGw1";
-	private static String SECRET = "12345678901234567890123456789010";
-	private JwtUtil jwtUtil;
+    private static String VALID_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjEsInJvbGUiOiJNRU1CRVIifQ.X99CPxQFtgjARrg9bqR6bQkp6JFMN9a-XUo9GAdO4so";
+    private static String INVALID_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjEsInJvbGUiOiJNRU1CRVIifQ.X99CPxQFtgjARrg9bqR6bQkp6JFMN9a-XUo9GAdO4so111";
+    private static String SECRET = "12345678901234567890123456789010";
+    private JwtUtil jwtUtil;
 
-	@BeforeEach
-	void setUp() {
-		jwtUtil = new JwtUtil(SECRET);
-	}
+    @BeforeEach
+    void setUp() {
+        jwtUtil = new JwtUtil(SECRET);
+    }
 
-	@Test
-	void encodeToken() {
-		assertThat(jwtUtil.encode(1L)).isEqualTo(VALID_TOKEN);
-	}
+    @Test
+    void encodeToken() {
+        assertThat(jwtUtil.encode(1L, Role.MEMBER)).isEqualTo("eyJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjEsInJvbGUiOiJNRU1CRVIifQ.X99CPxQFtgjARrg9bqR6bQkp6JFMN9a-XUo9GAdO4so");
+    }
 
-	@Test
-	void decodeWithValidToken() {
-		assertThat(jwtUtil.decode(VALID_TOKEN).get("userId", Long.class)).isEqualTo(1L);
-	}
+    @Test
+    void parseUserIdWithValidToken() {
+        assertThat(jwtUtil.decode(VALID_TOKEN).get("userId", Long.class)).isEqualTo(1L);
+    }
 
-	@Test
-	void decodeWithInValidToken() {
-		assertThatThrownBy(() -> jwtUtil.decode(INVALID_TOKEN))
-			.isInstanceOf(DecodingInValidTokenException.class);
-	}
+    @Test
+    void parseRoleWithValidToken() {
+        assertThat(jwtUtil.decode(VALID_TOKEN).get("role", String.class)).isEqualTo("MEMBER");
+    }
+
+    @Test
+    void decodeWithInValidToken() {
+        assertThatThrownBy(() -> jwtUtil.decode(INVALID_TOKEN))
+                .isInstanceOf(DecodingInValidTokenException.class);
+    }
 }

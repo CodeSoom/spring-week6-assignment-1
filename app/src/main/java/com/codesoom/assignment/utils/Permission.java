ac5a6fca6073255355b5df06eaa87c5efc351d8b
MinJoonLee <52944973+mj950425@users.noreply.github.com>
2022-06-12 11:06:52 +0900

    refactoring : Permission class 에 주석 추가

--- app/src/main/java/com/codesoom/assignment/utils/Permission.java
+++ app/src/main/java/com/codesoom/assignment/utils/Permission.java
@@ -7,6 +7,9 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/***
+ * Annotation where authorization is required
+ */
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Permission {

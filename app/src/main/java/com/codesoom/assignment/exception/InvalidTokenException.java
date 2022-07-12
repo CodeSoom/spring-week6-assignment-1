502fcfcb3df36216526ac42f57b1870d7e647500
unajun <bjyoon513@gmail.com>
2022-07-13 01:09:07 +0900

    feat: implement InvalidTokenException

---
+++ app/src/main/java/com/codesoom/assignment/exception/InvalidTokenException.java
@@ -0,0 +1,7 @@
+package com.codesoom.assignment.exception;
+
+public class InvalidTokenException extends RuntimeException{
+    public InvalidTokenException(String message) {
+        super(message);
+    }
+}

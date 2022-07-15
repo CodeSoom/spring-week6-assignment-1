878c2b66b4fbbd8e6dd87701a1c079e9a700636c
Noh Jinu <yes.jinu@gmail.com>
2022-07-15 20:46:01 +0900

    docs: JwtContents 클래스에 이해를 돕는 주석을 추가함

--- app/src/main/java/com/codesoom/assignment/jwt/JwtContents.java
+++ app/src/main/java/com/codesoom/assignment/jwt/JwtContents.java
@@ -4,16 +4,19 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jws;
 import lombok.Getter;
 
+/**
+ * @see <a href="https://datatracker.ietf.org/doc/html/rfc7519">RFC7519 - JSON Web Token (JWT)</a>
+ */
 @Getter
 public class JwtContents {
     // HEADER
-    private String alg = "HS256";
-    private String typ = "JWT";
+    private String alg = "HS256"; // algorithm, hash algorithm used to sign the JWT
+    private String typ = "JWT"; // type, media type of JWT
 
     // PAYLOAD
-    private String iss;
-    private String sub;
-    private String aud;
+    private String iss; // issuer, identifies the principal that issued the JWT
+    private String sub; // subject, identifies the principal that is the subject of the JWT
+    private String aud; // audience, identifies the recipients that the JWT is intended for
 
     public JwtContents(String iss, String sub, String aud) {
         this.iss = (iss == null) ? "" : iss;

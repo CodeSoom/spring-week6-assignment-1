cecfbdf88d339bfc4f8315045432b41fcd61214e
eunice0035 <eunice7357@gmail.com>
2022-08-08 20:55:30 +0900

    jwt 생성

--- app/src/main/java/com/codesoom/assignment/dto/SessionResponseData.java
+++ app/src/main/java/com/codesoom/assignment/dto/SessionResponseData.java
@@ -1,4 +1,14 @@
 package com.codesoom.assignment.dto;
 
-public class SessionResponse {
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class SessionResponseData {
+    private String accessToken;
+
+
 }

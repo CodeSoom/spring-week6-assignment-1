5c1e033e35c36ddffd635a71650dc1e837e98656
박범진 <qjawlsqjacks@fasoo.com>
2022-08-09 15:52:18 +0900

    회원 로그인 인수 테스트 재정의
    회원의 id 를 받아 토큰을 리턴하는 시나리오 ->
    회원의 id/pw 를 받아 토큰을 리턴하는 시나리오로 변경했습니다.

--- app/src/main/java/com/codesoom/assignment/controllers/AuthenticationController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/AuthenticationController.java
@@ -1,12 +1,16 @@
 package com.codesoom.assignment.controllers;
 
 import com.codesoom.assignment.application.AuthenticationService;
+import com.codesoom.assignment.dto.UserLoginData;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/session")
@@ -18,9 +22,9 @@ public class AuthenticationController {
         this.service = service;
     }
 
-    @PostMapping("/{id}")
+    @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public String login(@PathVariable("id") Long id) {
-        return service.create(id);
+    public String login(@RequestBody @Valid UserLoginData data) {
+        return service.login(data);
     }
 }
--- app/src/test/java/com/codesoom/assignment/controllers/AuthenticationControllerTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/AuthenticationControllerTest.java
@@ -1,12 +1,18 @@
 package com.codesoom.assignment.controllers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import java.util.Map;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -17,19 +23,49 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 @DisplayName("AuthenticationController 클래스의")
 public class AuthenticationControllerTest {
+    public static final String SESSION_PATH = "/session";
+    public static final String USER_PATH = "/users";
+
     @Autowired
     private MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    private final Map<String, Object> registerData = Map.of(
+            "email", "qjawlsqjacks@naver.com",
+            "name", "박범진",
+            "password", "1234"
+    );
+    private final Map<String, Object> loginData = Map.of(
+            "email", "qjawlsqjacks@naver.com",
+            "password", "1234"
+    );
+
+    private ResultActions postRequest(String path, Map<String, Object> requestBody) throws Exception {
+        return mockMvc.perform(post(path)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(requestBody)));
+    }
 
     @Nested
     @DisplayName("login 메서드는")
     class Describe_login {
         @Nested
-        @DisplayName("요청이 들어오면")
+        @DisplayName("인증된 유저라면")
         class Context_with_request {
+            private void registerUser() throws Exception {
+                postRequest(USER_PATH, registerData);
+            }
+
+            @BeforeEach
+            void prepare() throws Exception {
+                registerUser();
+            }
+
             @Test
-            @DisplayName("토큰 값과 상태코드 201을 응답한다")
+            @DisplayName("토큰과 상태코드 201을 응답한다")
             void It_respond_token() throws Exception {
-                mockMvc.perform(post("/session" + "/1"))
+                postRequest(SESSION_PATH, loginData)
                         .andExpect(status().isCreated())
                         .andExpect(content().string(containsString(".")));
             }

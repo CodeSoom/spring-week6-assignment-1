5fbc9d56bf0d1da57cc0f745ab3c1150622cf8c4
박범진 <qjawlsqjacks@fasoo.com>
2022-08-09 16:44:31 +0900

    login 메서드 시그니처 수정

--- app/src/main/java/com/codesoom/assignment/application/AuthenticationService.java
+++ app/src/main/java/com/codesoom/assignment/application/AuthenticationService.java
@@ -1,5 +1,13 @@
 package com.codesoom.assignment.application;
 
+import com.codesoom.assignment.dto.UserLoginData;
+
 public interface AuthenticationService {
-    String create(Long userId);
+    /**
+     * 유저 정보가 유효하면 토큰을 리턴한다.
+     *
+     * @param data 유저 정보
+     * @return 토큰
+     */
+    String login(UserLoginData data);
 }

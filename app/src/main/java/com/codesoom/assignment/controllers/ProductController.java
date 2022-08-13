7987fd2b26520027ac52c59735c6e047f2d82ea6
BEOMKING <qjawlsqjacks@naver.com>
2022-08-13 20:36:17 +0900

    토큰 검증 로직 인터셉터 적용
    인터셉터 생성 및 Config 적용
    토큰 검증 로직 분기문 작성
    상품 컨트롤러에 토큰 로직 검증 로직 제거

--- app/src/main/java/com/codesoom/assignment/controllers/ProductController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/ProductController.java
@@ -1,13 +1,20 @@
 package com.codesoom.assignment.controllers;
 
-import com.codesoom.assignment.application.AuthenticationService;
 import com.codesoom.assignment.application.ProductService;
 import com.codesoom.assignment.domain.Product;
 import com.codesoom.assignment.dto.ProductData;
 import com.codesoom.assignment.dto.ProductResponse;
-import io.jsonwebtoken.Claims;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -17,11 +24,9 @@ import java.util.List;
 @CrossOrigin
 public class ProductController {
     private final ProductService productService;
-    private final AuthenticationService authenticationService;
 
-    public ProductController(ProductService productService, AuthenticationService authenticationService) {
+    public ProductController(ProductService productService) {
         this.productService = productService;
-        this.authenticationService = authenticationService;
     }
 
     @GetMapping
@@ -37,31 +42,25 @@ public class ProductController {
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public ProductResponse create(
-            @RequestHeader("Authorization") String authorization,
             @RequestBody @Valid ProductData productData) {
 
-        Claims claims = authenticationService.parseToken(authorization);
         return productService.createProduct(productData);
     }
 
     @PutMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     public ProductResponse update(
-            @RequestHeader("Authorization") String authorization,
             @PathVariable("id") Long id,
             @RequestBody @Valid ProductData productData) {
 
-        authenticationService.parseToken(authorization);
         return productService.updateProduct(id, productData);
     }
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(
-            @RequestHeader("Authorization") String authorization,
             @PathVariable Long id) {
 
-        authenticationService.parseToken(authorization);
         productService.deleteProduct(id);
     }
 }

90eb0d14ad3348305e309f3e9ad5b552d0629404
Ernest Hong <hsh3592@gmail.com>
2022-08-13 14:17:47 +0900

    constructor에서 `@JsonCreator` 사용하도록 변경
    HttpMessageConverter를 위해서 lombok`@Setter`, `@NoArgsConstructor` 어노테이션을 사용했다고 생각했는데 더 나은 방법을 발견해서 수정했습니다.
    기존의 구현에서도 `@NoArgsConstructor`는 필요했지만 `@Setter`는 필요없었습니다.
    
    Jackson이 JSON을 Object로 변환할 때 특정 constructor를 사용하도록 지정할 수 있습니다.
    `@JsonCreator`로 Jackson이 어떤 constructor를 써야하는지 알려주고,
    `@JsonProperty`로 어떤 key로 프로퍼티를 매핑해야할지 지정할 수 있습니다.

--- app/src/main/java/com/codesoom/assignment/dto/SessionRegistrationData.java
+++ app/src/main/java/com/codesoom/assignment/dto/SessionRegistrationData.java
@@ -1,17 +1,19 @@
 package com.codesoom.assignment.dto;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Getter
-@Setter
-@NoArgsConstructor
 public class SessionRegistrationData {
-    private String email;
-    private String password;
+    private final String email;
+    private final String password;
 
-    public SessionRegistrationData(String email, String password) {
+    @JsonCreator
+    public SessionRegistrationData(
+            @JsonProperty("email") String email,
+            @JsonProperty("password") String password
+    ) {
         this.email = email;
         this.password = password;
     }

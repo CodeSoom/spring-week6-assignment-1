50766c1f5e098894dfa34ac6a64465b681534ee2
정현준 <jeongdalma@gmail.com>
2022-09-05 18:34:28 +0900

    Controller , Service 주석 작성

--- app/src/main/java/com/codesoom/assignment/application/UserService.java
+++ app/src/main/java/com/codesoom/assignment/application/UserService.java
@@ -22,6 +22,13 @@ public class UserService {
         this.userRepository = userRepository;
     }
 
+    /**
+     * 사용자를 등록한다.
+     *
+     * @param registrationData 등록할 유저의 정보
+     * @throws UserEmailDuplicationException 사용자의 Email이 중복되었을 경우
+     * @return 저장된 유저의 정보
+     */
     public User registerUser(UserRegistrationData registrationData) {
         String email = registrationData.getEmail();
         if (userRepository.existsByEmail(email)) {
@@ -32,6 +39,14 @@ public class UserService {
         return userRepository.save(user);
     }
 
+    /**
+     * 사용자를 수정한다.
+     *
+     * @param id 수정할 사용자의 식별자
+     * @param modificationData 수정할 사용자의 정보
+     * @throws UserNotFoundException 식별자에 해당하는 사용자가 존재하지 않을 경우
+     * @return 수정된 사용자
+     */
     public User updateUser(Long id, UserModificationData modificationData) {
         User user = findUser(id);
 
@@ -41,12 +56,26 @@ public class UserService {
         return user;
     }
 
+    /**
+     * 사용자를 삭제한다.
+     *
+     * @param id 삭제할 사용자의 식별자
+     * @throws UserNotFoundException 식별자에 해당하는 사용자가 존재하지 않을 경우
+     * @return 삭제된 사용자
+     */
     public User deleteUser(Long id) {
         User user = findUser(id);
         user.destroy();
         return user;
     }
 
+    /**
+     * 사용자를 조회한다.
+     *
+     * @param id 조회할 사용자의 식별자
+     * @throws UserNotFoundException 식별자에 해당하는 사용자가 존재하지 않을 경우
+     * @return 식별자에 해당하는 사용자
+     */
     private User findUser(Long id) {
         return userRepository.findByIdAndDeletedIsFalse(id)
                 .orElseThrow(() -> new UserNotFoundException(id));
--- app/src/main/java/com/codesoom/assignment/controllers/ProductController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/ProductController.java
@@ -3,6 +3,7 @@ package com.codesoom.assignment.controllers;
 import com.codesoom.assignment.application.ProductService;
 import com.codesoom.assignment.domain.Product;
 import com.codesoom.assignment.dto.ProductData;
+import com.codesoom.assignment.errors.ProductNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,16 +20,32 @@ public class ProductController {
         this.productService = productService;
     }
 
+    /**
+     * 모든 상품을 조회한다.
+     */
     @GetMapping
     public List<Product> list() {
         return productService.getProducts();
     }
 
+    /**
+     * 상품을 조회한다.
+     *
+     * @param id 조회할 상품의 식별자
+     * @throws ProductNotFoundException 식별자에 해당하는 상품이 존재하지 않을 경우
+     * @return 식별자에 해당하는 상품
+     */
     @GetMapping("{id}")
     public Product detail(@PathVariable Long id) {
         return productService.getProduct(id);
     }
 
+    /**
+     * 상품을 저장한다.
+     *
+     * @param productData 상품 저장 정보
+     * @return 저장된 상품의 정보
+     */
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Product create(
@@ -37,6 +54,14 @@ public class ProductController {
         return productService.createProduct(productData);
     }
 
+    /**
+     * 상품을 수정한다.
+     *
+     * @param id 수정할 상품의 식별자
+     * @param productData 상품 수정 정보
+     * @throws ProductNotFoundException 식별자에 해당하는 상품이 존재하지 않을 경우
+     * @return 수정된 상품
+     */
     @PatchMapping("{id}")
     public Product update(
             @PathVariable Long id,
@@ -45,6 +70,12 @@ public class ProductController {
         return productService.updateProduct(id, productData);
     }
 
+    /**
+     * 상품을 삭제한다.
+     *
+     * @throws ProductNotFoundException 식별자에 해당하는 상품이 존재하지 않을 경우
+     * @param id 삭제할 상품의 식별자
+     */
     @DeleteMapping("{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void destroy(

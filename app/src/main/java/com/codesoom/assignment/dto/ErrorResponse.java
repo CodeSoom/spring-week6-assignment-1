042eaaacc88f0ac16f0095e227c2463294421f0a
정현준 <jeongdalma@gmail.com>
2022-09-16 19:02:24 +0900

    빈 검증 실패 시 에러 메시지 추가

--- app/src/main/java/com/codesoom/assignment/dto/ErrorResponse.java
+++ app/src/main/java/com/codesoom/assignment/dto/ErrorResponse.java
@@ -1,13 +1,25 @@
 package com.codesoom.assignment.dto;
 
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+import java.util.List;
+
+@Getter
 public class ErrorResponse {
     private String message;
+    private HttpStatus status;
+    private List<String> errors;
 
     public ErrorResponse(String message) {
         this.message = message;
     }
 
-    public String getMessage() {
-        return message;
+    @Builder
+    public ErrorResponse(String message, HttpStatus status, List<String> errors) {
+        this.message = message;
+        this.status = status;
+        this.errors = errors;
     }
 }

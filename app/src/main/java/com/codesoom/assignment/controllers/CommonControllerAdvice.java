017b2a0ea1c08b9593523a54433e30981dcd4002
Eunha Jeong <eunhaay@gmail.com>
2022-10-18 17:46:34 +0900

    Feat: 세션 컨트롤러 구현

--- app/src/main/java/com/codesoom/assignment/controllers/CommonControllerAdvice.java
+++ app/src/main/java/com/codesoom/assignment/controllers/CommonControllerAdvice.java
@@ -4,15 +4,15 @@ import com.codesoom.assignment.dto.ErrorResponse;
 import com.codesoom.assignment.exception.ProductNotFoundException;
 import com.codesoom.assignment.exception.UserEmailDuplicationException;
 import com.codesoom.assignment.exception.UserNotFoundException;
+import com.codesoom.assignment.exception.WrongUserPasswordException;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ResponseBody
-@ControllerAdvice
+@RestControllerAdvice
 public class CommonControllerAdvice {
+
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(ProductNotFoundException.class)
     public ErrorResponse handleProductNotFound() {
@@ -29,5 +29,11 @@ public class CommonControllerAdvice {
     @ExceptionHandler(UserEmailDuplicationException.class)
     public ErrorResponse handleUserEmailIsAlreadyExisted() {
         return new ErrorResponse("User's email address is already existed");
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(WrongUserPasswordException.class)
+    public ErrorResponse handleWrongPassword(Exception e) {
+        return new ErrorResponse(e.getMessage());
     }
 }

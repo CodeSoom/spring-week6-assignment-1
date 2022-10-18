c0c2f33809075a1d23da320569846ea18d751873
hautblis <hautblis@gmail.com>
2022-10-18 20:31:57 +0900

    POST /sessions 에 대한 구현

---
+++ app/src/main/java/com/codesoom/assignment/controllers/SessionController.java
@@ -0,0 +1,28 @@
+package com.codesoom.assignment.controllers;
+
+import com.codesoom.assignment.application.SessionService;
+import com.codesoom.assignment.dto.LoginRequestDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/sessions")
+public class SessionController {
+    private final SessionService sessionService;
+
+    public SessionController(SessionService sessionService) {
+        this.sessionService = sessionService;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public String login(@RequestBody @Valid LoginRequestDTO loginRequestDTO) {
+        return sessionService.login(loginRequestDTO);
+    }
+}

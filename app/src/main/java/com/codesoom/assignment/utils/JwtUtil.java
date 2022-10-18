6c55c4a926aec2432d5a67a12ec6cf0353ee27a2
hautblis <hautblis@gmail.com>
2022-10-18 17:13:07 +0900

    JwtUtil 의 encode 메서드에 구현

---
+++ app/src/main/java/com/codesoom/assignment/utils/JwtUtil.java
@@ -0,0 +1,25 @@
+package com.codesoom.assignment.utils;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+
+@Component
+public class JwtUtil {
+    private final Key key;
+
+    public JwtUtil(@Value("${jwt.secret}") String secret) {
+        this.key = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+    }
+
+    public String encode(Long userId) {
+        return Jwts.builder()
+                .claim("userId", userId)
+                .signWith(key)
+                .compact();
+    }
+}

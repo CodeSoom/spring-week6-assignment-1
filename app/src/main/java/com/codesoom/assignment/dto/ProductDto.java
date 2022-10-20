2079f7cacb2b151daa370c81e939caf3ebafc7a8
luckycw87 <blissful.pcw@gmail.com>
2022-10-20 13:14:51 +0900

    Fix: 금액 범위 유효성 검증 제거

--- app/src/main/java/com/codesoom/assignment/dto/ProductDto.java
+++ app/src/main/java/com/codesoom/assignment/dto/ProductDto.java
@@ -25,7 +25,6 @@ public class ProductDto {
         private String maker;
 
         @NotNull(message = "금액은 필수항목 입니다.")
-        @Range(min = 1000, max = 1000000, message = "금액은 {min} ~ {max}원 사이만 입력할 수 있습니다.")
         private Long price;
 
         private String imageUrl;

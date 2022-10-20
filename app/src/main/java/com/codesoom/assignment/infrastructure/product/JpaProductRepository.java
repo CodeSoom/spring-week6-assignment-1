73c8f8e23fa920cd9e6b32ef22452419feb83c59
luckycw87 <blissful.pcw@gmail.com>
2022-10-20 13:13:13 +0900

    Fix: 불필요한 `@Repository` 애노테이션 삭제

--- app/src/main/java/com/codesoom/assignment/infrastructure/product/JpaProductRepository.java
+++ app/src/main/java/com/codesoom/assignment/infrastructure/product/JpaProductRepository.java
@@ -5,6 +5,6 @@ import com.codesoom.assignment.domain.product.ProductRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
 public interface JpaProductRepository extends ProductRepository, JpaRepository<Product, Long> {
+
 }
--- app/src/main/java/com/codesoom/assignment/infrastructure/user/JpaUserRepository.java
+++ app/src/main/java/com/codesoom/assignment/infrastructure/user/JpaUserRepository.java
@@ -5,6 +5,7 @@ import com.codesoom.assignment.domain.user.UserRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
+import java.util.Optional;
+
 public interface JpaUserRepository extends UserRepository, JpaRepository<User, Long> {
 }

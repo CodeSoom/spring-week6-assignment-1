144bd167bccfba90980d7d35ac2f949a8865a481
giibeom <kpmyung@gmail.com>
2022-11-16 20:23:44 +0900

    test: (session-application) 유효한 회원 정보가 주어지면 해당 id로 인코딩 된 토큰 반환 케이스

--- app/src/test/java/com/codesoom/assignment/session/AuthenticationServiceTest.java
+++ app/src/test/java/com/codesoom/assignment/session/AuthenticationServiceTest.java
@@ -1,5 +1,7 @@
 package com.codesoom.assignment.session;
 
+import com.codesoom.assignment.common.util.JwtUtil;
+import com.codesoom.assignment.user.domain.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DisplayNameGeneration;
@@ -7,8 +9,16 @@ import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
-import static com.codesoom.assignment.support.LoginFixture.LOGIN_VALID;
+import java.util.Optional;
+
+import static com.codesoom.assignment.support.IdFixture.ID_MIN;
+import static com.codesoom.assignment.support.TokenFixture.ACCESS_TOKEN_1_VALID;
+import static com.codesoom.assignment.support.UserFixture.USER_1;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 /*
     - login 메서드는
@@ -23,10 +33,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 @DisplayName("AuthenticationService 단위 테스트")
 class AuthenticationServiceTest {
     private AuthenticationService authenticationService;
+    private final UserRepository userRepository = mock(UserRepository.class);
+    private final JwtUtil jwtUtil = mock(JwtUtil.class);
 
     @BeforeEach
     void setUpVariable() {
-        authenticationService = new AuthenticationService();
+        authenticationService = new AuthenticationService(userRepository, jwtUtil);
     }
 
     @Nested
@@ -39,11 +51,21 @@ class AuthenticationServiceTest {
             @Test
             @DisplayName("토큰을 반환한다")
             void it_returns_token() {
-                String accessToken = authenticationService.login(LOGIN_VALID.로그인_요청_데이터_생성());
+                given(userRepository.findByEmail(USER_1.EMAIL()))
+                        .willReturn(
+                                Optional.of(USER_1.회원_엔티티_생성(ID_MIN.value()))
+                        );
 
-                assertThat(accessToken).isNotBlank();
-                // TODO: 실제 유저의 accessToken 값인지 검증하는 로직이 있으면 좋을 것 같음
-                assertThat(accessToken).contains(".");
+                given(jwtUtil.encode(any(Long.class)))
+                        .willReturn(ACCESS_TOKEN_1_VALID.토큰());
+
+                String accessToken = authenticationService.login(USER_1.로그인_요청_데이터_생성());
+
+                assertThat(accessToken).isNotBlank()
+                        .contains(".");
+
+                verify(userRepository).findByEmail(USER_1.EMAIL());
+                verify(jwtUtil).encode(any(Long.class));
             }
         }
     }

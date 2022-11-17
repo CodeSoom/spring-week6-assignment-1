0260115ffae0b51f3be13a20956426b5fa4c9418
giibeom <kpmyung@gmail.com>
2022-11-17 19:15:51 +0900

    test: (session-application) 로그인 시 찾을 수 없는 계정일 경우의 예외 케이스

--- app/src/test/java/com/codesoom/assignment/domain/session/application/AuthenticationServiceTest.java
+++ app/src/test/java/com/codesoom/assignment/domain/session/application/AuthenticationServiceTest.java
@@ -2,6 +2,7 @@ package com.codesoom.assignment.domain.session.application;
 
 import com.codesoom.assignment.common.util.JwtUtil;
 import com.codesoom.assignment.domain.user.domain.UserRepository;
+import com.codesoom.assignment.domain.user.exception.UserNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DisplayNameGeneration;
@@ -14,21 +15,13 @@ import java.util.Optional;
 import static com.codesoom.assignment.support.IdFixture.ID_MIN;
 import static com.codesoom.assignment.support.TokenFixture.ACCESS_TOKEN_1_VALID;
 import static com.codesoom.assignment.support.UserFixture.USER_1;
+import static com.codesoom.assignment.support.UserFixture.USER_NOT_REGISTER;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
-/*
-    - login 메서드는
-        - 유효한 회원 정보가 주어지면
-            - 토큰을 반환한다
-        - 유효하지 않는 회원 정보가 주어지면
-            - 찾을 수 없는 Email일 경우
-                - 예외를 던진다 (UserNotFoundException)
-            - 비밀번호가 틀렸을 경우
-                - 예외를 던진다 (InvalidUserPasswordException)
-*/
 @DisplayName("AuthenticationService 단위 테스트")
 class AuthenticationServiceTest {
     private AuthenticationService authenticationService;
@@ -45,8 +38,8 @@ class AuthenticationServiceTest {
     class login_메서드는 {
 
         @Nested
-        @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
-        class 유효한_회원_정보가_주어지면 {
+        @DisplayName("유효한 회원 정보가 주어지면")
+        class Context_with_valid_user {
             @BeforeEach
             void setUpGiven() {
                 given(userRepository.findByEmail(USER_1.EMAIL()))
@@ -69,6 +62,35 @@ class AuthenticationServiceTest {
 
                 verify(userRepository).findByEmail(USER_1.EMAIL());
                 verify(jwtUtil).encode(ID_MIN.value());
+            }
+        }
+
+        /*
+            - login 메서드는
+                - 유효하지 않는 회원 정보가 주어지면
+                    - 찾을 수 없는 Email일 경우
+                        - 예외를 던진다 (UserNotFoundException)
+                    - 비밀번호가 틀렸을 경우
+                        - 예외를 던진다 (UserInvalidPasswordException)
+        */
+        @Nested
+        @DisplayName("유효하지 않는 회원 정보가 주어지면")
+        class Context_with_invalid_user {
+            @Nested
+            @DisplayName("찾을 수 없는 계정일 경우")
+            class Context_with_not_found_user {
+                @BeforeEach
+                void setUpGiven() {
+                    given(userRepository.findByEmail(USER_NOT_REGISTER.EMAIL()))
+                            .willThrow(new UserNotFoundException(USER_NOT_REGISTER.EMAIL()));
+                }
+
+                @Test
+                @DisplayName("예외를 던진다")
+                void it_returns_exception() {
+                    assertThatThrownBy(() -> authenticationService.login(USER_NOT_REGISTER.로그인_요청_데이터_생성()))
+                            .isInstanceOf(UserNotFoundException.class);
+                }
             }
         }
     }

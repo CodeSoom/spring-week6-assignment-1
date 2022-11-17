57425288a9091f19d3e21f38b54abaa2f3983cc6
giibeom <kpmyung@gmail.com>
2022-11-17 20:30:39 +0900

    test: (common-util) 디코딩 할 토큰이 유효할 경우 userId가 포함된 클레임 반환 케이스

--- app/src/test/java/com/codesoom/assignment/common/util/JwtUtilTest.java
+++ app/src/test/java/com/codesoom/assignment/common/util/JwtUtilTest.java
@@ -1,5 +1,10 @@
 package com.codesoom.assignment.common.util;
 
+import io.jsonwebtoken.Claims;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -12,10 +17,31 @@ class JwtUtilTest {
     @Autowired
     private JwtUtil jwtUtil;
 
-    @Test
-    void it_returns_token() {
-        String encode = jwtUtil.encode(ACCESS_TOKEN_1_VALID.아이디());
+    @Nested
+    @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+    class encode_메서드는 {
+        @Test
+        @DisplayName("토큰을 secretKey로 인코딩하여 리턴한다")
+        void it_returns_token() {
+            String token = jwtUtil.encode(ACCESS_TOKEN_1_VALID.아이디());
 
-        assertThat(encode).isEqualTo(ACCESS_TOKEN_1_VALID.토큰());
+            assertThat(token).isEqualTo(ACCESS_TOKEN_1_VALID.토큰());
+        }
+    }
+
+    @Nested
+    @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+    class decode_메서드는 {
+        @Nested
+        @DisplayName("유효한 토큰이 주어질 경우")
+        class Context_with_blank_token {
+            @Test
+            @DisplayName("userId가 포함된 클레임을 리턴한다")
+            void it_returns_exception() {
+                Claims claims = jwtUtil.decode(ACCESS_TOKEN_1_VALID.토큰());
+
+                assertThat(claims.get("userId", Long.class)).isEqualTo(ACCESS_TOKEN_1_VALID.아이디());
+            }
+        }
     }
 }

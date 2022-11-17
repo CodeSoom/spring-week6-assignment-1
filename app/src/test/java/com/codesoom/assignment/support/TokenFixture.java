db0ca44738dc510631ee0984fc4c3eed95367866
giibeom <kpmyung@gmail.com>
2022-11-17 22:03:33 +0900

    fix: (common-jwtUtil) secretKey를 기존 값으로 변경하여 토큰 fixture 데이터의 인코딩 디코딩 값 변경

--- app/src/test/java/com/codesoom/assignment/support/TokenFixture.java
+++ app/src/test/java/com/codesoom/assignment/support/TokenFixture.java
@@ -1,9 +1,9 @@
 package com.codesoom.assignment.support;
 
 public enum TokenFixture {
-    ACCESS_TOKEN_1_VALID("giibeomIsA2YearsBackendDeveloper", 1L , "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjF9.u-4fczk36juKx46aceMNuz2WJJJj1STWPjfLQA-Z5xY"),
-    ACCESS_TOKEN_1_INVALID("giibeomIsA2YearsBackendDeveloper", 1L, "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjF9.ZZ3CUxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"),
-    ACCESS_TOKEN_1_INVALID_BLANK("giibeomIsA2YearsBackendDeveloper", 1L, "                        ");
+    ACCESS_TOKEN_1_VALID("12345678901234567890123456789010", 1L , "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjF9.neCsyNLzy3lQ4o2yliotWT06FwSGZagaHpKdAkjnGGw"),
+    ACCESS_TOKEN_1_INVALID("12345678901234567890123456789010", 1L, "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjF9.ZZ3CUxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"),
+    ACCESS_TOKEN_1_INVALID_BLANK("12345678901234567890123456789010", 1L, "                        ");
 
     private final String secretKey;
     private final Long id;

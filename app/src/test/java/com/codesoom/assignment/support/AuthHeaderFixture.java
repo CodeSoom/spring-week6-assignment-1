492feee8a8a23cdc597ed07d5b04825659a6b3fe
giibeom <kpmyung@gmail.com>
2022-11-18 17:42:38 +0900

    test: (common-jwtUtil) accessToken 검증 시 토큰이 유효하지 않은 3개의 예외 케이스
    - null이 주어질 경우
    - 토큰 타입이 Bearer가 아닐 경우
    - 토큰 타입이 Bearer이지만 유효하지 않은 토큰일 경우

---
+++ app/src/test/java/com/codesoom/assignment/support/AuthHeaderFixture.java
@@ -0,0 +1,16 @@
+package com.codesoom.assignment.support;
+
+public enum AuthHeaderFixture {
+    INVALID_TOKEN_TYPE("Giibeom eyJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjF9.neCsyNLzy3lQ4o2yliotWT06FwSGZagaHpKdAkjnGGw"),
+    INVALID_TOKEN_VALUE("Bearer eyJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjF9.ZZ3CUxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+
+    AuthHeaderFixture(String authorization) {
+        this.authorization = authorization;
+    }
+
+    private final String authorization;
+
+    public String 인증_헤더값() {
+        return authorization;
+    }
+}

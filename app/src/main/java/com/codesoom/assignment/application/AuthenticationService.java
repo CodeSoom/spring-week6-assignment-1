08bc5ab7a8cbacf6b5d69dae20480c7a0a24dde6
EuijeongHwang_Mac <euijeong.hwang97@gmail.com>
2022-12-14 01:20:26 +0900

    Feat: AuthenticationService 서비스 클래스 추가
    인증 관련 서비스 클래스 추가함

---
+++ app/src/main/java/com/codesoom/assignment/application/AuthenticationService.java
@@ -0,0 +1,43 @@
+package com.codesoom.assignment.application;
+
+import com.codesoom.assignment.domain.User;
+import com.codesoom.assignment.domain.UserRepository;
+import com.codesoom.assignment.dto.UserLoginData;
+import com.codesoom.assignment.errors.LoginFailedException;
+import com.codesoom.assignment.errors.UserNotFoundException;
+import com.codesoom.assignment.util.JwtUtil;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+@Transactional
+public class AuthenticationService {
+
+    private final JwtUtil jwtUtil;
+
+    private final UserRepository userRepository;
+
+    public AuthenticationService(JwtUtil jwtUtil, UserRepository userRepository) {
+        this.jwtUtil = jwtUtil;
+        this.userRepository = userRepository;
+    }
+
+    public String login(UserLoginData userLoginData) {
+        String email = userLoginData.getEmail();
+        if (!userRepository.existsByEmail(email)) {
+            throw new UserNotFoundException(email);
+        }
+
+        User user = findUser(userLoginData);
+
+        return jwtUtil.createToken(user.getId());
+    }
+
+    private User findUser(UserLoginData userLoginData) {
+        return userRepository.findByEmailAndPassword(userLoginData.getEmail(), userLoginData.getPassword())
+                                    .stream()
+                                    .findFirst()
+                                    .orElseThrow(() -> new LoginFailedException(userLoginData.getEmail()));
+    }
+}

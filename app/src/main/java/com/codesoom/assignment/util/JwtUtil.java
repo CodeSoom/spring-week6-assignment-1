a1eeabcf45be96a6e182199fc7160df46b416f4a
EuijeongHwang_Mac <euijeong.hwang97@gmail.com>
2022-12-15 22:51:49 +0900

    Refactor: JWT 유효시간 30분 추가, validateToken 메소드 exception 클래스 추가
    JWT 유효시간 30분 추가함
    validateToken시 ExpiredJwtException, RuntimeException, Exception 클래스 추가함

--- app/src/main/java/com/codesoom/assignment/util/JwtUtil.java
+++ app/src/main/java/com/codesoom/assignment/util/JwtUtil.java
@@ -1,15 +1,17 @@
 package com.codesoom.assignment.util;
 
-import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureException;
 import io.jsonwebtoken.security.Keys;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.security.Key;
 import java.util.Date;
 
+@Slf4j
 @Component
 public class JwtUtil {
     private static final Integer VALIDITY_PERIOD = 1800000; //30분
@@ -27,24 +29,22 @@ public class JwtUtil {
                 .compact();
     }
 
-    public Claims getClaims(String token) {
-        return Jwts.parserBuilder()
-                    .setSigningKey(key)
-                    .build()
-                    .parseClaimsJws(token)
-                    .getBody();
-    }
-
     public boolean validateToken(String token) {
         try {
             Jwts.parserBuilder()
                     .setSigningKey(key)
                     .build()
                     .parseClaimsJws(token);
-
             return true;
         } catch (SignatureException e) {
-            return false;
+            log.error("ERROR-01 SignatureException: " + e.getMessage());
+        } catch (ExpiredJwtException e) {
+            log.error("ERROR-02 ExpiredJwtException: " + e.getMessage());
+        } catch (RuntimeException e) {
+            log.error("ERROR-03 RuntimeException: " + e.getMessage());
+        } catch (Exception e) {
+            log.error("ERROR-04 Exception: " + e.getMessage());
         }
+        return false;
     }
 }

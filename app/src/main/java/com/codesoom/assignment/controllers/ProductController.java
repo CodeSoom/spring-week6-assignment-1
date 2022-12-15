b7cf05da8fa44835428abb3b545dfb966643caa4
EuijeongHwang_Mac <euijeong.hwang97@gmail.com>
2022-12-15 20:10:59 +0900

    Feat: craete, update, destroy 메소드에 RequestHeader 추가
    토큰을 받는 Authorization 추가함

--- app/src/main/java/com/codesoom/assignment/controllers/ProductController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/ProductController.java
@@ -1,5 +1,6 @@
 package com.codesoom.assignment.controllers;
 
+import com.codesoom.assignment.application.AuthenticationService;
 import com.codesoom.assignment.application.ProductService;
 import com.codesoom.assignment.domain.Product;
 import com.codesoom.assignment.dto.ProductData;
@@ -14,9 +15,11 @@ import java.util.List;
 @CrossOrigin
 public class ProductController {
     private final ProductService productService;
+    private final AuthenticationService authenticationService;
 
-    public ProductController(ProductService productService) {
+    public ProductController(ProductService productService, AuthenticationService authenticationService) {
         this.productService = productService;
+        this.authenticationService = authenticationService;
     }
 
     @GetMapping
@@ -29,27 +32,44 @@ public class ProductController {
         return productService.getProduct(id);
     }
 
+    //TODO JWT 필요
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Product create(
+            @RequestHeader("Authorization") String authorization,
             @RequestBody @Valid ProductData productData
     ) {
+        parseToken(authorization);
+
         return productService.createProduct(productData);
     }
 
+    //TODO JWT 필요
     @PatchMapping("{id}")
     public Product update(
+            @RequestHeader("Authorization") String authorization,
             @PathVariable Long id,
             @RequestBody @Valid ProductData productData
     ) {
+        parseToken(authorization);
+
         return productService.updateProduct(id, productData);
     }
 
+    //TODO JWT 필요
     @DeleteMapping("{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void destroy(
+            @RequestHeader("Authorization") String authorization,
             @PathVariable Long id
     ) {
+        parseToken(authorization);
+
         productService.deleteProduct(id);
+    }
+
+    private void parseToken(String authorization) {
+        String accessToken = authorization.substring("Bearer ".length());
+        authenticationService.decodeToken(accessToken);
     }
 }

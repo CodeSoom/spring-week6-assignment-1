f2d8e707fb5387bde8b7a4b0891d717ce496378b
EuijeongHwang_Mac <euijeong.hwang97@gmail.com>
2022-12-16 20:38:21 +0900

    Test: 테스트 대상을 mocking 하지 않도록 수정
    실제 인스턴스를 사용하도록 변경함

--- app/src/test/java/com/codesoom/assignment/util/JwtUtilTest.java
+++ app/src/test/java/com/codesoom/assignment/util/JwtUtilTest.java
@@ -4,30 +4,25 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.mock;
 
 class JwtUtilTest {
     private static final String VALID_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjF9.neCsyNLzy3lQ4o2yliotWT06FwSGZagaHpKdAkjnGGw";
-    private static final String INVALID_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjF9.neCsyNLzy3lQ4o2yliotWT06FwSGZagaHpKdAkjnaab";
+    private static final String INVALID_TOKEN = VALID_TOKEN  + "WRONG";
 
-    private final JwtUtil jwtUtil = mock(JwtUtil.class);
+    private static final String SECRET_KEY = "12345678901234567890123456789010";
+
+    private JwtUtil jwtUtil;
 
     @BeforeEach
     void setUp() {
-        given(jwtUtil.createToken(anyLong())).willReturn(VALID_TOKEN);
-
-        given(jwtUtil.validateToken(VALID_TOKEN)).willReturn(Boolean.TRUE);
-
-        given(jwtUtil.validateToken(INVALID_TOKEN)).willReturn(Boolean.FALSE);
+        jwtUtil = new JwtUtil(SECRET_KEY);
     }
 
     @Test
     void createTokenWithId() {
         String token = jwtUtil.createToken(1L);
 
-        assertThat(token).isEqualTo(VALID_TOKEN);
+        assertThat(token.contains(".")).isTrue();
     }
 
     @Test

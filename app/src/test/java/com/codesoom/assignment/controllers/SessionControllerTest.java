921aebffce5fb2392a6aa6ec302efafded0ba91a
tlatmsrud <tlatmsrud@naver.com>
2023-03-15 23:52:21 +0900

    SessionController CorssOrigin 어노테이션 추가하라

--- app/src/test/java/com/codesoom/assignment/controllers/SessionControllerTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/SessionControllerTest.java
@@ -12,6 +12,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import static org.hamcrest.Matchers.containsString;
 

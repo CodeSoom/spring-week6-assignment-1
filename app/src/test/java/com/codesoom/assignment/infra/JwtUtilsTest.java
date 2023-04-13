c6af8e8238f2f34d532d029c50214df809c19e09
yskim <yskim@aptner.com>
2023-04-13 09:41:36 +0900

    feat JwtUtils DecodeThenGetUserId 테스트코드 작성

--- app/src/test/java/com/codesoom/assignment/infra/JwtUtilsTest.java
+++ app/src/test/java/com/codesoom/assignment/infra/JwtUtilsTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.*;
+
 @DisplayName("JwtUtils")
 class JwtUtilsTest {
 
@@ -22,7 +24,25 @@ class JwtUtilsTest {
             @DisplayName("엑세스토큰을 리턴한다. ")
             void it_returns_valid_accessToken() {
                 String accessToken = jwtUtils.encode(1L);
-                Assertions.assertThat(accessToken).contains(".");
+                assertThat(accessToken).contains(".");
+            }
+        }
+    }
+
+    @Nested
+    @DisplayName("decodeThenGetUserId 메소드는")
+    class Describe_decodeThenGetUserId {
+
+        @Nested
+        @DisplayName("정상적인 토큰이 들어올 경우")
+        class context_with_valid_token {
+
+            @Test
+            @DisplayName("userId를 반환한다. ")
+            void it_returns_userId() {
+                String token = "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjF9.neCsyNLzy3lQ4o2yliotWT06FwSGZagaHpKdAkjnGGw";
+                Long userId = JwtUtils.decodeThenGetUserId(token);
+                assertThat(userId).isEqualTo(1L);
             }
         }
     }

9fd2f24fd9d972535b1cb987aff93c7f9f63623c
tmxhsk99 <tmxhsk99@naver.com>
2023-08-12 18:02:49 +0900

    feat : 커스텀 에러 추가에 따른 에러처리 추가

--- app/src/main/java/com/codesoom/assignment/controllers/ControllerErrorAdvice.java
+++ app/src/main/java/com/codesoom/assignment/controllers/ControllerErrorAdvice.java
@@ -1,10 +1,7 @@
 package com.codesoom.assignment.controllers;
 
 import com.codesoom.assignment.dto.ErrorResponse;
-import com.codesoom.assignment.errors.InvalidLoginException;
-import com.codesoom.assignment.errors.ProductNotFoundException;
-import com.codesoom.assignment.errors.UserEmailDuplicationException;
-import com.codesoom.assignment.errors.UserNotFoundException;
+import com.codesoom.assignment.errors.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -36,5 +33,17 @@ public class ControllerErrorAdvice {
     @ExceptionHandler(InvalidLoginException.class)
     public ErrorResponse handleInvalidLogin() {
         return new ErrorResponse("Invalid login request");
+    }
+
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    @ExceptionHandler(AccessTokenNotFoundException.class)
+    public ErrorResponse handleAccessTokenNotFound() {
+        return new ErrorResponse("Access token not found");
+    }
+
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    @ExceptionHandler(InvalidAccessTokenException.class)
+    public ErrorResponse handleInvalidAccessToken() {
+        return new ErrorResponse("Invalid access token");
     }
 }

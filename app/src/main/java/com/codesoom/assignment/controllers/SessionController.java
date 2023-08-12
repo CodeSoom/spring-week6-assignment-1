8f2991d6208d9929de6feb6c053d6c61ebd458fa
tmxhsk99 <tmxhsk99@naver.com>
2023-08-12 20:37:04 +0900

    fix : 누락된 @CrossOrigin 어노테이션 추가

--- app/src/main/java/com/codesoom/assignment/controllers/SessionController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/SessionController.java
@@ -10,6 +10,7 @@ import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/session")
+@CrossOrigin
 public class SessionController {
 
     private final AuthenticationService authenticationService;

019b774f6093260a21de13d600fdce462283674a
tmxhsk99 <tmxhsk99@naver.com>
2023-08-12 17:59:38 +0900

    feat : product 관련 인증 처리를 위한 AuthInterceptor 추가

---
+++ app/src/main/java/com/codesoom/assignment/interceptor/AuthInterceptor.java
@@ -0,0 +1,34 @@
+package com.codesoom.assignment.interceptor;
+
+import com.codesoom.assignment.errors.AccessTokenNotFoundException;
+import com.codesoom.assignment.errors.InvalidAccessTokenException;
+import com.codesoom.assignment.utils.JwtUtil;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component
+public class AuthInterceptor implements HandlerInterceptor {
+
+    private final JwtUtil jwtUtil;
+
+    public AuthInterceptor(JwtUtil jwtUtil) {
+        this.jwtUtil = jwtUtil;
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+        String authorization = request.getHeader("Authorization");
+        if (authorization == null) throw new AccessTokenNotFoundException();
+        String accessToken = authorization.substring("Bearer ".length());
+        try {
+            jwtUtil.decode(accessToken);
+        } catch (Exception e) {
+            throw new InvalidAccessTokenException(e.getMessage());
+        }
+        return true;
+    }
+}
